08095b720a94ef410594a25fa6c3b5409319d7ad
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-07-21 08:01:57 +0800

    AU: 1 updated - sourcetree.portable [skip ci] https://gist.github.com/119dd9b33c4f188c5e1cf95ae72f7a0e/728da37005fdd4b20834bba0c2239cb6838c86cb

--- sourcetree.portable/tools/chocolateyInstall.ps1
+++ sourcetree.portable/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'sourcetree.portable'
-$Url =  'https://www.sourcetreeapp.com/update/windows/ga/SourceTree-2.1.2.5-full.nupkg'
-$Checksum = 'fc5fdd1fa3c84c150da30afbe4b92acc952f5a2c'
+$Url =  'https://www.sourcetreeapp.com/update/windows/ga/SourceTree-2.1.7-full.nupkg'
+$Checksum = 'b33c7652a287d1d043d08433fe917e40fda01379'
 $ChecksumType = 'sha1'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
