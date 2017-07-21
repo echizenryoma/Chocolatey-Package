7b435c21b22ecad12b51972e5ea451bab440bdfa
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-07-21 21:57:42 +0800

    fix bug

--- repack_all.ps1
+++ repack_all.ps1
@@ -1,15 +1,13 @@
 ﻿WorkFlow Choco-Pack {
     $dirs = Get-ChildItem -Directory -Exclude ".git"
     foreach -parallel ($dir in $dirs) {
-        $nuspec = Get-ChildItem -File $(Join-Path $dir "*.nuspec")
-        if (($nuspec | Measure-Object).Count -ge 1) {
+        $nuspec = Join-Path $dir "$($dir.Name).nuspec"
+        if (Test-Path $nuspec) {
             [xml]$XmlDocument = Get-Content -Path $nuspec
-            $id = $XmlDocument.package.metadata.id
             $version = $XmlDocument.package.metadata.version
-
-            $nupkg = Join-Path $dir "$id.$version.nupkg"
+            $nupkg = Join-Path $dir "$($dir.Name).$version.nupkg"
             if (-Not (Test-Path $nupkg)) {
-                $null = choco pack $(Join-Path $dir "$($dir.Name).nuspec") --out "$dir"
+                $null = choco pack "$(Join-Path $dir "$($dir.Name).nuspec")" --out "$dir"
             }
         }
     }
