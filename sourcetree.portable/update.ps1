4c38864062cf5007ccd9d3e9b977c1bda01ff106
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-08-11 21:01:31 +0800

    fix bug

--- sourcetree.portable/update.ps1
+++ sourcetree.portable/update.ps1
@@ -11,13 +11,16 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://www.sourcetreeapp.com/"
+    $url = $page.Links -match "SourceTreeSetup-\d+(\.\d+)+\.exe" | Select-Object -First 1 -ExpandProperty href
+    $version = $url -split "\\|/|-|\.exe" -match '\d+(\.\d+)+'
+
     $page = Invoke-WebRequest -UseBasicParsing -Uri "https://www.sourcetreeapp.com/update/windows/ga/RELEASES"
     $page = [System.Text.Encoding]::ASCII.GetString($page.Content)
 
-    $release = ($page -split "\n" -notmatch "delta|#"  | ConvertFrom-String -PropertyNames sha1sum, filename, filesize) | Select-Object -Last 1
+    $release = ($page -split "\n" -match "$version" -notmatch "delta|#"  | ConvertFrom-String -PropertyNames sha1sum, filename, filesize) | Select-Object -Last 1
 
     $url = "https://www.sourcetreeapp.com/update/windows/ga/" + $release.filename
-    $version = $release.filename -split "-|\.exe" -match "^\d+(\.\d+){2,}$"
 	
     return @{
         Version        = $version
