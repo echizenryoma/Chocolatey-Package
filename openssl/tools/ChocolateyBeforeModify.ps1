445197451d90d963110e6b3dd94658fcccad71e8
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-08-17 11:36:15 +0800

    remove opencv, openssl

--- openssl/tools/ChocolateyBeforeModify.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$PackageName = 'openssl'
-$InstallationPath = Get-ToolsLocation
-
-$OpenSSL_HOME = Join-Path $InstallationPath $PackageName
-if (Test-Path $OpenSSL_HOME) {
-    Remove-Item -Path $OpenSSL_HOME -Recurse -Force
-}
--- openssl/tools/ChocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$envPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) -split ';' -notmatch 'openssl'
-[Environment]::SetEnvironmentVariable('Path', $envPath -join ';', [EnvironmentVariableTarget]::Machine)
-
-Uninstall-ChocolateyEnvironmentVariable -VariableName 'OPENSSL_ROOT_DIR' -VariableType 'Machine'
