f457a5eb31b4a3a92039edd7a61a410d35c09868
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-08-26 08:59:04 +0800

    fix bug

--- golang/tools/ChocolateyBeforeModify.ps1
+++ golang/tools/ChocolateyBeforeModify.ps1
@@ -5,7 +5,5 @@ $InstallationPath = Get-ToolsLocation
 
 $GOROOT = Join-Path $InstallationPath 'go'
 $GOPATH = Join-Path $GOROOT 'opt'
-Get-ChildItem -Path  $GOROOT -Recurse -exclude somefile.txt |
-Select-Object -ExpandProperty FullName |
-Where-Object {$_ -notlike $(Join-Path $GOPATH '*')} |
-Remove-Item -Force | Out-Null 
+Get-ChildItem -Path $GOROOT -exclude 'opt' |
+Remove-Item -Recurse -Force | Out-Null 
