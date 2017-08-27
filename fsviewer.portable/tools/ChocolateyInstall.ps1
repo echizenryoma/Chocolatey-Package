7a1d965c9c8d8cb6fe506548bb13f28d5161bd4b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-08-27 16:33:13 +0800

    add fsviewer.portable

---
+++ fsviewer.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'fsview.portable'
+$Url32 = 'http://www.faststonesoft.net/DN/FSViewer63.zip'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url32
+    Url64         = $Url32
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
---
+++ fsviewer.portable/update.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "http://www.faststone.org/FSViewerDownload.htm"
+    $version = ($page.Content -split "\n|<|>|&" -match "FastStone\s+Image\s+Viewer\s+\d+(\.\d+)+").Trim() -split "\s" -match "^\d+(\.\d+)+$"
+    
+    $url = $page.Links | Where-Object href -Match "FSViewer\d+.zip" | Select-Object -First 1 -ExpandProperty href
+
+    return @{
+        Version = $version
+        URL32   = $url
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
