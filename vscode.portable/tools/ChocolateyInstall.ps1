6f2103526857cae0ed9fd0876811a87fcd2db7f6
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-09-08 14:15:48 +0800

    AU: 1 updated - vscode.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/892d4dacfe9acbd32ddffaefd8917ea2e39be94c

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'vscode.portable'
-$Url = 'https://az764295.vo.msecnd.net/stable/41abd21afdf7424c89319ee7cb0445cc6f376959/VSCode-win32-ia32-1.15.1.zip'
-$Url64 = 'https://az764295.vo.msecnd.net/stable/41abd21afdf7424c89319ee7cb0445cc6f376959/VSCode-win32-x64-1.15.1.zip'
+$Url = 'https://az764295.vo.msecnd.net/stable/787b31c0474e6165390b5a5989c9619e3e16f953/VSCode-win32-ia32-1.16.0.zip'
+$Url64 = 'https://az764295.vo.msecnd.net/stable/787b31c0474e6165390b5a5989c9619e3e16f953/VSCode-win32-x64-1.16.0.zip'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
