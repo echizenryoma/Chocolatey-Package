c2ea7d00b51d0f4ec133258e1cbdb93454b972f4
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-09-28 11:13:19 +0800

    update jdk

--- jdk/tools/ChocolateyInstall.ps1
+++ jdk/tools/ChocolateyInstall.ps1
@@ -1,19 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'jdk'
-$Url32 = 'http://edelivery.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-windows-i586.exe'
-$Checksum32 = 'f2c6657812986aa4b992173da495cdc3620edcdc26927860d7d920958f12575c'
-$ChecksumType32 = 'SHA256'
-$Url64 = 'http://edelivery.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-windows-x64.exe'
-$Checksum64 = 'be9f6e920f817757ce1913c9c3f0a5d63046c720f37a95e4a14450a179f48a18'
+$Url64 = 'http://edelivery.oracle.com/otn-pub/java/jdk/9+181/jdk-9_windows-x64_bin.exe'
+$Checksum64 = 'd4885a5fbd2dce383c579fe4a1569b6d8b5db1574af602e37cadcac781115297'
 $ChecksumType64 = 'SHA256'
-$JavaVersion = '1.8.0_144'
+$JavaVersion = '9'
 
 $PackageArgs = @{
     PackageName    = $PackageName
-    Url            = $Url32
-    Checksum       = $Checksum32
-    ChecksumType   = $ChecksumType32
     Url64          = $Url64
     Checksum64     = $Checksum64
     ChecksumType64 = $ChecksumType64
@@ -27,10 +21,5 @@ $PackageArgs = @{
 }
 Install-ChocolateyPackage @PackageArgs
 
-if ($env:chocolateyForceX86 -ne $True) {
-    $JAVA_HOME = Join-Path $env:ProgramFiles "Java\jdk$JavaVersion"
-}
-else {
-    $JAVA_HOME = Join-Path ${env:ProgramFiles(x86)} "Java\jdk$JavaVersion"
-}
+$JAVA_HOME = Join-Path $env:ProgramFiles "Java\jdk-$JavaVersion"
 Install-ChocolateyEnvironmentVariable -VariableName 'JAVA_HOME' -VariableValue $JAVA_HOME -VariableType 'Machine'
--- jdk/update.ps1
+++ jdk/update.ps1
@@ -3,9 +3,6 @@
 function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
-            "(^[$]Url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(^[$]Checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(^[$]ChecksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
             "(^[$]Url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
             "(^[$]Checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
             "(^[$]ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
@@ -16,24 +13,19 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "http://www.oracle.com/technetwork/articles/javase/index-jsp-138363.html"
-    $url = "http://www.oracle.com/" + ($page.Links | Where-Object outerHTML -Match "Java\s+SE\s+\d+u\d+" | Select-Object -First 1 -ExpandProperty href)
-    $html = $page.Links | Where-Object outerHTML -Match "Java\s+SE\s+\d+u\d+" | Select-Object -First 1
-    $version = ($html -split "\s|<|>" -match "\d+u\d+").Trim() -replace "u", ".0."
-	
-    $java_version = "1." + ($version -split "\." | Select-Object -First 1) + ".0_" + ($version -split "\." | Select-Object -Last 1)
+    $url = "http://www.oracle.com/" + ($page.Links | Where-Object outerHTML -Match "Java\s+SE\s+\d+" | Select-Object -First 1 -ExpandProperty href)
 
     $page = Invoke-WebRequest -UseBasicParsing -Uri $url
-    $html = $page.Content -split "\n|;" -match "jdk-\d+u\d+-windows-i586.exe"
-    $json32 = $html -split "=" | Select-Object -Last 1 | ConvertFrom-Json
 	
-    $html = $page.Content -split "\n|;" -match "jdk-\d+u\d+-windows-x64.exe"
+    $html = $page.Content -split "\n|;" -match "windows-x64"
     $json64 = $html -split "=" | Select-Object -Last 1 | ConvertFrom-Json
+
+    $url64 = $json64.filepath
+    $version = ($url64 -split "/" -match "\d+\+\d+" | Select-Object -First 1) -replace "\+", ".0."
+    $java_version = ($version -split "\." | Select-Object -First 1)
 	
     return @{
         Version        = $version
-        URL32          = $json32.filepath -replace "download.oracle.com", "edelivery.oracle.com"
-        Checksum32     = $json32.SHA256
-        ChecksumType32 = "SHA256"
         URL64          = $json64.filepath -replace "download.oracle.com", "edelivery.oracle.com"
         Checksum64     = $json64.SHA256
         ChecksumType64 = "SHA256"
