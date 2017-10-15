ab7a8c3e705a4ffe03ea6d5bf76bdeb344e70205
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-10-15 11:37:25 +0800

    AU: 1 updated - mkvtoolnix.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/9992cbbce1c3238a8bcd708d7cf15e627266581f

--- mkvtoolnix.portable/tools/ChocolateyInstall.ps1
+++ mkvtoolnix.portable/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'mkvtoolnix.portable'
-$Url32 = 'https://mkvtoolnix.download/windows/releases/16.0.0/mkvtoolnix-32-bit-16.0.0.7z'
-$Url64 = 'https://mkvtoolnix.download/windows/releases/16.0.0/mkvtoolnix-64-bit-16.0.0.7z'
-$Checksum32 = '5fbca01615642b23da9fc0c4b7de77da46669a85'
+$Url32 = 'https://mkvtoolnix.download/windows/releases/17.0.0/mkvtoolnix-32-bit-17.0.0.7z'
+$Url64 = 'https://mkvtoolnix.download/windows/releases/17.0.0/mkvtoolnix-64-bit-17.0.0.7z'
+$Checksum32 = 'd7d7bb4e4fae82c22ed23399e51ce2dd2e04912b'
 $ChecksumType32 = 'sha1'
-$Checksum64 = 'fc309b1fb7c1e898b964f7c1d1d03cb1e4568374'
+$Checksum64 = '560f68b91a39723409f1d5bcd74fbee774e296ee'
 $ChecksumType64 = 'sha1'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
