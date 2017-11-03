79f88cdff7e96bfe7a02215db3111a4a1f59a403
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-11-03 09:17:56 +0800

    remove tesseract

--- tesseract/tools/ChocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$PackageName = 'tesseract'
-
-$EnvPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) -split ';' -notmatch $PackageName
-[Environment]::SetEnvironmentVariable('Path', $EnvPath -join ';', [EnvironmentVariableTarget]::Machine)
-Uninstall-ChocolateyEnvironmentVariable -VariableName 'TESSDATA_PREFIX' -VariableType 'Machine'
