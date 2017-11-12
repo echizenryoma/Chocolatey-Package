e82e909123751469584a4fd81c8c2979852f9852
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-11-12 11:12:05 +0800

    update citra

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-nightly/releases/download/nightly-408/citra-windows-msvc-20171109-f889d6c.7z'
+$Url = 'https://github.com/citra-emu/citra-nightly/releases/download/nightly-409/citra-windows-msvc-20171109-7ef905e.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
