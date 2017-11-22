269c73e3b04a6c1b982b70fd2475d78dc17ca705
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-11-22 16:16:04 +0800

    add ghostscript.portable

---
+++ ghostscript.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'ghostscript.portable'
+$Url32 = 'https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w32.exe'
+$Checksum32 = 'c01b36529c618f9218c39c84bf7168fc2e466a78406f273609f736a5f0cd965a'
+$ChecksumType32 = 'SHA256'
+$Url64 = 'https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs922/gs922w64.exe'
+$Checksum64 = '796cd308d3134d021356c0db1831c089db73d03aac07d28cbe77958163c79d57'
+$ChecksumType64 = 'SHA256'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName    = $PackageName
+    Url            = $Url32
+    Checksum       = $Checksum32
+    ChecksumType   = $ChecksumType32
+    Url64          = $Url64
+    Checksum64     = $Checksum64
+    ChecksumType64 = $ChecksumType64
+    UnzipLocation  = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+Remove-Item -Path $(Join-Path $ToolsPath "uninstgs.exe.nsis") -Force -ea 0
+Remove-Item -Path $(Join-Path $ToolsPath '$PLUGINSDIR') -Recurse -Force -ea 0
---
+++ ghostscript.portable/update.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    [version]$version = $Latest.Version | Select-Object -First 1
+    $docsUrl = "http://www.ghostscript.com/doc/$($version)/Readme.htm"
+    $releaseNotesUrl = "https://ghostscript.com/doc/$($version)/History$($version.Major).htm"
+    
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url32\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^[$]Checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]ChecksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]Url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^[$]Checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(^[$]ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+        "ghostscript.portable.nuspec" = @{
+            "(?i)(^\s*\<docsUrl\>).*(\<\/docsUrl\>)"           = "`${1}$docsUrl`${2}"
+            "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$releFaseNotesUrl`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/latest"
+    $version = ($page.Links | Where-Object outerHTML -match "Ghostscript/GhostPDL" | Select-Object -First 1) -split "\s|<|>|/" -match "\d+(\.\d+)+"
+    
+    $url = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs$($version.Replace(".", ''))/gs$($version.Replace(".", ''))w32.exe"
+    $url64 = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs$($version.Replace(".", ''))/gs$($version.Replace(".", ''))w64.exe"
+
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs$($version.Replace(".", ''))/SHA256SUMS"
+    $objs = [System.Text.Encoding]::UTF8.GetString($page.Content) -split '\n' | ConvertFrom-String -PropertyNames "SHA256SUM", "File"
+  
+    return @{
+        Version        = $version
+        URL32          = $url
+        Checksum32     = ($objs | Where-Object File -Match "gs$($version.Replace(".", ''))w32.exe").SHA256SUM
+        ChecksumType32 = "SHA256"
+        URL64          = $url64
+        Checksum64     = ($objs | Where-Object File -Match "gs$($version.Replace(".", ''))w64.exe").SHA256SUM
+        ChecksumType64 = "SHA256"
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
