e082735a41c8d78759cfb4c7c1f9cd58e75fffca
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-11-24 17:15:29 +0800

    add fiddler

---
+++ fiddler/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'fiddler'
+$Url = 'https://telerik-fiddler.s3.amazonaws.com/fiddler/FiddlerSetup.exe'
+$Checksum = '90650BB4D1801795C80DA27CE28E13707B57642405BCAB9F68C055467AAEF2D2'
+$ChecksumType = 'SHA256'
+
+$packageArgs = @{
+    PackageName    = $PackageName
+    Url            = $Url
+    Checksum       = $Checksum 
+    ChecksumType   = $ChecksumType
+    SilentArgs     = '/S'
+    ValidExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ fiddler/update.ps1
@@ -0,0 +1,31 @@
+Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^[$]Checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $url = 'https://telerik-fiddler.s3.amazonaws.com/fiddler/FiddlerSetup.exe'
+    $file = Join-Path $PSScriptRoot $([IO.Path]::GetFileName($url))
+
+    Write-Host "Downloading full setup file to find the version"
+    Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $file
+    $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file).FileVersion
+    $hash = Get-FileHash $file
+    Remove-Item $file -Force -ea 0
+
+    @{
+        Version        = $version
+        URL32          = $url
+        Checksum32     = $hash.Hash
+        ChecksumType32 = $hash.Algorithm
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
