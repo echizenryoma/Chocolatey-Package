30f125240dc301b69568611b08b47921a8be0bde
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-12-06 10:19:03 +0800

    add windows-repair-toolbox

---
+++ windows-repair-toolbox/tools/ChocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'windows-repair-toolbox'
+$Url = 'https://windows-repair-toolbox.com/files/Windows_Repair_Toolbox.zip'
+$Checksum = '174D3E720DA1B80555949077E83A9DBA7EECAFAFA2319EDE6AB9617AFAF998E9'
+$ChecksumType = 'SHA256'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    Checksum      = $Checksum 
+    ChecksumType  = $ChecksumType
+    UnzipLocation = $InstallationPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+Install-BinFile -Name 'Windows_Repair_Toolbox' -Path $(Join-Path $InstallationPath "Windows_Repair_Toolbox.exe")
---
+++ windows-repair-toolbox/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$PackageName = 'windows-repair-toolbox'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+Remove-Item $InstallationPath -Recurse -Force | Out-Null
+Uninstall-BinFile -Name 'Windows_Repair_Toolbox'
