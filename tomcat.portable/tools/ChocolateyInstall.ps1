51efb4705ecc0aeaa4d27a9abb324a0f3f720356
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-12-07 20:13:59 +0800

    add tomcat.portable

---
+++ tomcat.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'tomcat'
+$Url32 = 'https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-8/v8.5.24/bin/apache-tomcat-8.5.24-windows-x86.zip'
+$Url64 = 'https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-8/v8.5.24/bin/apache-tomcat-8.5.24-windows-x64.zip'
+$InstallationPath = Get-ToolsLocation
+$ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url32
+    Url64         = $Url64
+    UnzipLocation = $InstallationPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$UnzipPath = (Get-ChildItem $InstallationPath -Directory | Where-Object Name -Like "apache-tomcat-*" | Select-Object -First 1).FullName
+cmd /c xcopy $UnzipPath $ToolsPath /s /y /q
+cmd /c rmdir "$UnzipPath" /s /q
---
+++ tomcat.portable/update.ps1
@@ -0,0 +1,33 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    [version]$version = $Latest.Version | Select-Object -First 1
+    $docsUrl = "http://tomcat.apache.org/tomcat-$($version.Major).$($version.Minor)-doc/index.html"
+
+
+    @{
+        ".\tools\ChocolateyInstall.ps1" = @{
+            "(^[$]Url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        }
+        "tomcat.portable.nuspec"        = @{
+            "(?i)(^\s*\<docsUrl\>).*(\<\/docsUrl\>)" = "`${1}$docsUrl`${2}"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri 'http://tomcat.apache.org/'
+    $version = [version](($page.Content -split '\n|<|>|\(|\)' -match "Tomcat\s+\d+(\.\d+)+\s+Released" | Select-Object -First 1).Trim() -split "\s" -match '\d+(\.\d+)+' | Select-Object -First 1)
+
+    $url32 = "https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-$($version.Major)/v${version}/bin/apache-tomcat-${version}-windows-x86.zip"
+    $url64 = "https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-$($version.Major)/v${version}/bin/apache-tomcat-${version}-windows-x64.zip"
+
+    return @{
+        URL32   = $url32
+        URL64   = $url64
+        Version = $version
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
