ff4e97c694dc4fe87158833a437ba54f01c36e86
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-12-10 08:42:49 +0800

    AU: 1 updated - fastcopy.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/b171925f9473818ace4255bb9d91e8c75b740ca2

--- fastcopy.portable/tools/ChocolateyInstall.ps1
+++ fastcopy.portable/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'fastcopy.portable'
-$Url32 = 'https://ipmsg.org/archive/FastCopy332.zip'
-$Url64 = 'https://ipmsg.org/archive/FastCopy332_x64.zip'
+$Url32 = 'https://ipmsg.org/archive/FastCopy340.zip'
+$Url64 = 'https://ipmsg.org/archive/FastCopy340_x64.zip'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
