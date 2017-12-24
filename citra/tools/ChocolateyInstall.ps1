77706e9135701f8a6c30ab9b220d1c47c1b6bff0
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-12-24 22:37:56 +0800

    add citra

---
+++ citra/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'citra'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-291/citra-windows-mingw-20171223-2ca5e92.7z'
+$ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
