021604057fc730f7a6613121d4b329583115ff9f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-12-28 08:00:59 +0800

    AU: 1 updated - rdpwrapper [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/8543961d469d5d9aef60c6206308d3b736dd9fa2

--- rdpwrapper/tools/ChocolateyInstall.ps1
+++ rdpwrapper/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'rdpwrapper'
-$Url = 'https://github.com/stascorp/rdpwrap/releases/download/v1.6.1/RDPWrap-v1.6.1.zip'
+$Url = 'https://github.com/stascorp/rdpwrap/releases/download/v1.6.2/RDPWrap-v1.6.2.zip'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
