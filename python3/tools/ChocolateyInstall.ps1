5ba41e1e918d7f65c4037b03e7b4f865a356eb9a
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-12-29 20:27:15 +0800

    fix bug

--- python3/tools/ChocolateyInstall.ps1
+++ python3/tools/ChocolateyInstall.ps1
@@ -3,27 +3,22 @@
 $PackageName = 'python3'
 $Url32 = 'https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe'
 $Url64 = 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe'
-$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
-$FilePath = Join-Path $ToolsPath $([IO.Path]::GetFileName($Url32))
-$PackageArgs = @{
-    PackageName  = $PackageName
-    Url          = $Url32
-    Url64        = $Url64
-    FileFullPath = $FilePath
-}
-Get-ChocolateyWebFile @PackageArgs
+# This file should be identical for all python3* packages
+# https://docs.python.org/3/using/windows.html#installing-without-ui
+$InstallArgs = '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="{0}"' -f $InstallationPath
 
-$InstallArgs = '/c msiexec /a "{0}" /qn TargetDir="{1}"' -f $FilePath, $InstallationPath
-Start-ChocolateyProcessAsAdmin -ExeToRun 'cmd' -Statements $InstallArgs
-Remove-Item -Path $FilePath -Force -ErrorAction Ignore
+$PackageArgs = @{
+    PackageName = $PackageName
+    SilentArgs  = $InstallArgs
+    Url         = $Url32
+    Url64       = $Url64
+}
+Install-ChocolateyPackage @PackageArgs
 
 $PythonBin = $(Join-Path $InstallationPath 'python.exe')
 $Python3Bin = $(Join-Path $InstallationPath 'python3.exe')
 if (-Not (Test-Path $Python3Bin)) {
     Start-ChocolateyProcessAsAdmin -ExeToRun 'cmd' -Statements "/c mklink /h `"$Python3Bin`" `"$PythonBin`""
 }
-
-Install-ChocolateyPath $InstallationPath 'Machine'
-Install-ChocolateyPath $(Join-Path $InstallationPath "Scripts") 'Machine'
