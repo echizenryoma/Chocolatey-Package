a567477e680df1b4ef6a3ec37a3f34a8a7765bea
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-01 20:52:08 +0800

    AU: 1 updated - notepadplusplus.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/bc79b418dac67b5c5d54aced91a040e6a9785066

--- notepadplusplus.portable/tools/ChocolateyInstall.ps1
+++ notepadplusplus.portable/tools/ChocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'notepadplusplus'
-$Url = 'https://notepad-plus-plus.org/repository/7.x/7.5.3/npp.7.5.3.bin.7z'
-$Checksum = 'ddcdfe363d4539e36e9a0e0acba71ab88c350087'
-$ChecksumType = 'sha1'
+$Url = 'https://notepad-plus-plus.org/repository/7.x/7.5.4/npp.7.5.4.bin.7z'
+$Checksum = '9633920a02980be62273093c4364bd07b8bb64a2'
+$ChecksumType = 'SHA1'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
@@ -15,7 +15,7 @@ $PackageArgs = @{
 }
 Install-ChocolateyZipPackage @PackageArgs
 
-$UrlExtra = 'https://notepad-plus-plus.org/repository/7.x/7.5.3/npp.7.5.3.Installer.exe'
+$UrlExtra = 'https://notepad-plus-plus.org/repository/7.x/7.5.4/npp.7.5.4.Installer.exe'
 $FileName = [IO.Path]::GetFileName($UrlExtra)
 $InstallerPath = Join-Path $InstallationPath $FileName
 $PackageArgs = @{
