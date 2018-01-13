b58fda9fe2bbf0fdf62daf701acaa4800343af5f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-13 09:07:56 +0800

    add xshell

---
+++ xshell/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'xshell'
+$Url = 'https://cdn.netsarang.net/69322a12/Xshell-6.0.0070r_beta.exe'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$SetupIssPath = Join-Path $ToolsPath 'setup.iss'
+(Get-Content $SetupIssPath).replace('[InstallationPath]', $InstallationPath) | Set-Content $SetupIssPath
+
+$PackageArgs = @{
+    PackageName = $PackageName
+    Url         = $Url
+    SilentArgs  = "/S /f1`"$SetupIssPath`""
+}
+Install-ChocolateyPackage @PackageArgs
---
+++ xshell/update.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $request = Invoke-WebRequest -Uri "https://www.netsarang.com/beta/download.php?id=xsh" -MaximumRedirection 0 -ErrorAction Ignore
+    $url = $request.Headers.Location
+    $version = ($url -split "/" | Select-Object -Last 1) -split "-|r" -match "\d+(\.\d+)+" | Select-Object -First 1
+
+    return @{
+        Version = $version
+        URL32   = $url
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
