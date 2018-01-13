a51342ebf5e6127b916f9f8220c376798f06bf68
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-13 22:11:26 +0800

    update vscode.portable

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$PackageName = 'vscode.portable'
-$Url = 'https://vscode.cdn.azure.cn/stable/490ef761b76b3f3b3832eff7a588aac891e5fe80/VSCode-win32-ia32-1.19.2.zip'
-$Url64 = 'https://vscode.cdn.azure.cn/stable/490ef761b76b3f3b3832eff7a588aac891e5fe80/VSCode-win32-x64-1.19.2.zip'
+$PackageName = 'vscode'
+$Url32 = 'https://vscode-update.azurewebsites.net/latest/win32-archive/stable'
+$Url64 = 'https://vscode-update.azurewebsites.net/latest/win32-x64-archive/stable'
 $InstallationPath = Join-Path $(Get-ToolsLocation) 'vscode'
 
 $PackageArgs = @{
     PackageName   = $PackageName
-    Url           = $Url
+    Url           = $Url32
     Url64         = $Url64
     UnzipLocation = $InstallationPath
 }
--- vscode.portable/update.ps1
+++ vscode.portable/update.ps1
@@ -3,31 +3,17 @@
 function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
-            "(^[$]Url\s*=\s*)('.*')"   = "`$1'$($Latest.URL32)'"
-            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $url = "https://vscode-update.azurewebsites.net/latest/win32-archive/stable"
-    $request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
+    $request = Invoke-WebRequest -Uri "https://vscode-update.azurewebsites.net/latest/win32-archive/stable" -MaximumRedirection 0 -ErrorAction Ignore
     $url = $request.Headers.Location
     $version = $url.Substring($url.LastIndexOf("-") + 1).Replace(".zip", "").Trim()
 
-    $url64 = "https://vscode-update.azurewebsites.net/latest/win32-x64-archive/stable"
-    $request = Invoke-WebRequest -Uri $url64 -MaximumRedirection 0 -ErrorAction Ignore
-    $url64 = $request.Headers.Location
-    $version64 = $url.Substring($url64.LastIndexOf("-") + 1).Replace(".zip", "").Trim()
-
-    if ($version -eq $version64) {
-        throw "vesion is not matched."
-    }
-
     return @{
         Version = $version
-        URL32   = $url
-        URL64   = $url64
     }
 }
 
