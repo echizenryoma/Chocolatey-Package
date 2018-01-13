a0857283ff1c99dfab1bb4b3273674954c5d3b82
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-13 13:06:37 +0800

    add postman

---
+++ postman/update.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module AU
+
+$PackageName = "postman"
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $xml = [xml](Get-Content -Path $(Join-Path $PSScriptRoot "${PackageName}.nuspec"))
+    $nupkg_version = $xml.package.metadata.version
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://dl.pstmn.io/update/status?channel=stable&currentVersion=${nupkg_version}&arch=64&platform=win64" -ErrorAction Ignore
+    if ($page.StatusCode -eq 200) {
+        $obj = ConvertFrom-Json $page.Content
+        $version = $obj.version
+    }
+    else {
+        $version = $xml.package.metadata.version
+    }    
+
+    return @{
+        Version = $version
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
