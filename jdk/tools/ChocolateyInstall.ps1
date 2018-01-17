3b1ebd3166054ba89f0be099b68b944d2087ccc1
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-17 08:00:50 +0800

    AU: 2 updated - citra jdk [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/0cefa4ec9dbfdedf83d71fa9d990bf6c507d8fb9

--- jdk/tools/ChocolateyInstall.ps1
+++ jdk/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'jdk'
-$Url64 = 'http://edelivery.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_windows-x64_bin.exe'
-$Checksum64 = '4df5f74fe04c708977e23bdae8842297bce10d550e4a1cbedde9a33af56f4dab'
+$Url64 = 'http://edelivery.oracle.com/otn-pub/java/jdk/9.0.4+11/c2514751926b4512b076cc82f959763f/jdk-9.0.4_windows-x64_bin.exe'
+$Checksum64 = '56c67197a8f2f7723ffb0324191151075cdec0f0891861e36f3fadda28d556c3'
 $ChecksumType64 = 'SHA256'
 $JavaVersion = '9'
 
