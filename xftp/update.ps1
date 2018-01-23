6f851a3df7fb573e0af890aec2b7c07c88da07e7
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-23 21:40:31 +0800

    add xftp

---
+++ xftp/update.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $request = Invoke-WebRequest -Uri "https://www.netsarang.com/beta/download.php?id=xsh" -MaximumRedirection 0 -ErrorAction Ignore
+    $url = $request.Headers.Location
+    $version = ($url -split "/" | Select-Object -Last 1) -split "-|r" -match "\d+(\.\d+)+" | Select-Object -First 1
+
+    return @{
+        Version = $version
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
