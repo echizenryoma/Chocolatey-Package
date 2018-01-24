6c92a1ff368754730d050cfb7cc4b00f2bc9cac3
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-24 08:08:40 +0800

    fix bug

--- tomcat.portable/tools/ChocolateyInstall.ps1
+++ tomcat.portable/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'tomcat'
-$Url32 = 'https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-8/v8.5.27/bin/apache-tomcat-8.5.27-windows-x86.zip'
-$Url64 = 'https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-8/v8.5.27/bin/apache-tomcat-8.5.27-windows-x64.zip'
+$Url32 = 'https://mirrors.ustc.edu.cn/apache/tomcat/tomcat-8/v8.5.27/bin/apache-tomcat-8.5.27-windows-x86.zip'
+$Url64 = 'https://mirrors.ustc.edu.cn/apache/tomcat/tomcat-8/v8.5.27/bin/apache-tomcat-8.5.27-windows-x64.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Get-ToolsLocation
 
@@ -15,6 +15,6 @@ $PackageArgs = @{
 Install-ChocolateyZipPackage @PackageArgs
 
 $null = New-Item -ItemType Directory -Force -Path $InstallationPath -ErrorAction Ignore
-$UnzipPath = (Get-ChildItem $InstallationPath -Directory | Where-Object Name -Like "apache-tomcat-*" | Select-Object -First 1).FullName
+$UnzipPath = (Get-ChildItem $UnzipLocation -Directory | Where-Object Name -Like "apache-tomcat-*" | Select-Object -First 1).FullName
 Start-ChocolateyProcessAsAdmin -ExeToRun 'xcopy' -Statements "`"$UnzipPath`" `"$InstallationPath`" /s /y /q"
 Start-ChocolateyProcessAsAdmin -ExeToRun 'rmdir' -Statements "`"$UnzipPath`" /s /q"
--- tomcat.portable/update.ps1
+++ tomcat.portable/update.ps1
@@ -20,8 +20,8 @@ function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri 'http://tomcat.apache.org/'
     $version = [version](($page.Content -split '\n|<|>|\(|\)' -match "Tomcat\s+\d+(\.\d+)+\s+Released" | Select-Object -First 1).Trim() -split "\s" -match '\d+(\.\d+)+' | Select-Object -First 1)
 
-    $url32 = "https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-$($version.Major)/v${version}/bin/apache-tomcat-${version}-windows-x86.zip"
-    $url64 = "https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-$($version.Major)/v${version}/bin/apache-tomcat-${version}-windows-x64.zip"
+    $url32 = "https://mirrors.ustc.edu.cn/apache/tomcat/tomcat-$($version.Major)/v${version}/bin/apache-tomcat-${version}-windows-x86.zip"
+    $url64 = "https://mirrors.ustc.edu.cn/apache/tomcat/tomcat-$($version.Major)/v${version}/bin/apache-tomcat-${version}-windows-x64.zip"
 
     return @{
         URL32   = $url32
