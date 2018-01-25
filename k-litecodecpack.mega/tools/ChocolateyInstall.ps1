bbb5394e0e896965fe84d80814471c8e6ca9d681
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-01-25 21:35:59 +0800

    add setup.ini

--- k-litecodecpack.mega/tools/ChocolateyInstall.ps1
+++ k-litecodecpack.mega/tools/ChocolateyInstall.ps1
@@ -4,13 +4,16 @@ $PackageName = 'k-litecodecpack.mega'
 $Url32 = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1375_Mega.exe'
 $Checksum32 = 'caf68286d2fb66e7f2b857a2f98fdd0a2083abe822eb1ecb9675cfeeee065f53'
 $ChecksumType32 = 'SHA256'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$SetupIniPath = Join-Path $ToolsPath 'setup.ini'
+$SilentArgs = "/verysilent /norestart /LoadInf=`"$SetupIniPath`""
 
 $PackageArgs = @{
-    InstallerType = 'EXE'
-    PackageName   = $PackageName
-    Url           = $Url32
-    Checksum      = $Checksum32
-    ChecksumType  = $ChecksumType32
-    SilentArgs    = '/VERYSILENT'
+    PackageName  = $PackageName
+    Url          = $Url32
+    Checksum     = $Checksum32
+    ChecksumType = $ChecksumType32
+    SilentArgs   = $SilentArgs
 }
 Install-ChocolateyPackage @PackageArgs
