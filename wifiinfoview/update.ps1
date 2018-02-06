b475c9ce3275da74b77514568d25e75fab3f0ea7
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-02-06 10:54:21 +0800

    add wifiinfoview

---
+++ wifiinfoview/update.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "http://www.nirsoft.net/utils/wifi_information_view.html"
+    $version = ($page -split "\n|<|>" -match "WifiInfoView\s+v\d+\.\d+" | Select-Object -First 1) -split "\s|v" -match "\d+(\.\d+)+" | Select-Object -First 1
+
+    return @{
+        Version = $version
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
