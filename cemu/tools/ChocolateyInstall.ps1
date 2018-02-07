9ea113d1cf3a87969a5a12d472b3278be2fa21cb
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-02-07 15:22:06 +0800

    add cemu

---
+++ cemu/tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'cemu'
+$Url64 = 'http://cemu.info/releases/cemu_1.11.4.zip'
+$UnzipLocation = Get-ToolsLocation
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$PackageArgs = @{
+    PackageName    = $PackageName
+    Url64          = $Url64
+    UnzipLocation  = $UnzipLocation
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$null = New-Item -ItemType Directory -Force -Path $InstallationPath -ErrorAction Ignore
+$UnzipPath = Join-Path $UnzipLocation $([IO.Path]::GetFileNameWithoutExtension($Url64))
+Start-ChocolateyProcessAsAdmin -ExeToRun 'xcopy' -Statements "`"$UnzipPath`" `"$InstallationPath`" /s /y /q"
+Remove-Item -Path $UnzipPath -Recurse -Force -ErrorAction Ignore
---
+++ cemu/update.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "http://cemu.info"
+    $url64 = $page.Links | Where-Object name -eq "download" | Select-Object -First 1 -ExpandProperty href
+    $version = $url64 -split "_|\.zip" -match "\d+(\.\d+)+"
+	
+    return @{
+        Version = $version
+        URL64   = $url64
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
