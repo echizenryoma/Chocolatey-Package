31dc0a51c07a00ae25922ada5ef3e5a412384f0c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-02-12 09:16:09 +0800

    AU: 1 updated - cmder.mini [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/e98fc54c9bc870a17235c4369c9d59cacea26f6c

--- cmder.mini/tools/ChocolateyInstall.ps1
+++ cmder.mini/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'cmder'
-$Url = 'https://github.com/cmderdev/cmder/releases/download/v1.3.4/cmder_mini.zip'
+$Url = 'https://github.com/cmderdev/cmder/releases/download/v1.3.5/cmder_mini.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
