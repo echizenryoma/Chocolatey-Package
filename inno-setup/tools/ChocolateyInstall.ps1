1a39a770961238f326d475f3c359af1c3cc6a07d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-02-13 21:21:46 +0800

    add inno-setup

---
+++ inno-setup/tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'inno-setup'
+$Url32 = 'http://www.jrsoftware.org/download.php/is.exe'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$SetupInfPath = Join-Path $ToolsPath 'setup.inf'
+(Get-Content $SetupInfPath).replace('%InstallationPath%', $InstallationPath) | Set-Content $SetupInfPath
+
+$SilentArgs = "/verysilent /norestart /LoadInf=`"$SetupInfPath`""
+
+$PackageArgs = @{
+    PackageName = $PackageName
+    Url         = $Url32
+    SilentArgs  = $SilentArgs
+}
+Install-ChocolateyPackage @PackageArgs
---
+++ inno-setup/update.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\ChocolateyInstall.ps1" = @{
+            "(^[$]Url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri 'http://www.jrsoftware.org/'
+    $version = ($page.Links -match "\d+(\.\d+)+" | Select-Object -First 1).outerHTML -split '\s|<|>' -match '\d+(\.\d+)+' | Select-Object -First 1
+    $url32 = 'http://www.jrsoftware.org/download.php/is.exe'
+    
+    return @{
+        URL32   = $url32
+        Version = $version
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
