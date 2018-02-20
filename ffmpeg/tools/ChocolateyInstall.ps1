2b0ea4ee87fc2414e7a0b4adf1138aff3ecf8712
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-02-20 09:36:44 +0800

    add ffmpeg

---
+++ ffmpeg/tools/ChocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'ffmpeg'
+$Url32 = 'https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-3.4.1-win32-shared.zip'
+$Url64 = 'https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-3.4.1-win64-shared.zip'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url32
+    Url64         = $Url64
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
---
+++ ffmpeg/update.ps1
@@ -0,0 +1,31 @@
+Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+            "(^[$]Url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $base32 = "https://ffmpeg.zeranoe.com/builds/win32/shared/"
+
+    $page = Invoke-WebRequest -UseBasicParsing -Uri $base32
+    $version = ($page.Links | Where-Object href -match '\d+(\.\d+)+\-win32\-.*\.(zip|7z)$').href -split '-' -match '\d+(\.\d+)+' | Select-Object -Last 1
+
+    $url32 = $base32 + ($page.Links | Where-Object href -match "\-${version}\-").href
+
+    $base64 = $base32 -replace 'win32', 'win64'
+    $page = Invoke-WebRequest -UseBasicParsing -Uri $base64
+    $url64 = $base64 + ($page.Links | Where-Object href -match "\-${version}\-").href
+
+    return @{ 
+        Version = $version
+        URL32   = $url32
+        URL64   = $url64
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
