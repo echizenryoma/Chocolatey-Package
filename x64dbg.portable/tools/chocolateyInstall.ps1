f3e0fb81b6395ea8d65ef436d3bcb5e6eecb931e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-02-22 09:32:41 +0800

    add x64dbg.portable

---
+++ x64dbg.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'x64dbg'
+$Url = 'https://github.com/x64dbg/x64dbg/releases/download/snapshot/snapshot_2018-02-14_22-42.zip'
+$ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    PackageName   = $packageName
+    Url           = $url
+    Url64         = $url
+    UnzipLocation = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ x64dbg.portable/update.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/x64dbg/x64dbg/releases/latest"
+    $url = "https://github.com" + ($page.Links.href -match "snapshot_.*\.zip")
+    $version = ($url -split "\.|_" -match "\d+(-\d+)+$" -Replace "-", "" | Select-Object -First 2) -join "."
+	
+    return @{
+        Version = $version
+        URL32   = $url
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
