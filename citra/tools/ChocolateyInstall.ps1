f219f84f922caa0b29e553ff0017d218361fa513
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-03-12 19:12:28 +0800

    add gpg4win-light.portable

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-393/citra-windows-mingw-20180306-016d688.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-412/citra-windows-mingw-20180311-fb8c878.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
---
+++ gpg4win-light.portable/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿$PackageName = 'gpg'
+
+$EnvPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) -split ';' -notmatch "\\$PackageName\\"
+[Environment]::SetEnvironmentVariable('Path', $EnvPath -join ';', [EnvironmentVariableTarget]::Machine)
