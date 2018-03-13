3a785df8591111b3fc02b95f1fb8020f9ef4c6c8
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-03-13 22:10:42 +0800

    Update

--- gnupg.portable/tools/ChocolateyUninstall.ps1
+++ gnupg.portable/tools/ChocolateyUninstall.ps1
@@ -1,4 +1,6 @@
 ﻿$PackageName = 'gpg'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
 
 $EnvPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) -split ';' -notmatch "\\$PackageName\\"
 [Environment]::SetEnvironmentVariable('Path', $EnvPath -join ';', [EnvironmentVariableTarget]::Machine)
