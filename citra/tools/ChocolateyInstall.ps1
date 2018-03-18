0815b6c6552e5d88c2df0580f3dcab3272d212a3
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-03-18 19:04:53 +0800

    Update

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-416/citra-windows-mingw-20180316-04273de.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-420/citra-windows-mingw-20180317-b53efdc.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
