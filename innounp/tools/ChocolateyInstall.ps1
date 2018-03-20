f2ae87e29f04df4e05f953cdc23924ffd1b929f5
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-03-20 11:06:00 +0800

    fix bug

--- innounp/tools/ChocolateyInstall.ps1
+++ innounp/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'innounp'
-$Url = 'https://sourceforge.net/projects/innounp/files/innounp/innounp%200.46/innounp0.46.rar'
+$Url = 'https://sourceforge.net/projects/innounp/files/innounp/innounp%200.46/innounp046.rar/download'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
