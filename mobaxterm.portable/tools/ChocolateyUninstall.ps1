ef371e41daee9b8e1b0c4df09d126d5907cc8eb7
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-04-02 17:32:12 +0800

    remove mobaxterm.portable

--- mobaxterm.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$PackageName = 'mobaxterm'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "MobaXterm.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
