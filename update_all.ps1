37ebc06f4262cd223dd49916373eb5cecb292e37
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-04-07 08:44:53 +0800

    Update update_all.ps1

--- update_all.ps1
+++ update_all.ps1
@@ -5,40 +5,40 @@ param([string[]] $Name, [string] $ForcedPackages, [string] $Root = $PSScriptRoot
 if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
-    Timeout       = 60
-    UpdateTimeout = 600
-    Threads       = 10
-    Push          = $Env:au_Push -eq 'true'
+    Timeout        = 60
+    UpdateTimeout  = 600
+    Threads        = 10
+    Push           = $Env:au_Push -eq 'true'
 	
-    History       = @{
+    History        = @{
         Lines           = 30
         Github_UserRepo = $Env:github_user_repo
         Path            = $Env:history_path
     }
 	
-    Gist          = @{
+    Gist           = @{
         Id     = $Env:gist_id
         ApiKey = $Env:github_api_key
         Path   = @($Env:history_path)
     }
 	
-    Git           = @{
+    Git            = @{
         Password = $Env:github_api_key
         Force    = $true
     }
 
     ForcedPackages = $ForcedPackages -split ' '
 
-    BeforeEach    = {
+    BeforeEach     = {
         param($PackageName, $Options )
 
         $pattern = "^${PackageName}(?:\\(?<stream>[^:]+))?(?:\:(?<version>.+))?$"
-        $p = $Options.ForcedPackages | ? { $_ -match $pattern }
+        $p = $Options.ForcedPackages | Where-Object { $_ -match $pattern }
         if (!$p) { return }
 
-        $global:au_Force         = $true
+        $global:au_Force = $true
         $global:au_IncludeStream = $Matches['stream']
-        $global:au_Version       = $Matches['version']
+        $global:au_Version = $Matches['version']
     }
 }
 
