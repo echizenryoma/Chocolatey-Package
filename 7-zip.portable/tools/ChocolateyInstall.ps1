fa96174f1afec013aae4b818f0eaffac79733851
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-05-01 15:28:09 +0800

    AU: 2 updated - 7-zip.portable python2.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/8bf28a9b6842ba031e29a6bb349c19c67b166c40

--- 7-zip.portable/tools/ChocolateyInstall.ps1
+++ 7-zip.portable/tools/ChocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = '7-zip'
-$Url32 = 'http://www.7-zip.org/a/7z1801.exe'
-$Url64 = 'http://www.7-zip.org/a/7z1801-x64.exe'
-$UrlExtra = 'http://www.7-zip.org/a/7z1801-extra.7z'
+$Url32 = 'http://www.7-zip.org/a/7z1805.exe'
+$Url64 = 'http://www.7-zip.org/a/7z1805-x64.exe'
+$UrlExtra = 'http://www.7-zip.org/a/7z1805-extra.7z'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
