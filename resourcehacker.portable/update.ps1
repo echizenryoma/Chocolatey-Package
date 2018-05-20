3a4fb9930cb670562e2ff6014825d73a48090be7
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-05-20 20:46:31 +0800

    disable update

--- resourcehacker.portable/update.ps1
+++ resourcehacker.portable/update.ps1
@@ -9,10 +9,6 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_AfterUpdate ($Package) {
-    $global:Options.Push = $true
-}
-
 function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "http://www.angusj.com/resourcehacker"
     $version = ($page.Content -split "\n|<|>" -match "^Version\s+\d+(\.\d+)+$" | Select-Object -First 1) -split "\s" -match "\d+(\.\d+)+" | Select-Object -First 1
