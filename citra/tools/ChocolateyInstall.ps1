7626dbf3f02a28d62687f694317c8b25c0c9cf35
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-06-02 08:00:51 +0800

    AU: 3 updated - cemu citra pkhex [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/bef8de52d25b42682a9367a5a4609694f58c83a5

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-521/citra-windows-mingw-20180531-88eace0.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-532/citra-windows-mingw-20180601-a5a950f.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
