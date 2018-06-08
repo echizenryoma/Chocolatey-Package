227ef093e0ad5efba0332f76fa30e4c997bd90c4
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-06-08 08:01:11 +0800

    AU: 2 updated - citra golang [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/3674ddbd59dae7298407a53784517acbb01e003e

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-535/citra-windows-mingw-20180603-d69147b.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-537/citra-windows-mingw-20180607-ae140ed.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
