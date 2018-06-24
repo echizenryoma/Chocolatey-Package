40e3009a3f58a999d9d669fef1f87f62a5333690
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-06-24 10:08:24 +0800

    Update picpick.portable

--- picpick.portable/tools/ChocolateyInstall.ps1
+++ picpick.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'picpick'
-$Url32 = 'https://picpick.app/download/latest/picpick_portable.zip'
+$Url32 = 'https://www.picpick.org/releases/latest/picpick_portable.zip'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
