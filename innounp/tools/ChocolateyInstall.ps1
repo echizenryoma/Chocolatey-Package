4700125682d6ba1d0f68dd1949f248cb19b48f18
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-07-03 11:27:03 +0800

    AU: 1 updated - innounp [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/626d839d0bd8274fac0d5086b97cb47c538e3995

--- innounp/tools/ChocolateyInstall.ps1
+++ innounp/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'innounp'
-$Url = 'https://sourceforge.net/projects/innounp/files/innounp/innounp%200.46/innounp046.rar/download'
+$Url = 'https://sourceforge.net/projects/innounp/files/innounp/innounp%200.47/innounp0.47.rar'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
