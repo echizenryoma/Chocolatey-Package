4ba95237472a8331c149b0c63677af3639d3137f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-07-13 09:52:08 +0800

    Remove qemu.portable

--- qemu.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$PackageName = 'qemu'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-
-$EnvPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) -split ';' -ne $InstallationPath
-[Environment]::SetEnvironmentVariable('Path', $EnvPath -join ';', [EnvironmentVariableTarget]::Machine)
