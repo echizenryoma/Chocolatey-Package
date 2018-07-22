548cd5172d0c400cb53befb098b0ac7f67a5e8f6
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-07-22 15:09:08 +0800

    AU: 1 updated - fastcopy.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/47fb692f775a57725f94dd4a2d5b390fea964504

--- fastcopy.portable/tools/ChocolateyInstall.ps1
+++ fastcopy.portable/tools/ChocolateyInstall.ps1
@@ -1,15 +1,28 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'fastcopy'
-$Url32 = 'https://ipmsg.org/archive/FastCopy352.zip'
-$Url64 = 'https://ipmsg.org/archive/FastCopy352_x64.zip'
+$Url32 = 'https://fastcopy.jp/archive/FastCopy352_installer.zip'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Get-ChildItem -Directory $ToolsPath | Remove-Item -Recurse -Force -ErrorAction Ignore
 
 $PackageArgs = @{
     PackageName   = $PackageName
     Url           = $Url32
-    Url64         = $Url64
+    Url64         = $Url32
     UnzipLocation = $ToolsPath
 }
 Install-ChocolateyZipPackage @PackageArgs
+
+$InstallerPath = Get-ChildItem -Path $ToolsPath -Filter *.exe -File
+$SilentArgs = "/DIR=`"${ToolsPath}`" /EXTRACT"
+
+$Url32 = $InstallerPath
+$PackageArgs = @{
+    PackageName  = $PackageName
+    Url          = $Url32
+    Url64        = $Url32
+    SilentArgs   = $SilentArgs
+}
+Install-ChocolateyPackage @PackageArgs
 Get-ChildItem $ToolsPath -File -Include "setup.exe" -Recurse | ForEach-Object { $null = New-Item "$($_.FullName).ignore" -Type File -Force }
--- fastcopy.portable/update.ps1
+++ fastcopy.portable/update.ps1
@@ -4,21 +4,19 @@ function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
             "(^[$]Url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://ipmsg.org/tools/fastcopy.html.en"
+    $base = "https://fastcopy.jp"
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "${base}/en"
+    $url = $base + ($page.Links.href -match "zip" | Select-Object -First 1)
     $version = ($page.Content -split "\n|<|>" -match "Download\s+v\d+(\.\d+)+") -split "\s|v" -match "^\d+(\.\d+)+$"
-    $url32 = "https://ipmsg.org/archive/FastCopy$($version.Replace(".", '')).zip"
-    $url64 = "https://ipmsg.org/archive/FastCopy$($version.Replace(".", ''))_x64.zip"
 		
     return @{
         Version = $version
-        URL32   = $url32
-        URL64   = $url64
+        URL32   = $url
     }
 }
 
