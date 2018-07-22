5080486e5a16b073f2ea2533b3b2dcfece9bca32
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-07-22 15:21:04 +0800

    Fix bug

--- fastcopy.portable/tools/ChocolateyInstall.ps1
+++ fastcopy.portable/tools/ChocolateyInstall.ps1
@@ -9,20 +9,20 @@ Get-ChildItem -Directory $ToolsPath | Remove-Item -Recurse -Force -ErrorAction I
 $PackageArgs = @{
     PackageName   = $PackageName
     Url           = $Url32
-    Url64         = $Url32
     UnzipLocation = $ToolsPath
 }
 Install-ChocolateyZipPackage @PackageArgs
 
-$InstallerPath = Get-ChildItem -Path $ToolsPath -Filter *.exe -File
+$InstallerPath = (Get-ChildItem -Path $ToolsPath -Filter "*_installer.exe" -File | Select-Object -Last 1).FullName
 $SilentArgs = "/DIR=`"${ToolsPath}`" /EXTRACT"
 
 $Url32 = $InstallerPath
 $PackageArgs = @{
     PackageName  = $PackageName
     Url          = $Url32
-    Url64        = $Url32
     SilentArgs   = $SilentArgs
 }
 Install-ChocolateyPackage @PackageArgs
+Remove-Item -Path $InstallerPath -Force
+
 Get-ChildItem $ToolsPath -File -Include "setup.exe" -Recurse | ForEach-Object { $null = New-Item "$($_.FullName).ignore" -Type File -Force }
