b773987ddad0e858f3f03d8caff7d4a623b50229
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-09-14 10:05:04 +0800

    Update brotli

--- brotli/tools/ChocolateyInstall.ps1
+++ brotli/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $PackageName = 'brotli'
 $Url = 'https://github.com/google/brotli/releases/download/v1.0.4/brotli-v1.0.4-win_i686.zip'
-$Url64 = 'https://github.com/google/brotli/releases/download/v1.0.4/brotli_dll-v1.0.4-win_x86_64.zip'
+$Url64 = 'https://github.com/google/brotli/releases/download/v1.0.4/brotli-v1.0.4-win_x86_64.zip'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
