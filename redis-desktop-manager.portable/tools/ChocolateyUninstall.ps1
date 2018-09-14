1a3513f87072876b62f6c70dcefbc17853cfd79c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-09-14 09:43:41 +0800

    Remove redis-desktop-manager.portable

--- redis-desktop-manager.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$PackageName = 'redis-desktop-manager'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-Uninstall-BinFile -Name 'rdm'
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "Redis Desktop Manager.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
