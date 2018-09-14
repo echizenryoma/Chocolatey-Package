c1211bcec9a05b8995a50cc0156aed62b679f404
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-09-14 09:43:18 +0800

    Add brotli

---
+++ brotli/tools/ChocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'brotli'
+$Url = 'https://github.com/google/brotli/releases/download/v1.0.4/brotli-v1.0.4-win_i686.zip'
+$Url64 = 'https://github.com/google/brotli/releases/download/v1.0.4/brotli_dll-v1.0.4-win_x86_64.zip'
+$ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    Url64         = $Url64
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
---
+++ brotli/update.ps1
@@ -0,0 +1,31 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $base = "https://github.com"
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "$base/google/brotli/releases/latest"
+    $version = (($page.Links.href -match "releases/tag/v\d+(\.\d+)+$" | Select-Object -Unique -First 1) -split "/" -match "\d+(\.\d+)+" | Select-Object -First 1).Replace("v", "")
+
+    $url = $base + ($page.Links.href -match "brotli.*win_i686\.zip" | Select-Object -First 1)
+    $url64 = $base + ($page.Links.href -match "brotli.*win_x86_64\.zip" | Select-Object -First 1)
+
+    if (($url -eq $base) -and ($url64 -eq $base)) {
+        $version = $Latest.NuspecVersion
+    }
+	
+    return @{
+        Version = $version
+        URL32   = $url
+        URL64   = $url64
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
