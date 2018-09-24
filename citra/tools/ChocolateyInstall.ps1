446280925dc740c8e12e9e9a4565c507f458aa3b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-09-24 08:58:18 +0800

    AU: 2 updated - citra imagemagick.tool [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/7388e752ed0e73844781cbfc43411986e66f5e6f

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-866/citra-windows-mingw-20180921-b0d9df5.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-877/citra-windows-mingw-20180923-5a151fc.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- imagemagick.tool/tools/ChocolateyInstall.ps1
+++ imagemagick.tool/tools/ChocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'imagemagick.tool'
-$Url32 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-11-portable-Q16-x86.zip'
-$Url64 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-11-portable-Q16-x64.zip'
-$Checksum32 = '714ce94006635f5bfb97e46eedb57593c40333663c926342436a4e4c512ea4f0'
-$Checksum64 = '8f21b4259c20121f534611522ffd74dda02b7c1dba95c4020b5382a53fc01472'
+$Url32 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-12-portable-Q16-x86.zip'
+$Url64 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-12-portable-Q16-x64.zip'
+$Checksum32 = 'c4d4b0d976691f9b4c8a4546ec4cb0cd69f218be75c946a5c9a18dbb60919b0f'
+$Checksum64 = '7684dc6a777c9618ed2cf2abefcb1145a69956f9f0b80bc7ec142af27b58a617'
 $ChecksumType32 = 'sha256'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
