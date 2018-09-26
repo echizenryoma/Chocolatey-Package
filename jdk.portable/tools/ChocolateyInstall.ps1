d0fac2d1073e4b1527a46c3c062074a555c24974
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-09-26 10:02:42 +0800

    Update jdk-11

--- jdk.portable/tools/ChocolateyInstall.ps1
+++ jdk.portable/tools/ChocolateyInstall.ps1
@@ -4,17 +4,16 @@ $PackageName = 'jdk'
 $Url64 = 'http://edelivery.oracle.com/otn-pub/java/jdk/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_windows-x64_bin.zip'
 $Checksum64 = 'd64b9d725f0ed096ae839ef8506deb3b6f509b2e3ee1f9b0792c5116623d4c9d'
 $ChecksumType64 = 'sha256'
-$Version = '11.0.0'
-$InstallationPath = Join-Path $(Get-ToolsLocation) "jdk-$Version"
-$UnzipPath = $(Join-Path $InstallationPath "tmp")
+$FileName = (([IO.Path]::GetFileNameWithoutExtension($Url64)) -split "_")[0]
+$InstallationPath = Join-Path $(Get-ToolsLocation) $FileName
 
 $PackageArgs = @{
     PackageName    = $PackageName
     Url64          = $Url64
     Checksum64     = $Checksum64
     ChecksumType64 = $ChecksumType64
-    UnzipLocation  = $UnzipPath
-    FileType       = 'exe'
+    UnzipLocation  = $(Get-ToolsLocation)
+    FileType       = ([IO.Path]::GetExtension($Url64)) -replace "\.", ""
     Options        = @{
         Headers = @{
             Cookie = 'oraclelicense=accept-securebackup-cookie'
@@ -22,23 +21,6 @@ $PackageArgs = @{
     }
 }
 Install-ChocolateyZipPackage @PackageArgs
-
-$JdkToolsPath = (Get-ChildItem -Path $UnzipPath -File -Recurse | Sort-Object Length -Descending | Select-Object -First 1).FullName
-$PackageArgs = @{
-    PackageName   = $PackageName
-    Url64         = $JdkToolsPath
-    UnzipLocation = $InstallationPath
-}
-Install-ChocolateyZipPackage @PackageArgs
-
-Remove-Item -Path $UnzipPath -Recurse -Force -ErrorAction Ignore
-
-$UnpackFiles = Get-ChildItem -Path $InstallationPath -File -Filter "*.pack" -Recurse
-ForEach ($UnpackFile in $UnpackFiles) {
-    $src = $UnpackFile.FullName
-    $dst = Join-Path $UnpackFile.Directory.FullName $("{0}.{1}" -f $UnpackFile.BaseName, "jar")
-    & "$InstallationPath\bin\unpack200.exe" -r -v "$src" "$dst"
-}
 
 $JAVA_HOME = $InstallationPath
 Install-ChocolateyEnvironmentVariable -VariableName 'JAVA_HOME' -VariableValue $JAVA_HOME -VariableType 'Machine'
