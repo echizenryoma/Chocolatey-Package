d848f30b4de75a1c8be7bc7b53d60ca42218c33b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-10-05 12:36:30 +0800

    Add otf-xits

---
+++ otf-xits/update.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/alif-type/xits/releases/latest"
+    $version = ($page.Links.href -match "releases/tag/v\d+(\.\d+)+$" | Select-Object -Unique -First 1) -split "/|v" -match "\d+(\.\d+)+" | Select-Object -First 1
+    $url32 = "https://github.com/alif-type/xits/releases/download/v${version}/XITS-${version}.zip"
+    
+    return @{
+        Version = $version
+        URL32   = $url32
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
