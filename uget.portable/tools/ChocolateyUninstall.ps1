2277c55a642414d7f73ee6778a3832cc841c9039
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-10-07 20:13:45 +0800

    Add uget.portable

---
+++ uget.portable/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿$PackageName = 'uget'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
+Uninstall-BinFile -Name $PackageName
+
+$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "uGet.lnk"
+Remove-Item $LinkPath -Force -ErrorAction Ignore
