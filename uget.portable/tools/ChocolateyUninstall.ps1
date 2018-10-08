b60ed7fa0ab51e672d08050ccfd3ce96da601c11
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-10-09 07:58:03 +0800

    Remove official package

--- uget.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$PackageName = 'uget'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-Uninstall-BinFile -Name $PackageName
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "uGet.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
