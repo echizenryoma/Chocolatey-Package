073c61d641fb808c22a236f6df1b2792458fd33a
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-10-09 10:04:33 +0800

    [7-zip.portable]: add start menu

--- 7-zip.portable/tools/ChocolateyInstall.ps1
+++ 7-zip.portable/tools/ChocolateyInstall.ps1
@@ -8,7 +8,7 @@ $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
     PackageName   = $PackageName
-    Url           = $Url32
+    Url32         = $Url32
     Url64         = $Url64
     UnzipLocation = $InstallationPath
 }
@@ -17,9 +17,14 @@ Install-ChocolateyZipPackage @PackageArgs
 $PackageArgs = @{
     PackageName   = $PackageName
     Url           = $UrlExtra
-    Url64         = $UrlExtra
     UnzipLocation = $InstallationPath
 }
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyZipPackage @PackageArgs
 
-Install-BinFile -Path $(Join-Path $InstallationPath "7z.exe") -Name "7z"
+$BinFileName = Join-Path $InstallationPath "7z.exe"
+Install-BinFile -Path $BinPath -Name $([IO.Path]::GetFileName($BinFileName))
+
+$SystemStartMenuPath = [IO.Path]::Combine($([Environment]::GetFolderPath('CommonApplicationData')), "Microsoft", "Windows", "Start Menu", "Programs")
+$BinFileName = Join-Path $InstallationPath "7zFM.exe"
+$LinkPath = Join-Path $SystemStartMenuPath "7-Zip File Manager.lnk"
+Install-ChocolateyShortcut -ShortcutFilePath $LinkPath -TargetPath $BinFileName -WorkingDirectory $InstallationPath
--- 7-zip.portable/tools/ChocolateyUninstall.ps1
+++ 7-zip.portable/tools/ChocolateyUninstall.ps1
@@ -3,3 +3,7 @@ $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
 Uninstall-BinFile -Name '7z'
+
+$SystemStartMenuPath = [IO.Path]::Combine($([Environment]::GetFolderPath('CommonApplicationData')), "Microsoft", "Windows", "Start Menu", "Programs")
+$LinkPath = Join-Path $SystemStartMenuPath "7-Zip File Manager.lnk"
+Remove-Item -Path $LinkPath -Force -ErrorAction Ignore
