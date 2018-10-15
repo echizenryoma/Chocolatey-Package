27dbfa4d34814dd3438760837f98545a6e65f26a
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-10-15 00:04:00 +0000

    AU: 2 updated - citra notepadplusplus.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/bf26fb9d032445d9189d6ab00670d85dba53f8ab

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-962/citra-windows-mingw-20181013-740c748.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-965/citra-windows-mingw-20181014-c8dcd59.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
