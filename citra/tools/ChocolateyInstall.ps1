6ab91dcc54a63f56722f2ce09730164108e54b2f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-10-17 00:04:36 +0000

    AU: 4 updated - citra jdk8.portable wifiinfoview x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/9f27ffa8ec94481090e9aa0c4e3429f647b071fb

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-969/citra-windows-mingw-20181015-eec6d62.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-973/citra-windows-mingw-20181016-458f121.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
