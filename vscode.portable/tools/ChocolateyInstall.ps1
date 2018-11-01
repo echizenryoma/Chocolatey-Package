1af97f343e73299c1ed9a589d64395af67afa1e6
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-11-01 09:33:51 +0800

    [vscode.portable] Update update.ps1

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -2,17 +2,25 @@
 
 $PackageName = 'vscode'
 $Url32 = 'https://vscode-update.azurewebsites.net/latest/win32-archive/stable'
+$ChecksumType32 = 'sha256'
+$Checksum32 = '3a2b38b8ccfe776ed78d497ad04d0d972d7ffc21db89c4d74f518498756fcd97'
 $Url64 = 'https://vscode-update.azurewebsites.net/latest/win32-x64-archive/stable'
+$ChecksumType64 = 'sha256'
+$Checksum64 = 'ed2ce139ead23e90ee7eb9e6a679ac2fb1e43f087248d2d97793dc1a030e2809'
 $InstallationPath = Join-Path $(Get-ToolsLocation) 'vscode'
 
 Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
 New-Item -ItemType Directory -Path $InstallationPath -Force -ErrorAction Ignore
 
 $PackageArgs = @{
-    PackageName   = $PackageName
-    Url           = $Url32
-    Url64         = $Url64
-    UnzipLocation = $InstallationPath
+    PackageName    = $PackageName
+    Url32          = $Url32
+    ChecksumType32 = $ChecksumType32
+    Checksum32     = $Checksum32
+    Url64          = $Url64
+    ChecksumType64 = $ChecksumType64
+    Checksum64     = $Checksum64
+    UnzipLocation  = $InstallationPath
 }
 Install-ChocolateyZipPackage @PackageArgs
 
--- vscode.portable/update.ps1
+++ vscode.portable/update.ps1
@@ -3,21 +3,35 @@
 function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Checksum32\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]ChecksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]Checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(^[$]ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
 
-function global:au_AfterUpdate ($Package)  {
+function global:au_AfterUpdate ($Package) {
     $global:Options.Push = $true
 }
 
 function global:au_GetLatest {
-    $request = Invoke-WebRequest -Uri "https://vscode-update.azurewebsites.net/latest/win32-archive/stable" -MaximumRedirection 0 -ErrorAction Ignore
-    $url = $request.Headers.Location
-    $version = $([IO.Path]::GetFileNameWithoutExtension($url)) -split "-" -match "\d+(\.\d+)+" | Select-Object -First 1
+    $url32 = 'https://update.code.visualstudio.com/api/update/win32-archive/stable/VERSION'
+    $url64 = 'https://update.code.visualstudio.com/api/update/win32-x64-archive/stable/VERSION'
+
+    $json32 = Invoke-WebRequest -UseBasicParsing -Uri $url32 | ConvertFrom-Json
+    $json64 = Invoke-WebRequest -UseBasicParsing -Uri $url64 | ConvertFrom-Json
+
+    if ($json32.productVersion -ne $json64.productVersion) {
+        throw "Different versions for 32-Bit and 64-Bit detected."
+    }
 
     return @{
-        Version = $version
+        Version        = $json64.productVersion
+        ChecksumType32 = 'sha256'
+        Checksum32     = $json32.sha256hash
+        ChecksumType64 = 'sha256'
+        Checksum64     = $json64.sha256hash
     }
 }
 
