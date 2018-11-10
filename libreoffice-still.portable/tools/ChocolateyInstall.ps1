cfcff944b569f8bb495d02ed6ad6793561d5322f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-11-10 09:30:36 +0800

    [libreoffice-still.portable] Add

---
+++ libreoffice-still.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'LibreOffice'
+$Url32 = 'https://mirrors.ustc.edu.cn/tdf/libreoffice/stable/6.0.7/win/x86/LibreOffice_6.0.7_Win_x86.msi'
+$Url64 = 'https://mirrors.ustc.edu.cn/tdf/libreoffice/stable/6.0.7/win/x86_64/LibreOffice_6.0.7_Win_x64.msi'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$FilePath = Join-Path $ToolsPath $([IO.Path]::GetFileName($Url32))
+$PackageArgs = @{
+    PackageName  = $PackageName
+    Url32        = $Url32
+    Url64        = $Url64
+    FileFullPath = $FilePath
+}
+Get-ChocolateyWebFile @PackageArgs
+
+$InstallArgs = '/a "{0}" /qn TargetDir="{1}"' -f $FilePath, $InstallationPath
+Start-ChocolateyProcessAsAdmin -ExeToRun 'msiexec' -Statements $InstallArgs
+Remove-Item -Path $FilePath -Force -ErrorAction Ignore
---
+++ libreoffice-still.portable/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿$PackageName = 'LibreOffice'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
