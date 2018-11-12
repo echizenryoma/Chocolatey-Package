8edb1e3cb4c45c5a048fc3f18cf6a741c9e339b0
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-11-12 09:56:44 +0800

    [qbittorrent.portable] Add

---
+++ qbittorrent.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'qbittorrent'
+$Url32 = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.1.3/qbittorrent_4.1.3_setup.exe/download'
+$Url64 = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.1.3/qbittorrent_4.1.3_x64_setup.exe/download'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url32
+    Url64         = $Url64
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+Remove-Item -Path $(Join-Path $ToolsPath "uninst.exe") -Force -ErrorAction Ignore
+Remove-Item -Path $(Join-Path $ToolsPath '$PLUGINSDIR') -Recurse -Force -ErrorAction Ignore
