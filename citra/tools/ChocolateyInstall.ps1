031a92141d2069df69b2e5ea915b93d6c61ea560
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-11-14 00:06:33 +0000

    AU: 3 updated - citra notepadplusplus.portable x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/f8e1cb16ce5dc671823a4167fd073f1f85dbed11

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1046/citra-windows-mingw-20181112-c35a8b2.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1049/citra-windows-mingw-20181113-3ca5151.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- x64dbg.portable/tools/ChocolateyInstall.ps1
+++ x64dbg.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'x64dbg'
-$Url = 'https://github.com/x64dbg/x64dbg/releases/download/snapshot/snapshot_2018-11-04_23-45.zip'
+$Url = 'https://github.com/x64dbg/x64dbg/releases/download/snapshot/snapshot_2018-11-13_12-19.zip'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
