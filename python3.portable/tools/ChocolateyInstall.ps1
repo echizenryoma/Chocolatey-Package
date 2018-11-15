6445742773065951d212414d569829ed6402a2e5
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-11-15 15:42:01 +0800

    [python3.portable] Remove launcher and tcl/tk

--- python3.portable/tools/ChocolateyInstall.ps1
+++ python3.portable/tools/ChocolateyInstall.ps1
@@ -7,7 +7,7 @@ $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 # This file should be identical for all python3* packages
 # https://docs.python.org/3/using/windows.html#installing-without-ui
-$InstallArgs = '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="{0}"' -f $InstallationPath
+$InstallArgs = '/quiet InstallAllUsers=1 PrependPath=1 AssociateFiles=0 Include_launcher=0 Include_tcltk=0 TargetDir="{0}"' -f $InstallationPath
 
 $PackageArgs = @{
     PackageName = $PackageName
