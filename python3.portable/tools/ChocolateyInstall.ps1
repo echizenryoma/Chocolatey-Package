ece752333d6e1032211d96af2ad041e7da9a9461
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-11-15 16:29:40 +0800

    [python.portable] Update InstallArgs

--- python3.portable/tools/ChocolateyInstall.ps1
+++ python3.portable/tools/ChocolateyInstall.ps1
@@ -7,13 +7,13 @@ $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 # This file should be identical for all python3* packages
 # https://docs.python.org/3/using/windows.html#installing-without-ui
-$InstallArgs = '/quiet InstallAllUsers=1 PrependPath=1 AssociateFiles=0 Include_launcher=0 Include_tcltk=0 TargetDir="{0}"' -f $InstallationPath
+$InstallArgs = '/quiet InstallAllUsers=1 PrependPath=1 AssociateFiles=0 Include_launcher=0 Include_doc=0 Include_tcltk=0 TargetDir="{0}"' -f $InstallationPath
 
 $PackageArgs = @{
     PackageName = $PackageName
-    SilentArgs  = $InstallArgs
     Url         = $Url32
     Url64       = $Url64
+    SilentArgs  = $InstallArgs
 }
 Install-ChocolateyPackage @PackageArgs
 
