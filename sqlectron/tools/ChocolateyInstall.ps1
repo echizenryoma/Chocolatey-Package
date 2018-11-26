40e0c6a4ab0013b0bb32820c3e09c56f262ca51e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-11-26 16:24:36 +0800

    [sqlectron] Add

---
+++ sqlectron/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'sqlitebrowser'
+$Url = 'https://github.com/sqlectron/sqlectron-gui/releases/download/v1.30.0/Sqlectron-1.30.0-win.7z'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
