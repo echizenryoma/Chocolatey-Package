2fd3a541fac7fa1d9816fb4563ab019d89e639a8
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-12-09 08:50:27 +0800

    [cemu] Add

---
+++ cemu/update.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "http://cemu.info"
+    $url64 = $page.Links | Where-Object name -eq "download" | Select-Object -First 1 -ExpandProperty href
+    $version = ($url64 -split "_|\.zip" -match "\d+(\.\d+)+")[0]
+    $rev = ([byte][char]$version[$version.Length - 1]) - [byte][char]'a' + 1
+    $version = $version -replace "[a-z]", ".$rev"
+	
+    return @{
+        Version = $version
+        URL64   = $url64
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
