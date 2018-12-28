6d6950d7a10892ff3a598cc4f7aa9f6ae7dd07ff
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2018-12-28 20:18:07 +0800

    [imagemagick.tool] Repack

--- imagemagick.tool/tools/ChocolateyInstall.ps1
+++ imagemagick.tool/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $PackageName = 'imagemagick.tool'
 $Url32 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-21-portable-Q16-x86.zip'
 $Url64 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-21-portable-Q16-x64.zip'
-$Checksum32 = '04fd08471932135221485f2c4889053c869cd344c601e2c3a77cf0329370e4ed'
-$Checksum64 = 'c52a3ebb5b21433360249af22f00b18b5fe645bd35c1ff60282d68a5a58c7c74'
+$Checksum32 = '1e655c63c8ec29c3a5712f80312366e2fda5d7ddfb89a0c1c406ca6c62560a2a'
+$Checksum64 = '43fa793125305b4ee0fc6d16a8b96a09faa91ae312e9e5a3f962c22ee21fc8c8'
 $ChecksumType32 = 'sha256'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
