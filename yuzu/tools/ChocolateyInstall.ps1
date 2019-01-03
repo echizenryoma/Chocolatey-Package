39fc1cf94e62f4692786815e5a2d41b5edf4100c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-01-03 00:03:03 +0000

    AU: 4 updated - citra imagemagick.tool pkhex yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/edd8cc7434f91e41793a297b311c5a0f21b54b40

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-1784/yuzu-windows-mingw-20181231-2cc9bdd.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-1788/yuzu-windows-mingw-20190102-e3731fb.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
