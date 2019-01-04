e6533951260579d6a004c64d3805be121383d5eb
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-01-04 00:02:36 +0000

    AU: 2 updated - resourcehacker.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/24b6318c55d3559f9a7c4d60caaeadec6de3d614

--- resourcehacker.portable/tools/ChocolateyInstall.ps1
+++ resourcehacker.portable/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $PackageName = 'resourcehacker.portable'
 $Url = 'http://www.angusj.com/resourcehacker/resource_hacker.zip'
-$Checksum = 'de24309105f589d0c4f6d19c85d0354c0c8ffe9a2f34054ab2028b4a380c664d'
+$Checksum = '7d33dbc5ab0e4a9e1c88471d8afcba024dc62792f643833869f4e459306b01e6'
 $ChecksumType = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-1788/yuzu-windows-mingw-20190102-e3731fb.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-1792/yuzu-windows-mingw-20190103-733cbb5.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
