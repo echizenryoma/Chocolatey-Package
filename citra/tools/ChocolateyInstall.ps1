3a465e314cb710b0382397fd518fbb89ea63e87a
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-01-24 00:01:48 +0000

    AU: 2 updated - citra fsviewer.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/14484131905eb0a2a0f91baf85abb6be3e04d753

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1169/citra-windows-mingw-20190122-1f9f63b.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1170/citra-windows-mingw-20190123-a1f764b.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
