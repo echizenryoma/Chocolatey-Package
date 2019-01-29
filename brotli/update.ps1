fa8063f4df29fbefd48e2efa3ba9618d25d37ff6
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-01-29 21:18:18 +0800

    Change to Azure DevOps Services

--- brotli/update.ps1
+++ brotli/update.ps1
@@ -14,18 +14,18 @@ function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "$base/google/brotli/releases/latest"
     $version = (($page.Links.href -match "releases/tag/v\d+(\.\d+)+$" | Select-Object -Unique -First 1) -split "/" -match "\d+(\.\d+)+" | Select-Object -First 1).Replace("v", "")
 
-    $url = $base + ($page.Links.href -match "brotli.*win_i686\.zip" | Select-Object -First 1)
+    $url32 = $base + ($page.Links.href -match "brotli.*win_i686\.zip" | Select-Object -First 1)
     $url64 = $base + ($page.Links.href -match "brotli.*win_x86_64\.zip" | Select-Object -First 1)
     
-    if (($url -eq $base) -and ($url64 -eq $base)) {
+    if (($url32 -eq $base) -and ($url64 -eq $base)) {
         $version = "0.0.0"
     }
 	
     return @{
         Version = $version
-        URL32   = $url
+        URL32   = $url32
         URL64   = $url64
     }
 }
 
-Update-Package -NoCheckChocoVersion -ChecksumFor none
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
