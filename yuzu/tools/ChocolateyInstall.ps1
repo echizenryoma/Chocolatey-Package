3e25d51569939dfebed35318bd550bbe521fe1a8
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-03-01 16:07:42 +0000

    AU: 2 updated - postman yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/38326edcbd686333e20993d9780e91f8a1e88d58

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-1939/yuzu-windows-mingw-20190228-235126e.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-1942/yuzu-windows-mingw-20190301-701fcfe.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
