2fd7865edf430038914fae9cf540a65241d5af04
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-03-02 19:22:27 +0800

    [motrix.portable] Add

---
+++ motrix.portable/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+﻿$PackageName = 'Motrix'
+$ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+. $(Join-Path $ToolsPath "StopProcess.ps1")
+
+Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
+Uninstall-BinFile -Name ${PackageName}
+
+$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
+Remove-Item $LinkPath -Force -ErrorAction Ignore
