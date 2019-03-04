ed5a3374188ba44ffbd8c72bcbef9890bb6479b6
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-03-04 09:04:24 +0800

    [innounp] Update

--- innounp/tools/ChocolateyInstall.ps1
+++ innounp/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'innounp'
-$Url = 'https://sourceforge.net/projects/innounp/files/innounp/innounp%200.47/innounp047.rar'
+$Url = 'https://sourceforge.net/projects/innounp/files/innounp/innounp%200.48/innounp048.rar'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
--- innounp/update.ps1
+++ innounp/update.ps1
@@ -11,7 +11,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $base = "https://sourceforge.net/projects/innounp/files/innounp"
     $page = Invoke-WebRequest -UseBasicParsing -Uri $base
-    $version = ($page.Links.title -match "\d+(\.\d+)+") -split "/|\:|\s|-" -match "\d+(\.\d+)+" | Select-Object -Unique -First 1
+    $version = ($page.Links.title -match "\d+(\.\d+)+$") -split "\s" -match "\d+(\.\d+)+$" | Select-Object -Unique -First 1
     $url = "$base/innounp%20${version}/innounp$($version -replace "\.", '').rar"
 	
     return @{
