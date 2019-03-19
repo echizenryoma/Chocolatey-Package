4f360868ada862fa67cb5275b48fd2c1311d5932
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-03-19 05:02:51 +0000

    AU: 2 updated - goldendict.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/8b1499410b56d27d8d292a6aed3af1b66b7785a0

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2022/yuzu-windows-mingw-20190318-b062ca3.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2025/yuzu-windows-mingw-20190319-403113f.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
