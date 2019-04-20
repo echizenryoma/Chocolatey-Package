27630c6639f5275477933431e433ddc6d9b9b9fe
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-04-20 16:04:03 +0000

    AU: 2 updated - imagemagick.tool yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/9ce686e3d4ade9af50d0fb21b94d5ca6246e60f0

--- imagemagick.tool/tools/ChocolateyInstall.ps1
+++ imagemagick.tool/tools/ChocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'imagemagick.tool'
-$Url32 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-40-portable-Q16-x86.zip'
-$Url64 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-40-portable-Q16-x64.zip'
-$Checksum32 = '9c61ae4087467292840f50926ba60c23f7797e9bf89a955518f02a3bc7cb7ddb'
-$Checksum64 = '266a13b7fbda1ae1ad33e68e9ea19fa21c4bc68391b500bc75690cf43cd28169'
+$Url32 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-41-portable-Q16-x86.zip'
+$Url64 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.8-41-portable-Q16-x64.zip'
+$Checksum32 = 'e2b279ef46b4a6f28ef8e31e645f138adad6f299b7bf3f9c26a621b30e59d3bf'
+$Checksum64 = '0bcf6544373757cb6d8079c8ac27876a711ed090706424b4a60e5783f8e46407'
 $ChecksumType32 = 'sha256'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2195/yuzu-windows-mingw-20190419-3df59b9.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2203/yuzu-windows-mingw-20190420-1263745.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
