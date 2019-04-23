9f09421b27ef7a4f6eca1222b138a0aaa5f896fb
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-04-23 16:06:58 +0000

    AU: 3 updated - otf-stix2 wifiinfoview yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/047b4ee4090549f0bca51a8663862873a55107cf

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2205/yuzu-windows-mingw-20190421-f7cc66a.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2211/yuzu-windows-mingw-20190423-6a54315.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
