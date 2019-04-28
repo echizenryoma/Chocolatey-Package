61a14c7fbe379257d779f1f5ab1807851ae5fb8c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-04-28 09:02:28 +0800

    [goldendict.portable] Fix the bug

--- goldendict.portable/tools/ChocolateyInstall.ps1
+++ goldendict.portable/tools/ChocolateyInstall.ps1
@@ -3,7 +3,8 @@
 $PackageName = 'GoldenDict'
 $Url32 = 'https://sourceforge.net/projects/goldendict/files/early%20access%20builds/Qt5-based/GoldenDict-1.5.0-RC2-372-gc3ff15f_%28QT_5123%29.7z/download'
 $Url64 = 'https://sourceforge.net/projects/goldendict/files/early%20access%20builds/Qt5-based/64bit/GoldenDict-1.5.0-RC2-372-gc3ff15f_%28QT_5123%29%2864bit%29.7z/download'
-$InstallationPath = Get-ToolsLocation
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$UnzipPath = Get-ToolsLocation
 
 Get-ChildItem -Path $InstallationPath -Exclude "content", "portable" -ErrorAction Ignore | Remove-Item -Recurse -Force -ErrorAction Ignore
 
@@ -11,6 +12,6 @@ $PackageArgs = @{
     PackageName   = $PackageName
     Url           = $Url32
     Url64         = $Url64
-    UnzipLocation = $InstallationPath
+    UnzipLocation = $UnzipPath
 }
 Install-ChocolateyZipPackage @PackageArgs
