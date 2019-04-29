7ce3469f3395f86d5f91053541a1276953ac4a86
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-04-29 16:04:25 +0000

    AU: 2 updated - x64dbg.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/a6f00e6c0b30c71edd8d9480edcd30e4e34fe3ea

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2222/yuzu-windows-mingw-20190426-70d4f91.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2226/yuzu-windows-mingw-20190429-b384c7a.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
