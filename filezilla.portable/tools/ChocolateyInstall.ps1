ef416e4ea53b16f56ed5e51aa533a374196e7bda
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-05-06 16:03:52 +0000

    AU: 2 updated - filezilla.portable qbittorrent.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/2d0e2080e14d4d06daada25ca2fde9630867b245

--- filezilla.portable/tools/ChocolateyInstall.ps1
+++ filezilla.portable/tools/ChocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $PackageName = 'FileZilla'
 $Page = Invoke-WebRequest -UseBasicParsing -Uri 'https://filezilla-project.org/download.php?show_all=1'
 $Url32 = $page.Links.href -match "win32\.zip" | Select-Object -First 1
-$Checksum32 = '72d1adf580a5ab498cb928e5487accc249882fba03b05b47948b81cb0fdb87f7e603fb0366f1c052fadecaee3467f32d7cf649d0c599a3e94d3f900206005498'
+$Checksum32 = 'c487969f388fd9fae6a1d539a6251591789460ed2c0540266663a7744befb1a766efba87bc389fb3b7ce2ac3824028d07a8ac56e9e03e9709ea289e6bb484896'
 $ChecksumType32 = 'sha512'
 $Url64 = $page.Links.href -match "win64\.zip" | Select-Object -First 1
-$Checksum64 = '63ad44c14fe501777fe577b449d8e05f9c9ab05aff4aa26acf99e90074dc814c210d5de801fb796ce2b41be2e3963e57d2572d139bd5a96a8635fbc6bb0c5488'
+$Checksum64 = 'dce18de7ce16a381219f335d63f4ba4efa8e2cdfb58a6fd4d3b1a3db279e74239a167282a4c404aaff384a8cc6afd31c866c357719c5d17ec00dbf8ee433d5a6'
 $ChecksumType64 = 'sha512'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Get-ToolsLocation
