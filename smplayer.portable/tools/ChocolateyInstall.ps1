c2087c245d077ed2da9dc6bdb909818cbf6136b4
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-05-12 12:44:04 +0800

    [smplayer.portable] Add

---
+++ smplayer.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'smplayer'
+$Url32 = 'https://sourceforge.net/projects/smplayer/files/SMPlayer/19.1.0/smplayer-portable-19.1.0.0.7z/download'
+$Url64 = 'https://sourceforge.net/projects/smplayer/files/SMPlayer/19.1.0/smplayer-portable-19.1.0.0-x64.7z/download'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Get-ChildItem -Directory $ToolsPath | Remove-Item -Recurse -Force -ErrorAction Ignore
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url32
+    Url64         = $Url64
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$InstallationPath = (Get-ChildItem $ToolsPath -Directory | Where-Object Name -Match "${PackageName}" | Select-Object -First 1).FullName
+$BinFileName = Join-Path $InstallationPath "${PackageName}.exe"
+$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
+Install-ChocolateyShortcut -ShortcutFilePath $LinkPath -TargetPath $BinFileName -WorkingDirectory $InstallationPath
---
+++ smplayer.portable/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,3 @@
+﻿$PackageName = 'smplayer'
+$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
+Remove-Item $LinkPath -Force -ErrorAction Ignore
