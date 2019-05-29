4b19d247e0c7804750e4d4d06c2ab6958c3f049c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-05-29 14:34:00 +0800

    [audacity.portable] Add

---
+++ audacity.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,46 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'audacity'
+$Checksum = '7edd522506274df9fff2839182988c210a7c2187295942b963eb81ea094cd4ad'
+$ChecksumType = 'sha256'
+$ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$url = 'https://www.fosshub.com/Audacity.html'
+$page = Invoke-WebRequest -Uri $url
+$obj = ($page.Scripts.outerText -match "settings") -split "=" -match "{" | ConvertFrom-Json
+$body = @{
+    "projectId"  = $obj.projectId
+    "releaseId"  = ($obj.pool.f.r | Select-Object -Unique -First 1)
+    "projectUri" = $obj.pool.u
+    "fileName"   = ($obj.pool.f.n -match "zip" -notmatch "manual")[0]
+    "source"     = $obj.pool.c
+} | ConvertTo-Json -Compress
+
+$base = 'https://api.fosshub.com'
+$url = "${base}/download/"
+$page = Invoke-WebRequest -Method Options -Uri $url -SessionVariable session
+$page = Invoke-WebRequest -Method Post -Body $body -ContentType "application/json" -Uri $url -WebSession $session
+$obj = $page.Content | ConvertFrom-Json
+$Url = $obj.data.url
+
+$cookies = $session.Cookies.GetCookies($base)
+$cookie = ""
+foreach ($it in $cookies) {
+    $cookie = $cookie + ('{0}={1};' -f $it.Name, $it.Value)
+}
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    Checksum      = $Checksum
+    ChecksumType  = $ChecksumType
+    UnzipLocation = $ToolsPath
+    Options       = @{
+        Headers = @{
+            UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36' 
+            Cookie    = $cookie
+            Referer   = $url
+        }
+    }
+}
+Install-ChocolateyZipPackage @PackageArgs
---
+++ audacity.portable/update.ps1
@@ -0,0 +1,28 @@
+﻿import-module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]ChecksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $url = 'https://www.fosshub.com/Audacity.html'
+    $page = Invoke-WebRequest -Uri $url
+    $obj = ($page.Scripts.outerText -match "settings") -split "=" -match "{" | ConvertFrom-Json
+    $file = ($obj.pool.f -match "zip" -notmatch "manual")[0]
+    $checksum_type = 'sha256'
+    $checksum = $file.hash.$checksum_type
+    $version = $file.n -split "-|\.zip" -match "\d+(\.\d+)+"
+
+    return @{
+        Version        = $version
+        ChecksumType32 = $checksum_type
+        Checksum32     = $checksum
+    }
+}
+
+Update-Package -NoCheckChocoVersion -ChecksumFor none
