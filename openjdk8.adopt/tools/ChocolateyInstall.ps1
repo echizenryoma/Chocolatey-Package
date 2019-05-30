d180cd16bca33cb8d3c5d0284ba391654c7936dc
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-05-30 22:45:38 +0800

    [openjdk8.adopt] Add

---
+++ openjdk8.adopt/tools/ChocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'openjdk8'
+$Url = 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b04_openj9-0.14.2/OpenJDK8U-jdk_x86-32_windows_openj9_8u212b04_openj9-0.14.2.zip'
+$Checksum = 'e79204650df11ff488e4ffe4741836d8bcfec2b570159bcb17224ea0020837ca'
+$ChecksumType = 'sha256'
+$Url64 = 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b04_openj9-0.14.2/OpenJDK8U-jdk_x64_windows_openj9_8u212b04_openj9-0.14.2.zip'
+$Checksum64 = '2358865ef78e4b50d470f65fc0f10d60c2d83c7534a9934847e3e3461687962a'
+$ChecksumType64 = 'sha256'
+$InstallationPath = Get-ToolsLocation
+
+$PackageArgs = @{
+    PackageName    = $PackageName
+    Url            = $Url
+    Checksum       = $Checksum
+    ChecksumType   = $ChecksumType
+    Url64          = $Url64
+    Checksum64     = $Checksum64
+    ChecksumType64 = $ChecksumType64
+    UnzipLocation  = $InstallationPath
+}
+Install-ChocolateyZipPackage @PackageArgs
---
+++ openjdk8.adopt/update.ps1
@@ -0,0 +1,48 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+            "(^[$]Checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(^[$]ChecksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]Url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^[$]Checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(^[$]ChecksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $url = 'https://api.github.com/repos/AdoptOpenJDK/openjdk8-binaries/releases/latest'
+    $page = Invoke-WebRequest -UseBasicParsing -Uri $url
+    $json = $page.Content | ConvertFrom-Json
+
+    $url32 = ($json.assets | Where-Object { $_.name -NotMatch "jre" -and $_.name -Match "32.*windows.*zip$" })[0].browser_download_url
+    $url = ($json.assets | Where-Object { $_.name -NotMatch "jre" -and $_.name -Match "32.*windows.*zip.*txt" })[0].browser_download_url
+    $checksum_type32 = ($url -split "\." -match "sha\d+")[0]
+    $page = Invoke-WebRequest -UseBasicParsing -Uri $url
+    $table = [System.Text.Encoding]::UTF8.GetString($page.Content) -split "\n" | ConvertFrom-String -PropertyNames checksum, file
+    $checksum32 = $table.checksum
+
+    $url64 = ($json.assets | Where-Object { $_.name -NotMatch "jre" -and $_.name -Match "x64.*windows.*zip$" })[0].browser_download_url
+    $url = ($json.assets | Where-Object { $_.name -NotMatch "jre" -and $_.name -Match "64.*windows.*zip.*txt" })[0].browser_download_url
+    $checksum_type64 = ($url -split "\." -match "sha\d+")[0]
+    $page = Invoke-WebRequest -UseBasicParsing -Uri $url
+    $table = [System.Text.Encoding]::UTF8.GetString($page.Content) -split "\n" | ConvertFrom-String -PropertyNames checksum, file
+    $checksum64 = $table.checksum
+
+    $version = [version](($json.name -split "_")[0] -split "[a-z]|-" -match "\d" -join ".")
+	
+    return @{
+        Version        = $version
+        URL32          = $url32
+        ChecksumType32 = $checksum_type32
+        Checksum32     = $checksum32
+        URL64          = $url64
+        ChecksumType64 = $checksum_type64
+        Checksum64     = $checksum64
+    }
+}
+
+Update-Package -NoCheckChocoVersion -ChecksumFor none
