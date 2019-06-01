61560c88d70b163ea4cc6a83f1b140393c8b7997
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-06-01 14:19:52 +0800

    [smplayer.portable] Move to opt

--- smplayer.portable/tools/ChocolateyInstall.ps1
+++ smplayer.portable/tools/ChocolateyInstall.ps1
@@ -1,21 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$PackageName = 'smplayer'
+$PackageName = 'SMPlayer'
 $Url32 = 'https://sourceforge.net/projects/smplayer/files/SMPlayer/19.5.0/smplayer-portable-19.5.0.0.7z/download'
 $Url64 = 'https://sourceforge.net/projects/smplayer/files/SMPlayer/19.5.0/smplayer-portable-19.5.0.0-x64.7z/download'
-$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-Get-ChildItem -Directory $ToolsPath | Remove-Item -Recurse -Force -ErrorAction Ignore
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$UnzipLocation = Join-Path $InstallationPath 'tmp'
 
 $PackageArgs = @{
     PackageName   = $PackageName
     Url           = $Url32
     Url64         = $Url64
-    UnzipLocation = $ToolsPath
+    UnzipLocation = $UnzipLocation
 }
 Install-ChocolateyZipPackage @PackageArgs
 
-$InstallationPath = (Get-ChildItem $ToolsPath -Directory | Where-Object Name -Match "${PackageName}" | Select-Object -First 1).FullName
-$BinFileName = Join-Path $InstallationPath "${PackageName}.exe"
+$UnzipPath = (Get-ChildItem $UnzipLocation -Directory | Where-Object Name -Match "${PackageName}" | Select-Object -First 1).FullName
+Copy-Item -Path $(Join-Path $UnzipPath "*") -Destination $InstallationPath -Recurse -Force
+Remove-Item -Path $UnzipLocation -Recurse -Force -ErrorAction Ignore
+
+$BinFile = Join-Path $InstallationPath "${PackageName}.exe"
+Install-BinFile -Name ${PackageName} -Path $BinFile
 $LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
-Install-ChocolateyShortcut -ShortcutFilePath $LinkPath -TargetPath $BinFileName -WorkingDirectory $InstallationPath
+Install-ChocolateyShortcut -ShortcutFilePath $LinkPath -TargetPath $BinFile -WorkingDirectory $InstallationPath
