5b71238b60697c573a2752172d2ada1fa308a18f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-06-04 16:04:07 +0000

    AU: 2 updated - fastcopy.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/00a17cdb3d6f5110fbef2e1ea08948df66e76ef5

--- fastcopy.portable/tools/ChocolateyInstall.ps1
+++ fastcopy.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'fastcopy'
-$Url32 = 'https://fastcopy.jp/archive/FastCopy380_installer.exe'
+$Url32 = 'https://fastcopy.jp/archive/FastCopy381_installer.exe'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $SilentArgs = "/DIR=`"${ToolsPath}`" /EXTRACT"
 
