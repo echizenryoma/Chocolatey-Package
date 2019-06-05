07e60ca5de988cc27baddbd31cf7c71ced97479d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-06-05 23:47:32 +0000

    AU: 1 updated - vscode.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/da24fce598973083c1c72857d1688712e0bc47c4

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $PackageName = 'vscode'
 $Url32 = 'https://vscode-update.azurewebsites.net/latest/win32-archive/stable'
 $ChecksumType32 = 'sha256'
-$Checksum32 = 'f8a2daf83a413793a0d8b5b4dd3067540b895f75f35473cee091869729894883'
+$Checksum32 = '9d94ad7c751e54fff8067af9fa3fc6eead37667e4cb84ce19657bdca3034e9ca'
 $Url64 = 'https://vscode-update.azurewebsites.net/latest/win32-x64-archive/stable'
 $ChecksumType64 = 'sha256'
-$Checksum64 = 'a454b90305023e0d67cbc943766c4041ba426bce907c83d900e809fcc3c125f8'
+$Checksum64 = '8f5997dc6e06974f9b9104fa073c426a4584e2cb823b21273106cb03fcdf8b9d'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 Get-ChildItem -Path $InstallationPath -Exclude data -ErrorAction Ignore | Remove-Item -Recurse -Force -ErrorAction Ignore
