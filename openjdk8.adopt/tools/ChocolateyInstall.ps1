1281f5b15d569101c5cef5554f6919d4bc35325b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-06-07 08:22:01 +0800

    [openjdk8.adopt] Remove old version jdk

--- openjdk8.adopt/tools/ChocolateyInstall.ps1
+++ openjdk8.adopt/tools/ChocolateyInstall.ps1
@@ -6,6 +6,12 @@ $Checksum64 = 'bc16200794603d41cf0402cc8af7e24d4ba32a48608505a7cead9c4997b526fa'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Get-ToolsLocation
 
+$JdkVersion = $Url64 -split "/" | Select-Object -Last 2 | Select-Object -First 1
+$UnzipLocation = Join-Path $(Get-ToolsLocation) $JdkVersion
+if (Test-Path $UnzipLocation) {
+    Remove-Item -Path $UnzipLocation -Force -Recurse
+}
+
 $PackageArgs = @{
     PackageName    = $PackageName
     Url64          = $Url64
