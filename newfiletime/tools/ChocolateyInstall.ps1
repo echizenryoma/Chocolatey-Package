9d6ca3a58e808ee6eaa73d968d99ef8eb4718982
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-07-15 16:04:29 +0000

    AU: 2 updated - newfiletime yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/0bc85b1505620917c7215ef2d86d8f513616cc38

--- newfiletime/tools/ChocolateyInstall.ps1
+++ newfiletime/tools/ChocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $PackageName = 'newfiletime'
 $Url32 = 'https://www.softwareok.com/Download/NewFileTime.zip'
-$Checksum32 = 'f177b944c2e44d5781a8f09eeaffb637639adbe3c25435c90c4cf9369505c8db'
+$Checksum32 = '3a6a5548825a7c80ac136ad241698ec5f85a105c22e057111cbfb8da2cc650e0'
 $ChecksumType32 = 'sha256'
 $Url64 = 'https://www.softwareok.com/Download/NewFileTime_x64.zip'
-$Checksum64 = 'f794de90ae7e4f086284c055f807ee6eb9a309bae8a953b63ca9624eb9db5242'
+$Checksum64 = 'cb52e478a36feffcf2d5e6e1d13f5a37013dbbaa0f73fdccd7ea4c2d172db58f'
 $ChecksumType64 = 'sha256'
 $UrlExtra = 'https://www.softwareok.com/Download/NewFileTime_Unicode.zip'
-$ChecksumExtra = '780747c286a7a7c118fd969732e4d74be9ac8abec2556e365570743e90b2ae07'
+$ChecksumExtra = 'bca64219f4e277fa5a04b56816ade2b38e3767d2e58c92c84c99ce56048fc9af'
 $ChecksumTypeExtra = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
