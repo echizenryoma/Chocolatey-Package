c1bec1f7b37b753627e4728234d05b652abd28c1
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-09-21 16:02:51 +0000

    AU: 2 updated - tomcat.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/67067e02e28b20c7443667e4f39eaa5b2d8b5bb4

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2628/yuzu-windows-mingw-20190920-d899f42.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2641/yuzu-windows-mingw-20190921-7ae7ceb.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
