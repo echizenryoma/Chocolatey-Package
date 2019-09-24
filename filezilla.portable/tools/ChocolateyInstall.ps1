124b4987a02c02c14dc3dbf84c1d08d038f2a2d1
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-09-24 16:02:55 +0000

    AU: 3 updated - filezilla.portable wifiinfoview yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/0bb5a594829a6c3d441cb384082b281497e3cb58

--- filezilla.portable/tools/ChocolateyInstall.ps1
+++ filezilla.portable/tools/ChocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $PackageName = 'FileZilla'
 $Page = Invoke-WebRequest -UseBasicParsing -Uri 'https://filezilla-project.org/download.php?show_all=1'
 $Url32 = $page.Links.href -match "win32\.zip" | Select-Object -First 1
-$Checksum32 = '32496bc357c3a40ed636edb46e5bb4d74cf2fd0a78a24845904ab054b2911049399f813f3be5eac0b4887496fa439ebd8adf890bb0c5ce0d59db7854a96a09df'
+$Checksum32 = 'c05d14a7e8b71697fd6680c718eaed49b288a21aaee92db77df00ec95862586dee295bee339b15507c59f9c615fc7f700ef87e17a131164588448046629d4e21'
 $ChecksumType32 = 'sha512'
 $Url64 = $page.Links.href -match "win64\.zip" | Select-Object -First 1
-$Checksum64 = '1c3d0aa64806962239e93172c1ad6ec69942b7ce3e7602bec24ccee5647d7a9ac3fdf57fe8bdfc9dda7ec2dfe1f083b2afb9b521aeac63a3df1b9a51bfc54813'
+$Checksum64 = 'd315c65a49ec5dace7104eb21b1170d82c838f76f9a07eabca9f6f9965707ae9cfd2c492efe6b497d61a3e5673ac9fd4d05a4557d686ad4b58a4725c6a10a3e7'
 $ChecksumType64 = 'sha512'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2671/yuzu-windows-mingw-20190923-41925bc.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2673/yuzu-windows-mingw-20190924-aa6f72b.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
