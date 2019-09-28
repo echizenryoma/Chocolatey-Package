18fb46870af49f36eabbbfb43aeaa0deda4a221e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-09-28 16:02:52 +0000

    AU: 2 updated - cemu yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/91d076516ae47cb3a9d034a314afebd49015465b

--- cemu/tools/ChocolateyInstall.ps1
+++ cemu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'cemu'
-$Url64 = 'http://cemu.info/releases/cemu_1.15.14.zip'
+$Url64 = 'http://cemu.info/releases/cemu_1.15.15.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2677/yuzu-windows-mingw-20190926-bf1adf6.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2680/yuzu-windows-mingw-20190927-5df0e06.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
