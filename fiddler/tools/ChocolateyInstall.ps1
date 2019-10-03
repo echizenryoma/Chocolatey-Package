bef3b85d7b18bc9fe4d87090784ed2297e45e127
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-10-03 16:03:04 +0000

    AU: 1 updated - fiddler [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/df0bb7d9d276a9a5eadf7ce33be91c4a927f0e1e

--- fiddler/tools/ChocolateyInstall.ps1
+++ fiddler/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $PackageName = 'fiddler'
 $Url = 'https://telerik-fiddler.s3.amazonaws.com/fiddler/FiddlerSetup.exe'
-$Checksum = '4DFF6CB1B88A503D3982C7CCBCD5622496EBA4C21AAB5A2C0C0F1E376A2EB809'
+$Checksum = '2AFE37AB5CDB7DF4A80FCBE85B9298F509DF064173E6A4EE7008E5D409BC6172'
 $ChecksumType = 'sha256'
 
 $PackageArgs = @{
