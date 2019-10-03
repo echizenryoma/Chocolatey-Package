e1f619413b0c42a297c795759edd0b422a69dcd8
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-10-03 14:55:20 +0800

    [drawio.portable] Add

---
+++ drawio.portable/tools/Chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'drawio'
+$Url64 = 'https://github.com/jgraph/drawio-desktop/releases/download/v11.3.0/draw.io-11.3.0-windows-no-installer.exe'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$UnzipLocation = Join-Path $InstallationPath 'tmp'
+
+New-Item -ItemType Directory -Path "$UnzipLocation" -Force -ErrorAction Ignore
+
+$PackageArgs = @{
+  PackageName   = $PackageName
+  Url64         = $Url64
+  UnzipLocation = $UnzipLocation
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$App7zPath = (Get-ChildItem $UnzipLocation -File -Recurse | Sort-Object Length -Descending | Select-Object -First 1).FullName
+$PackageArgs = @{
+  PackageName   = $PackageName
+  Url64         = $App7zPath
+  UnzipLocation = $InstallationPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+Remove-Item -Path $UnzipLocation -Recurse -Force -ErrorAction Ignore
---
+++ drawio.portable/update.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $domain = "https://github.com"
+    $base = "${domain}/jgraph/drawio-desktop"
+
+    $url = "${base}/releases/latest"
+    $page = Invoke-WebRequest -UseBasicParsing -Uri $url
+    $version = ($page.Links.href -match "releases/tag/(v?)\d+(\.\d+)+$" | Select-Object -Unique -First 1) -split "/|v" -match "\d+(\.\d+)+" | Select-Object -First 1
+    $url64 = $domain + ($page.Links.href -match "^/.*windows-no-installer" | Select-Object -First 1)
+	
+    return @{
+        Version = $version
+        URL64   = $url64
+    }
+}
+
+Update-Package -NoCheckChocoVersion -ChecksumFor none
