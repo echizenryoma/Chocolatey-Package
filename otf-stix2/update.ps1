aec571aa1999ea523bd6c38e244a8a8a2b5eaa0f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-10-04 09:49:12 +0800

    [otf-stix2] fix update.ps1

--- otf-stix2/update.ps1
+++ otf-stix2/update.ps1
@@ -11,7 +11,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "https://ctan.org/pkg/stix2-otf"
     $version = ($page.Content -split "<|>|`n" -match "^\d+(\.\d+)+" | Select-Object -First 1).Trim() -replace "\s", "." -replace "\-", ""
-    $url32 = $page.Links -match "Down&shy;load" | Select-Object -First 1 -ExpandProperty href
+    $url32 = $page.Links -match "Download" | Select-Object -First 1 -ExpandProperty href
     
     return @{
         Version = $version
