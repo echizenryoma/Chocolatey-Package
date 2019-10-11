c03ca15fc9129134e0e8d56ab3cfcccc74b11916
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-10-11 16:05:23 +0000

    AU: 4 updated - drawio.portable inno-setup.portable vscode.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/51a66873fb1062e31ccbe3c4a498d05f0e5ca67b

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2730/yuzu-windows-mingw-20191010-2f9f3c2.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2731/yuzu-windows-mingw-20191011-9d29c1b.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
