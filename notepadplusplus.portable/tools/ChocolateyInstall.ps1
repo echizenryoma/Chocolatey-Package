d81e6b43e680ac3dd4cb616fb5de172140efd854
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-10-16 00:35:07 +0000

    AU: 2 updated - notepadplusplus.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/5347554c093b4aff6e15179e8f030b781b1d4035

--- notepadplusplus.portable/tools/ChocolateyInstall.ps1
+++ notepadplusplus.portable/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'notepadplusplus'
-$Url32 = 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.bin.7z'
-$Checksum32 = 'f0af67993bd5f420ef0d9268f9667d628090491899f6529a7a75f60244700ef1'
+$Url32 = 'http://download.notepad-plus-plus.org/repository/7.x/7.8/npp.7.8.bin.7z'
+$Checksum32 = ''
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.bin.x64.7z'
-$Checksum64 = '528ec2bf90fd409b4bf914c198b93db28cecd4fa2a8cdf6180f1b9cded7f8dfa'
+$Url64 = 'http://download.notepad-plus-plus.org/repository/7.x/7.8/npp.7.8.bin.x64.7z'
+$Checksum64 = ''
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
