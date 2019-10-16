1edb2b23658b1b593907edf2363550f805edcf13
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-10-16 08:31:45 +0800

    Merge from origin/master

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,17 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'vscode'
-<<<<<<< HEAD
-$Url32 = 'https://vscode-update.azurewebsites.net/1.39.2/win32-archive/stable'
-$Checksum32 = 'c6f871e594133d9f81b9cca7ccb463ad3ebb265eb666a58541e4b81ee47b1d95'
-$Url64 = 'https://vscode-update.azurewebsites.net/1.39.2/win32-x64-archive/stable'
-=======
-$Version = '1.39.2'
 $Url32 = 'https://vscode-update.azurewebsites.net/1.39.2/win32-archive/stable'
 $ChecksumType32 = 'sha256'
 $Checksum32 = 'c6f871e594133d9f81b9cca7ccb463ad3ebb265eb666a58541e4b81ee47b1d95'
 $Url64 = 'https://vscode-update.azurewebsites.net/1.39.2/win32-x64-archive/stable'
->>>>>>> bf85287d07e25a2304c40447f611f2950a38715f
 $ChecksumType64 = 'sha256'
 $Checksum64 = '3adfc6dc97ca62cd607793ce8a37a1d9ab985fef888d5b622973494d663a09a8'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
