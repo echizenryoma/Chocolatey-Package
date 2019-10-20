c61c8c3a34c6f0df0cc7c021906cc653c9e00bef
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-10-20 15:54:43 +0800

    [x64dbg.portable] Change to sourceforge.net

--- x64dbg.portable/tools/ChocolateyInstall.ps1
+++ x64dbg.portable/tools/ChocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'x64dbg'
-$Url = 'https://github.com/x64dbg/x64dbg/releases/download/snapshot/snapshot_2019-10-14_00-57.zip'
+$Url32 = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2019-10-14_00-57.zip'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 . $(Join-Path $ToolsPath "StopProcess.ps1")
 
 $PackageArgs = @{
     PackageName   = $PackageName
-    Url           = $Url
+    Url           = $Url32
     UnzipLocation = $ToolsPath
 }
 Install-ChocolateyZipPackage @PackageArgs
