bd3662bc56f8e1981aba21250b0db6814ceee148
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-11-04 07:57:11 +0800

    [inno-setup.portable] fix update.ps1

--- inno-setup.portable/update.ps1
+++ inno-setup.portable/update.ps1
@@ -4,7 +4,6 @@ function global:au_SearchReplace {
     @{
         ".\tools\ChocolateyInstall.ps1" = @{
             "(^[$]Url32\s*=\s*)('.*')"    = "`$1'$($Latest.URL32)'"
-            "(^[$]UrlExtra\s*=\s*)('.*')" = "`$1'$($Latest.UrlExtra)'"
         }
     }
 }
@@ -14,9 +13,6 @@ function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "${base}/jrsoftware/ispack/releases"
     $url32 = $base + ($page.Links.href -match "is-\d+(_\d+)+\.zip" | Select-Object -First 1)
     $version = [IO.Path]::GetFileNameWithoutExtension($url32) -split "-" -replace "_", "." -match "\d+(\.\d+)+" | Select-Object -First 1
-
-    $page = Invoke-WebRequest -UseBasicParsing -Uri "http://other.jrsoftware.org/ip/"
-    $url_extra = ($page.Links.href -match ".zip" | Select-Object -First 1)
 
     return @{
         Version  = $version
