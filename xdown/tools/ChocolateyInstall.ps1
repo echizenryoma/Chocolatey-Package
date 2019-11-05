46ae0aac3c913c17ad32166116085a63d7adc70b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-11-05 16:03:30 +0000

    AU: 2 updated - x64dbg.portable xdown [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/ecca47592b65a77994ca57f453952d3408d9fd66

--- xdown/tools/ChocolateyInstall.ps1
+++ xdown/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'XDown'
-$Url32 = 'https://dl.xdown.org/xdown-1.0.1.1.zip'
+$Url32 = 'https://dl.xdown.org/xdown-1.0.1.2.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName.ToLower()
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
