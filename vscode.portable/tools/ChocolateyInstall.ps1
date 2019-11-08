72fe8c054ef2bea604eaafeb6b99ade59e431a2d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-11-08 16:04:17 +0000

    AU: 2 updated - calibre.portable vscode.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/bb0c0ae5cfa9120598552fedb08276548b737daf

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'vscode'
-$Url32 = 'https://vscode-update.azurewebsites.net/1.39.2/win32-archive/stable'
+$Url32 = 'https://vscode-update.azurewebsites.net/1.40.0/win32-archive/stable'
 $ChecksumType32 = 'sha256'
-$Checksum32 = 'c6f871e594133d9f81b9cca7ccb463ad3ebb265eb666a58541e4b81ee47b1d95'
-$Url64 = 'https://vscode-update.azurewebsites.net/1.39.2/win32-x64-archive/stable'
+$Checksum32 = '1a2527f87d52c24f9e2eddb2e8e867e959bf0dcafa970546b2fe0f8e426431d8'
+$Url64 = 'https://vscode-update.azurewebsites.net/1.40.0/win32-x64-archive/stable'
 $ChecksumType64 = 'sha256'
-$Checksum64 = '3adfc6dc97ca62cd607793ce8a37a1d9ab985fef888d5b622973494d663a09a8'
+$Checksum64 = '58040125cc4493e5210f4116497e86a05afa0a6af5da6d8256c7ebe694a80a32'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 Get-ChildItem -Path $InstallationPath -Exclude data -ErrorAction Ignore | Remove-Item -Recurse -Force -ErrorAction Ignore
