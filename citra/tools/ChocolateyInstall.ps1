bc274d5a723ee3d9c50462fca7c292028d414904
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-11-09 23:20:02 +0800

    [citra] Add

---
+++ citra/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'citra'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1492/citra-windows-mingw-20191005-33f2c8b.7z'
+$ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
