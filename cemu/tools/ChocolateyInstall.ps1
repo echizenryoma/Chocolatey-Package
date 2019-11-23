438a36f86beb0ae0cc906672361e77bca9f77520
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-11-23 16:03:58 +0000

    AU: 2 updated - cemu tomcat.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/d4b3461b83ba4a6a1d7eceaa1112bfa509295e9d

--- cemu/tools/ChocolateyInstall.ps1
+++ cemu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'cemu'
-$Url64 = 'http://cemu.info/releases/cemu_1.15.18.zip'
+$Url64 = 'http://cemu.info/releases/cemu_1.15.19.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
