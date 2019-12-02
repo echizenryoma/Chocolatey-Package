b7a1b746bc6f27f23541bed8ebc6867fb8445067
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-12-02 00:30:22 +0000

    AU: 4 updated - citra imagemagick.tool xdown yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/9a6dca30dd7399289521053dcd3c83ff1ec2b000

--- imagemagick.tool/tools/ChocolateyInstall.ps1
+++ imagemagick.tool/tools/ChocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'imagemagick.tool'
-$Url32 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.9-6-portable-Q16-x86.zip'
-$Url64 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.9-6-portable-Q16-x64.zip'
-$Checksum32 = '7444700ab473ef7558a5047ae50a21399da3b948f0675755612ea71d378e9b57'
-$Checksum64 = 'b2bda2b9ec38c4bdcbcec2ac8025c6d41b0c62eaa7d9e24552ab146d724d8c07'
+$Url32 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.9-7-portable-Q16-x86.zip'
+$Url64 = 'https://www.imagemagick.org/download/binaries/ImageMagick-7.0.9-7-portable-Q16-x64.zip'
+$Checksum32 = '22d7a8b47a570ff8c00de55d15a64903a49dfc62a63121713acc252a1d757b81'
+$Checksum64 = '3608b007831edba17a45a384aca5d507b5a42d396419b7c3b2c90f7acc00ce16'
 $ChecksumType32 = 'sha256'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
