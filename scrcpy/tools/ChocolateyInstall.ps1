184bf80a9d291333d6bac793601fea1b7fc46e40
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-12-10 16:06:57 +0000

    AU: 3 updated - scrcpy xdown yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/343a990d53feff86f0897224bafa11ce637ad39c

--- scrcpy/tools/ChocolateyInstall.ps1
+++ scrcpy/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'scrcpy'
-$Url32 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.11/scrcpy-win32-v1.11.zip'
-$Checksum32 = 'f25ed46e6f3e81e0ff9b9b4df7fe1a4bbd13f8396b7391be0a488b64c675b41e'
+$Url32 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.12/scrcpy-win32-v1.12.zip'
+$Checksum32 = 'b2c8c4a3899c037cf448a2102906775114826ba646ce1b847826925103fa801d'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.11/scrcpy-win64-v1.11.zip'
-$Checksum64 = '3802c9ea0307d437947ff150ec65e53990b0beaacd0c8d0bed19c7650ce141bd'
+$Url64 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.12/scrcpy-win64-v1.12.zip'
+$Checksum64 = '7d47983b426f7287de0230b88975dc17c1d9c343fa61a93ff2af78b6e9ef5c8c'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2850/yuzu-windows-mingw-20191209-517202a.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2852/yuzu-windows-mingw-20191210-7b374c9.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
