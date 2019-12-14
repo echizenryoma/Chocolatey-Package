843743cf231f0596f54285254478850e42f1032d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-12-14 16:03:06 +0000

    AU: 2 updated - citra yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/446a69d9909bc0d76705f858acbb700f2e972b30

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1513/citra-windows-mingw-20191205-c4cb8b2.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1514/citra-windows-mingw-20191214-fc6e0bf.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2861/yuzu-windows-mingw-20191213-098d996.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2862/yuzu-windows-mingw-20191214-1b67dc8.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
