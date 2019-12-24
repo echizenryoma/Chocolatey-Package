73e3e5b811812fb6a0d4f31360fa6670537b2e87
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2019-12-24 08:25:33 +0800

    [wincontig] Update update.ps1

--- wincontig/update.ps1
+++ wincontig/update.ps1
@@ -1,5 +1,8 @@
 ﻿Import-Module AU
 
+function global:au_SearchReplace {
+}
+
 function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "https://wincontig.mdtzone.it/en/index.htm"
     $version = ($page -split "\n|<|>" -match "WinContig\s+\d+(\.\d+)+" | Select-Object -First 1) -split "\s|v" -match "\d+(\.\d+)+" | Select-Object -First 1
