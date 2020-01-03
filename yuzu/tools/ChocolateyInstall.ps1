85cba03cbee88f65fc712ad9be728c50073720a0
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-01-03 16:02:53 +0000

    AU: 4 updated - calibre.portable cemu wifiinfoview yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/c9d54c90aed1f74edfa66460ef4da1e985d7cc93

--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2889/yuzu-windows-mingw-20200102-4f8f689.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2890/yuzu-windows-mingw-20200103-df545a5.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
