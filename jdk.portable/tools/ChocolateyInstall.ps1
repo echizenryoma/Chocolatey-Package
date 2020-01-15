e6523dd95e811e92c1b4a2c00b19ffb09419d03f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-01-15 00:43:20 +0000

    AU: 2 updated - jdk.portable yuzu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/cceed5d24dd2694fea933b297cd58295497347dc

--- jdk.portable/tools/ChocolateyInstall.ps1
+++ jdk.portable/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'jdk'
-$Url64 = 'https://edelivery.oracle.com/otn-pub/java/jdk/13.0.1+9/cec27d702aa74d5a8630c65ae61e4305/jdk-13.0.1_windows-x64_bin.zip'
-$Checksum64 = 'fda3b4dfe72e857a5714c9719e4ccc957d35bbf8df99f47221e508ac09852d3d'
+$Url64 = 'https://edelivery.oracle.com/otn-pub/java/jdk/13.0.2+8/d4173c853231432d94f001e99d882ca7/jdk-13.0.2_windows-x64_bin.zip'
+$Checksum64 = '73cb9a7d389746cb01a546bae54e36feaf978930c1b49bd40e751a83417869d9'
 $ChecksumType64 = 'sha256'
 $FileName = (([IO.Path]::GetFileNameWithoutExtension($Url64)) -split "_")[0]
 $InstallationPath = Join-Path $(Get-ToolsLocation) $FileName
--- yuzu/tools/ChocolateyInstall.ps1
+++ yuzu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'yuzu'
-$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2906/yuzu-windows-mingw-20200114-0658b12.7z'
+$Url64 = 'https://github.com/yuzu-emu/yuzu-canary/releases/download/canary-2907/yuzu-windows-mingw-20200114-ffdb343.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
