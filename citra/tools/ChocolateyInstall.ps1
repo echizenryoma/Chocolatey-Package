9a611182111cb73d12908afde7d650daa619fa0e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-03-04 16:03:39 +0000

    AU: 2 updated - citra everything.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/845b2133fe6faa23f22bc3805e15640f8cea2cac

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1588/citra-windows-mingw-20200303-0d5bce7.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1589/citra-windows-mingw-20200303-4e33f0e.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- everything.portable/tools/ChocolateyInstall.ps1
+++ everything.portable/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'everything'
-$Url32 = 'https://www.voidtools.com/Everything-1.4.1.935.x86.zip'
-$Checksum32 = 'b0a5c0854a4ed33e0f3f10a0c58db434486f554d260cfdbe15c2aa2fa4bd67c5'
+$Url32 = 'https://www.voidtools.com/Everything-1.4.1.965.x86.zip'
+$Checksum32 = 'b39a274654a1937352142e1dde0b747d3fea320adc3b88c8242774542c8a1a3e'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://www.voidtools.com/Everything-1.4.1.935.x64.zip'
-$Checksum64 = 'e6e4524d55745d55e1672893304257c45f106bbe63718a8e6568e8cfd63ba571'
+$Url64 = 'https://www.voidtools.com/Everything-1.4.1.965.x64.zip'
+$Checksum64 = 'b2afe2fd0c239ac9688f5897b096823293cc38c1f629ee2d39b3230c2be3e792'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $UrlExtra = 'https://www.voidtools.com/es.exe'
