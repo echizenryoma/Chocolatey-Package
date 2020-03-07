678bf7da8ed10d6bb9c85e39417f3b401856fa11
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-03-07 16:05:12 +0000

    AU: 3 updated - cemu freeplane.portable mathpix.snip [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/53937f3d7b130bb1bcec3d2f5b4241d0949910a1

--- mathpix.snip/tools/ChocolateyInstall.ps1
+++ mathpix.snip/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $PackageName = 'mathpix.snip'
 $Url = 'https://mathpix.com/win_app/mathpix_snipping_tool_setup.exe'
-$Checksum = '7345D3F292C30F91D29E31F387A90610D99352C77AA8FC5CF86AB0DD351B7269'
+$Checksum = 'B07465DA320B68EFB621175175763CF1D8F95AD40DD430ED063296DDEE6270E9'
 $ChecksumType = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
