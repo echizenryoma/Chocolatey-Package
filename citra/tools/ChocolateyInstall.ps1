bd78221f819e2937b7b42bdbf26b6a6d3075b45c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-03-21 16:07:04 +0000

    AU: 2 updated - cemu citra [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/71ebbdec3324d73ad41044ce2faefa0299a69ec1

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1605/citra-windows-mingw-20200318-f6f16ce.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1606/citra-windows-mingw-20200321-330ca63.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
