7a9ff06bb76c09aa7f6ea393402d3853b577031a
ryomazhang <ryomazhang@tencent.com>
2020-04-04 14:18:28 +0800

    [notepad3.portable] Add

---
+++ notepad3.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'notepad3'
+$Url = 'https://github.com/rizonesoft/Notepad3/releases/download/RELEASE_5.19.815.2595/Notepad3_5.19.815.2595.zip'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+   PackageName   = $PackageName
+   Url           = $Url
+   UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+
+$Url32 = (Get-ChildItem -Path $ToolsPath | Where-Object Name -Match "x86.zip" | Select-Object -Last 1).FullName
+$Url64 = (Get-ChildItem -Path $ToolsPath | Where-Object Name -Match "x64.zip" | Select-Object -Last 1).FullName
+
+$PackageArgs = @{
+   PackageName   = $PackageName
+   Url           = $Url32
+   Url64         = $Url64
+   UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+Remove-Item -Path $(Join-Path $ToolsPath "*.zip") -WarningAction Ignore
---
+++ notepad3.portable/update.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $base = "https://github.com"
+    $url = $base + "/rizonesoft/Notepad3/releases/latest"
+    $page = Invoke-WebRequest -UseBasicParsing -Uri $url
+    $version = ($page.Links | Where-Object outerHTML -Match "Notepad3 Release")[0].href -split "_" -match "\d+(\.\d+)+"
+    $url32 = $base + ($page.Links.href -match "Notepad3_${version}.zip")[0]
+	
+    return @{
+        Version = $version
+        URL32   = $url32
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
