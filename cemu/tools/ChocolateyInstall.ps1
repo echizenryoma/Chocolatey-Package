a2cdc09c67e9fece858bf2603424738cc9285623
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-04-04 16:07:39 +0000

    AU: 3 updated - cemu citra mkvtoolnix.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/2cb3595ac6050ffc44d99e13b6ceb2a88ac711d6

--- cemu/tools/ChocolateyInstall.ps1
+++ cemu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'cemu'
-$Url64 = 'http://cemu.info/releases/cemu_1.17.4.zip'
+$Url64 = 'http://cemu.info/releases/cemu_1.18.0.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
