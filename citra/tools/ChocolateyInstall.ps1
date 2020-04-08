f6aed8586eda41dfab95de218ce84c6e1aaebd71
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-04-08 16:03:35 +0000

    AU: 2 updated - citra drawio.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/6230db055f82edea36f7ab79ab27879d8a660396

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1639/citra-windows-mingw-20200407-6b633d1.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1640/citra-windows-mingw-20200407-4f8f8e8.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
