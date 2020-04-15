e6dca2556cc80d79fab320e73e4d14f1045da4bc
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-04-15 16:05:39 +0000

    AU: 1 updated - notepad3.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/cc5d6e26ff7f093722fc705b3009174c1265bd39

--- notepad3.portable/tools/ChocolateyInstall.ps1
+++ notepad3.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'notepad3'
-$Url = 'https://github.com/rizonesoft/Notepad3/releases/download/RELEASE_5.20.411.2/Notepad3_5.20.411.2.zip'
+$Url = 'https://github.com/rizonesoft/Notepad3/releases/download/RELEASE_5.20.414.1/Notepad3_5.20.414.1.zip'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
