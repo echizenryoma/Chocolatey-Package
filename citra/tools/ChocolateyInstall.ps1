07cef06c3a9a539624e43567f8a093601fc10937
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-04-25 16:04:04 +0000

    AU: 2 updated - citra qbittorrent.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/5f81e1015728a20222d61383d74a4c1400da370a

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1693/citra-windows-mingw-20200424-bc7e243.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1695/citra-windows-mingw-20200424-874091b.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
