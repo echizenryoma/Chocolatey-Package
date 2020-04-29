5ab756011d8afe60d701a3f53b11b348ad69024a
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-04-29 16:06:37 +0000

    AU: 4 updated - citra drawio.portable fastcopy.portable nodejs.lts [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/18a086132e161e2883f6d8739f7a2fe185ac37cd

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1704/citra-windows-mingw-20200427-d6f4c63.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1708/citra-windows-mingw-20200429-4a2a2f6.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- nodejs.lts/tools/ChocolateyInstall.ps1
+++ nodejs.lts/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'nodejs'
-$Url32 = 'https://npm.taobao.org/mirrors/node/v12.16.2/node-v12.16.2-win-x86.7z'
-$Checksum32 = '3e047cd9d738d358a7e66c7f96874ab81199693ee3d7ed92d3e475becb5454a3'
+$Url32 = 'https://npm.taobao.org/mirrors/node/v12.16.3/node-v12.16.3-win-x86.7z'
+$Checksum32 = 'bbb46883746012693d6a4002281a62b5b401db0fb1961701e8e5b69f0b4c346b'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://npm.taobao.org/mirrors/node/v12.16.2/node-v12.16.2-win-x64.7z'
-$Checksum64 = '25a134821a5f45b7ef6f7eaf0196c60de90132cb280eea5497666f0b42740f37'
+$Url64 = 'https://npm.taobao.org/mirrors/node/v12.16.3/node-v12.16.3-win-x64.7z'
+$Checksum64 = '743962e097d40383fac3c2e727251400e60185b294dad2a894dbce3687710f10'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
