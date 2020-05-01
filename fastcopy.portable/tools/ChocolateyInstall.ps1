dfc2a150b69b2f3ef6b36207ca662768fe0b6143
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-05-01 16:03:17 +0000

    AU: 3 updated - cemu citra fastcopy.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/5a1098df8595d765600b36c13c6ec0cb92af8940

--- fastcopy.portable/tools/ChocolateyInstall.ps1
+++ fastcopy.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'fastcopy'
-$Url32 = 'https://fastcopy.jp/archive/FastCopy388_installer.exe'
+$Url32 = 'https://fastcopy.jp/archive/FastCopy389_installer.exe'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $SilentArgs = "/DIR=`"${ToolsPath}`" /EXTRACT"
 
