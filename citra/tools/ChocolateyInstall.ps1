70fdbee2ca1985a9cc36d835457c1010b91b8511
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-05-12 16:10:18 +0000

    AU: 3 updated - citra freeplane.portable tomcat.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/4e1ba274faecd797ff6afd7e6bda12a286f13302

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1744/citra-windows-mingw-20200511-8e9fce9.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1749/citra-windows-mingw-20200511-b78d27f.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
