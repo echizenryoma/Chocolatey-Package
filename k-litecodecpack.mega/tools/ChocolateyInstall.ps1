491b4f27f9f16f7a525da6a2f829b403181b0db0
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-05-18 16:06:01 +0000

    AU: 2 updated - citra k-litecodecpack.mega [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/f8ce46ac21493584c2fcf07cfe0df367243cf751

--- k-litecodecpack.mega/tools/ChocolateyInstall.ps1
+++ k-litecodecpack.mega/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'k-litecodecpack.mega'
-$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1544_Mega.exe'
-$Checksum32 = 'f227144ab611cbf894a140bd8f353b2e935a82c17b3a67e1ecbfd7d7242837d0'
+$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1548_Mega.exe'
+$Checksum32 = 'afd055c15d3e5b9742c0cae52687c131026c37a91bc7b93d30f123f39d00a04c'
 $ChecksumType32 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
