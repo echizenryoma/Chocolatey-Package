fbd36e5113dcb1e71733db7a4ac21259701ff0e5
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-05-23 03:46:46 +0000

    AU: 1 updated - calibre.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/a5e44bb752a970e886d7bf0ff71d1ef0d77d8c75

--- calibre.portable/tools/ChocolateyInstall.ps1
+++ calibre.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'Calibre'
-$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v4.16.0/calibre-portable-installer-4.16.0.exe'
+$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v4.17.0/calibre-portable-installer-4.17.0.exe'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 $SilentArgs = "${UnzipLocation}"
