3b70ca5daac359a2b0280c555486919311e9dbaf
ryomazhang <ryomazhang@tencent.com>
2020-05-23 10:52:53 +0800

    [inkscape.portable] Remove

--- inkscape.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$PackageName = 'InkScape'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $($PackageName.ToLower())
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-Uninstall-BinFile -Name $($PackageName.ToLower())
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
