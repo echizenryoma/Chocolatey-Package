ac67b53f8e2a7c2e5f40c109bc0536452f264d83
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-05-24 09:24:49 +0800

    [jdk] Remove

--- jdk.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿Uninstall-ChocolateyEnvironmentVariable -VariableName 'JAVA_HOME' -VariableType 'Machine'
-
-$EnvPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) -split ';' -notmatch 'jdk'
-[Environment]::SetEnvironmentVariable('Path', $EnvPath -join ';', [EnvironmentVariableTarget]::Machine)
