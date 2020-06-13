1a9064e45c376bb6cd54eec84c8330608863d95e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-06-13 16:04:03 +0000

    AU: 1 updated - cemu [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/bc52833b446fd92f4360d6781f79d9565429eaed

--- cemu/tools/ChocolateyInstall.ps1
+++ cemu/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'cemu'
-$Url64 = 'http://cemu.info/releases/cemu_1.19.1.zip'
+$Url64 = 'http://cemu.info/releases/cemu_1.19.2.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
