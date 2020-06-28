a4e1d902d099d1bd9931407fa6defc209d02c716
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-06-28 16:04:44 +0000

    AU: 2 updated - citra mkvtoolnix.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/74dd84b05200d3ba701eb7d9f6de6323e3a2643e

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1792/citra-windows-mingw-20200626-4093bd2.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1795/citra-windows-mingw-20200627-61f0e52.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- mkvtoolnix.portable/tools/ChocolateyInstall.ps1
+++ mkvtoolnix.portable/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'mkvtoolnix'
-$Url32 = 'https://mkvtoolnix.download/windows/releases/47.0.0/mkvtoolnix-32-bit-47.0.0.7z'
-$Url64 = 'https://mkvtoolnix.download/windows/releases/47.0.0/mkvtoolnix-64-bit-47.0.0.7z'
-$Checksum32 = '978b8f36f90d6fc2e392de77186e20e4820fa03f'
+$Url32 = 'https://mkvtoolnix.download/windows/releases/48.0.0/mkvtoolnix-32-bit-48.0.0.7z'
+$Url64 = 'https://mkvtoolnix.download/windows/releases/48.0.0/mkvtoolnix-64-bit-48.0.0.7z'
+$Checksum32 = 'bef4a9998a8ea8e85f06501383b3bc6d32aee38a'
 $ChecksumType32 = 'sha1'
-$Checksum64 = '039fd585b41ad41fb4fabcf1b5fc81a9d5efccd9'
+$Checksum64 = '99f2de061292bcba308f69532fd6b575b7084ffd'
 $ChecksumType64 = 'sha1'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
