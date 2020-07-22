bc0a6315648a950588d27b4f7da798514f924c10
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-07-22 16:03:32 +0000

    AU: 2 updated - citra notepad3.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/3f79d3e960a4d3285186a019ba6d09fad8ddf5c2

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1816/citra-windows-mingw-20200721-c13db93.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1821/citra-windows-mingw-20200722-a7fa0b2.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
