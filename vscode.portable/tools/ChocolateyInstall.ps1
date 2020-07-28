f15d22855f80fb35249e99afe0ade9e320f84b19
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-07-28 16:04:59 +0000

    AU: 1 updated - vscode.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/09778a9b0185907868db2fd1129ee7622cb77041

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'vscode'
-$Url32 = 'https://vscode-update.azurewebsites.net/1.47.2/win32-archive/stable'
+$Url32 = 'https://vscode-update.azurewebsites.net/1.47.3/win32-archive/stable'
 $ChecksumType32 = 'sha256'
-$Checksum32 = 'bc581c0dc027cf8eb57ed6ca33d0918078e4fcc7ec08e039a1711cb6a564e998'
-$Url64 = 'https://vscode-update.azurewebsites.net/1.47.2/win32-x64-archive/stable'
+$Checksum32 = 'ab0f9c757061e285c04f08bedf15b86cb0a788e5648c741b57638b12986a0575'
+$Url64 = 'https://vscode-update.azurewebsites.net/1.47.3/win32-x64-archive/stable'
 $ChecksumType64 = 'sha256'
-$Checksum64 = '17e357ee000ae15877582d013320b396ed00695a76843fb2a8ea4659ae0a4207'
+$Checksum64 = 'e5f11b54b5f9554aea982f9f4196ac49f749ed01c9ebc68cec1fb1653f7014cc'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $DataPath = Join-Path $InstallationPath 'data'
 
