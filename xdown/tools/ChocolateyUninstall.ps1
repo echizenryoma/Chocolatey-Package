2189a41234ac6ace4c02bf784aad37f37feb3b16
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-08-02 18:00:42 +0800

    Remove

--- xdown/tools/ChocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$PackageName = 'XDown'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName.ToLower()
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-Uninstall-BinFile -Name "${PackageName}"
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
