2e93bc324670a60a26e67583aa3dc945c9baefc5
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-08-18 00:46:50 +0000

    AU: 2 updated - citra freepascal [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/79a8488c0be81e82dd71e7297ddb2d49ef0c56bf

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1830/citra-windows-mingw-20200811-55b529b.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1831/citra-windows-mingw-20200817-d0ec7c0.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- freepascal/tools/ChocolateyInstall.ps1
+++ freepascal/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'freepascal'
-$Url32 = 'https://sourceforge.net/projects/freepascal/files/Win32/3.0.4/fpc-3.0.4.i386-win32.exe'
-$Url64 = 'https://sourceforge.net/projects/freepascal/files/Win32/3.0.4/fpc-3.0.4.i386-win32.cross.x86_64-win64.exe'
+$Url32 = 'https://sourceforge.net/projects/freepascal/files/Win32/3.2.0/fpc-3.2.0.i386-win32.exe'
+$Url64 = 'https://sourceforge.net/projects/freepascal/files/Win32/3.2.0/fpc-3.2.0.i386-win32.cross.x86_64-win64.exe'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
