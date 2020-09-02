7f3754032c07145e5f12a539762d09184132b297
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-09-02 16:05:42 +0000

    AU: 4 updated - citra foobar2000.portable freeplane.portable golang [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/80c8ef2ffa1ded47b4d980bf8e4809252e8d5fe3

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1842/citra-windows-mingw-20200830-745532a.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-1843/citra-windows-mingw-20200901-67126e3.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
