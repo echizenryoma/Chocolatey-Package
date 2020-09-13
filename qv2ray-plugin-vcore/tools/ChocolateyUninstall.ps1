b1f3d84a4887c6f20af3a2064a03bfa33eed2e96
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-09-13 13:56:38 +0800

    [qv2ray-plugin-trojan] add

--- qv2ray-plugin-vcore/tools/ChocolateyUninstall.ps1
+++ qv2ray-plugin-vcore/tools/ChocolateyUninstall.ps1
@@ -1,9 +1,10 @@
 ﻿$PackageName = 'qv2ray'
+
+. $(Join-Path $ToolsPath "StopProcess.ps1")
+
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $Qv2rayPath = Join-Path $(Get-ToolsLocation) $PackageName
 $Qv2rayConfigPath = Join-Path $Qv2rayPath 'config'
 $InstallationPath = Join-Path $Qv2rayConfigPath 'vcore'
-
-. $(Join-Path $ToolsPath "StopProcess.ps1")
 
 Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
