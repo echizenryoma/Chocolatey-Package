e7d74c5b4ee910a2fa1fad858329d35b6b98710f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-09-13 08:40:03 +0800

    [qv2ray-plugin-vcore] fix path

--- qv2ray-plugin-vcore/tools/ChocolateyInstall.ps1
+++ qv2ray-plugin-vcore/tools/ChocolateyInstall.ps1
@@ -6,17 +6,19 @@ $Url64 = 'https://github.com/v2fly/v2ray-core/releases/download/v4.28.1/v2ray-wi
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $Qv2rayPath = Join-Path $(Get-ToolsLocation) $PackageName
 $Qv2rayConfigPath = Join-Path $Qv2rayPath 'config'
-$InstallationPath = Join-Path $Qv2rayConfigPath 'vcore'
+$UnzipLocation = Join-Path $Qv2rayConfigPath 'vcore'
+
+Write-Output $UnzipLocation
 
 . $(Join-Path $ToolsPath "StopProcess.ps1")
 
-New-Item -ItemType Directory -Path $InstallationPath -Force -ErrorAction Ignore
+New-Item -ItemType Directory -Path $UnzipLocation -Force -ErrorAction Ignore
 
 $PackageArgs = @{
     PackageName   = $PackageName
     Url           = $Url32
     Url64         = $Url64
-    UnzipLocation = $InstallationPath
+    UnzipLocation = $UnzipLocation
 }
 Install-ChocolateyZipPackage @PackageArgs
 
