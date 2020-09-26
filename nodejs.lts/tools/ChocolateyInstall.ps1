873456d1927bd407cdf2a85cf6c6800816061c24
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-09-26 09:36:34 +0800

    [nodejs.lts] fix url

--- nodejs.lts/tools/ChocolateyInstall.ps1
+++ nodejs.lts/tools/ChocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'nodejs'
-$Url32 = 'https://npm.taobao.org/mirrors/node/v12.18.4/node-v12.18.4-win-x86.7z'
+$Url32 = 'https://nodejs.org/dist/v12.18.4/node-v12.18.4-win-x86.7z'
 $Checksum32 = '1ffb83bdc45ec54ef2663dfcbadcbebc6907a4f88a90cae818c999864aa501db'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://npm.taobao.org/mirrors/node/v12.18.4/node-v12.18.4-win-x64.7z'
+$Url64 = 'https://nodejs.org/dist/v12.18.4/node-v12.18.4-win-x64.7z'
 $Checksum64 = 'ee974de37139ecf68e8b0d9ae029f3424a5ddba7f338aa936c877e863793702b'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
--- nodejs.lts/update.ps1
+++ nodejs.lts/update.ps1
@@ -17,11 +17,11 @@ function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "https://nodejs.org/en/download/"
     $url = $page.Links.href -match ".zip$" | Select-Object -First 1
     $version = [IO.Path]::GetFileNameWithoutExtension($url) -split "v|-" -match "^\d+(\.\d+)+$" | Select-Object -First 1
-    
-    $url32 = "https://npm.taobao.org/mirrors/node/v${version}/node-v${version}-win-x86.7z"
-    $url64 = "https://npm.taobao.org/mirrors/node/v${version}/node-v${version}-win-x64.7z"
 
-    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://npm.taobao.org/mirrors/node/v${version}/SHASUMS256.txt"
+    $url32 = "https://nodejs.org/dist/v${version}/node-v${version}-win-x86.7z"
+    $url64 = "https://nodejs.org/dist/v${version}/node-v${version}-win-x64.7z"
+
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://nodejs.org/dist/v${version}/SHASUMS256.txt"
     $obj = $page.Content -split "\n" | ConvertFrom-String -PropertyNames sha256, file
     $checksum_type = 'sha256'
     $checksum32 = $obj | Where-Object file -Match "win-x86.7z$" | Select-Object -First 1 -ExpandProperty sha256
@@ -31,10 +31,10 @@ function global:au_GetLatest {
         Version        = $version
         URL32          = $url32
         Checksum32     = $checksum32
-        ChecksumType32 = $checksum_type 
+        ChecksumType32 = $checksum_type
         URL64          = $url64
         Checksum64     = $checksum64
-        ChecksumType64 = $checksum_type 
+        ChecksumType64 = $checksum_type
     }
 }
 
