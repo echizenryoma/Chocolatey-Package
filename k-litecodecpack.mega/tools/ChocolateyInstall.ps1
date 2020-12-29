4b32715d5711387e06550a5cc11a8541350d8d21
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2020-12-29 16:07:06 +0000

    AU: 2 updated - k-litecodecpack.mega x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0

--- k-litecodecpack.mega/tools/ChocolateyInstall.ps1
+++ k-litecodecpack.mega/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'k-litecodecpack.mega'
-$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1590_Mega.exe'
-$Checksum32 = '7439604e1375483b9e725f25e705bcfeb995eada13d68f46262e738987de3f3e'
+$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1595_Mega.exe'
+$Checksum32 = '9f58a599189f8ec7c5e98711560a44e4da01533a89fe3644476b75ef11962e32'
 $ChecksumType32 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
