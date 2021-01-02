1d2cbbf44d58a31e07bae023d677e798d4e453f8
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-01-02 16:04:45 +0000

    AU: 2 updated - cemu scrcpy [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0

--- scrcpy/tools/ChocolateyInstall.ps1
+++ scrcpy/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'scrcpy'
-$Url32 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.16/scrcpy-win32-v1.16.zip'
-$Checksum32 = 'b606e7a04212e35ae39a14f5943e4804ed83eb99f794ddc569d4179a1d9c0272'
+$Url32 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.17/scrcpy-win32-v1.17.zip'
+$Checksum32 = '9a964721c67cc2af0c89d99c2264b2ccb630f004317cbd1611a9256018456878'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.16/scrcpy-win64-v1.16.zip'
-$Checksum64 = '3f30dc5db1a2f95c2b40a0f5de91ec1642d9f53799250a8c529bc882bc0918f0'
+$Url64 = 'https://github.com/Genymobile/scrcpy/releases/download/v1.17/scrcpy-win64-v1.17.zip'
+$Checksum64 = '8b9e57993c707367ed10ebfe0e1ef563c7a29d9af4a355cd8b6a52a317c73eea'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
