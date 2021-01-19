092034d1e306012f0ab183ead1301fc751504cef
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-01-19 16:04:51 +0000

    AU: 1 updated - everything.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0

--- everything.portable/tools/ChocolateyInstall.ps1
+++ everything.portable/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'everything'
-$Url32 = 'https://www.voidtools.com/Everything-1.4.1.1003.x86.zip'
-$Checksum32 = 'a56c7b7960b1e6520ecf81ef2629cb12c6d0fffc5d3e9d114995e4a67ab80ec9'
+$Url32 = 'https://www.voidtools.com/Everything-1.4.1.1004.x86.zip'
+$Checksum32 = '877e67065781878d95e89dcfd879b62fba8343f046fefbaa5aa05aca7b19468a'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://www.voidtools.com/Everything-1.4.1.1003.x64.zip'
-$Checksum64 = '4a067fa79d1ed1d49947c76984e804cc9ad9f0a9325987af4c5db8e4b82092e0'
+$Url64 = 'https://www.voidtools.com/Everything-1.4.1.1004.x64.zip'
+$Checksum64 = 'b5750368ba7fc474c0b0fdd6212dfd28211b08a0aedb91558e01c3f6acfbf7a5'
 $ChecksumType64 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $UrlExtra = 'https://www.voidtools.com/es.exe'
