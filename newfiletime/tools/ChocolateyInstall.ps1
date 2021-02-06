f218226aef9b936132281dacbaed9179b147a3bd
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-02-06 10:41:28 +0800

    update

--- newfiletime/tools/ChocolateyInstall.ps1
+++ newfiletime/tools/ChocolateyInstall.ps1
@@ -5,10 +5,10 @@ $Url32 = 'https://www.softwareok.com/Download/NewFileTime.zip'
 $Checksum32 = 'e9fc83485ab112cc19763664e5a5a6ba78413f1e0368ca0348ed17da7e6cb44c'
 $ChecksumType32 = 'sha256'
 $Url64 = 'https://www.softwareok.com/Download/NewFileTime_x64.zip'
-$Checksum64 = 'f33abc44f293b9c541851066e47a57d99479692c3a9f6492a690bdb42afa39a2'
+$Checksum64 = 'e686f279784515ece8d0f21ad5bf959c1a6fc77f1274848a6b5ec32487818529'
 $ChecksumType64 = 'sha256'
 $UrlExtra = 'https://www.softwareok.com/Download/NewFileTime_Unicode.zip'
-$ChecksumExtra = '8ddcf6a3e3e0908fe3734ecf792781cd6bf54e291f4438c17409f4439c92f563'
+$ChecksumExtra = '8f8abe948fefeae98a39c09fab8b7192b3a74ce495dd34d901d72d1de323e54d'
 $ChecksumTypeExtra = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
--- resourcehacker.portable/update.ps1
+++ resourcehacker.portable/update.ps1
@@ -11,7 +11,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "http://www.angusj.com/resourcehacker"
-    $version = ($page.Content -split "\n|<|>" -match "^Version\s+\d+(\.\d+)+$" | Select-Object -First 1) -split "\s" -match "\d+(\.\d+)+" | Select-Object -First 1
+    $version = ((($page.Content -split "\n|<|>" -match "^Version\s+\d+(\.\d+)+\s+\(\d+\)$")[0] -replace " \(", "." -replace "\)", "") -split "\s" -match "\d+(\.\d+)+")[0]
 
     return @{
         Version = $version
