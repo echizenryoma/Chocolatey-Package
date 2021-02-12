9ae7bacc62e13dbe99335e355d46b9f8e4d056a9
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-02-12 19:45:53 +0800

    [sourcetree] Remove

--- sourcetree.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$PackageName = 'sourcetree'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-Uninstall-BinFile -Name $PackageName
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "Source Tree.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
