fbecd81fab47b9cd36ba455d1a3679ba846978fd
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-03-10 16:06:37 +0000

    AU: 2 updated - calibre.portable newfiletime [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0

--- newfiletime/tools/ChocolateyInstall.ps1
+++ newfiletime/tools/ChocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $PackageName = 'newfiletime'
 $Url32 = 'https://www.softwareok.com/Download/NewFileTime.zip'
-$Checksum32 = '7e4023aaab319eea7c5532de3346cea3746d2b548066d3116f7645d9bcd3241c'
+$Checksum32 = '1c924672435923064db350a6b64439ee88d277690fac0858b45f5c09cdff1ddb'
 $ChecksumType32 = 'sha256'
 $Url64 = 'https://www.softwareok.com/Download/NewFileTime_x64.zip'
-$Checksum64 = '9ec036f53cbd7d6de6b40c034e2afc7e70f81912b485296b943305557a57b751'
+$Checksum64 = '474fe91b357905cbd600c17cca94587e3825d97bdb5f6e9828b6f50a9d612791'
 $ChecksumType64 = 'sha256'
 $UrlExtra = 'https://www.softwareok.com/Download/NewFileTime_Unicode.zip'
-$ChecksumExtra = 'deab99941b15144e10be0d6a8a09dc099eb0656212f9de427309b043dbf516db'
+$ChecksumExtra = 'c8503493c0d339f869fa4e62d8a2ae6b3c80844de75480e1900154e1db906c5f'
 $ChecksumTypeExtra = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
