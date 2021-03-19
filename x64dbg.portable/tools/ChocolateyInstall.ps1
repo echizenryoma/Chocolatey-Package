d9f3424df93f1ae665cc5ffdec9ef9774c18be27
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-03-19 16:05:31 +0000

    AU: 1 updated - x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0

--- x64dbg.portable/tools/ChocolateyInstall.ps1
+++ x64dbg.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'x64dbg'
-$Url32 = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2021-03-16_01-19.zip'
+$Url32 = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2021-03-19_10-45.zip'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 . $(Join-Path $ToolsPath "StopProcess.ps1")
