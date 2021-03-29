b0bd434321ea33ebe700c7d3fd71335d46435524
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-03-29 16:06:08 +0000

    AU: 1 updated - filezilla.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0

--- filezilla.portable/tools/ChocolateyInstall.ps1
+++ filezilla.portable/tools/ChocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $PackageName = 'FileZilla'
 $Page = Invoke-WebRequest -UseBasicParsing -Uri 'https://filezilla-project.org/download.php?show_all=1'
 $Url32 = $page.Links.href -match "win32\.zip" | Select-Object -First 1
-$Checksum32 = '8eaaaf4a37442824c634d2e3e66173548bc7abb1cf5b424aff0c6f6d3d175948bd030b67d67a04da5face3b12c4d1e8639dc2a759f4201e49e4edd0240a0d3fb'
+$Checksum32 = 'ad45ab37b4ae527e85675b56aad15743d1392088cd6d3eee550ff109a888d90acbe88ddc9b64d27f05d4f754992f15e47009acbc7e0942f5fc511b14e6eb5fcf'
 $ChecksumType32 = 'sha512'
 $Url64 = $page.Links.href -match "win64\.zip" | Select-Object -First 1
-$Checksum64 = '5fee8b7d33cf8d20b36bac6f90fd6d13b5d7b3ab45023c9ab693db877841a4a23e65e10b6df7ae7720a3f9faa7aac7a17fa8574d04f6d3e83ac6944bd99d55b5'
+$Checksum64 = '90ef7a85a02c19fb24e897f8cca5cb0008aef6a625b8e2ffe4435c27ffabc3a4d27f20943940c123e590177ce118b3aa6b41e0972344807e9913a9c4a8a96c7e'
 $ChecksumType64 = 'sha512'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
