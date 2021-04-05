2255b5679947f864c2c0a0ce16ba7c7d6927d7d0
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-04-05 09:54:18 +0800

    [cmder.mini] Remove

--- cmder.mini/tools/ChocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$PackageName = 'cmder'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-Uninstall-BinFile -Name 'Cmder'
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "Cmder.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
