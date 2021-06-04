00b8552f980fb7533474fbc5f59cf35087e34381
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-06-04 01:21:09 +0000

    AU: 1 updated - golang [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0

--- golang/tools/ChocolateyInstall.ps1
+++ golang/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'go'
-$Url32 = 'https://storage.googleapis.com/golang/go1.16.4.windows-386.zip'
-$Url64 = 'https://storage.googleapis.com/golang/go1.16.4.windows-amd64.zip'
+$Url32 = 'https://storage.googleapis.com/golang/go1.16.5.windows-386.zip'
+$Url64 = 'https://storage.googleapis.com/golang/go1.16.5.windows-amd64.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
