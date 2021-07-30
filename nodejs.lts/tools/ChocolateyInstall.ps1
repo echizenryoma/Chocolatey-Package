57e137fe7617533ce40297f8a183ccd15744d1af
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-07-30 16:04:40 +0000

    AU: 2 updated - calibre.portable nodejs.lts [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/ed901465d9de3a2920314299e4f80982a3d4c48a

--- nodejs.lts/tools/ChocolateyInstall.ps1
+++ nodejs.lts/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'nodejs'
-$Url32 = 'https://nodejs.org/dist/v14.17.3/node-v14.17.3-win-x86.7z'
-$Checksum32 = '3fe56227a2f3ed935e62a9236eb7f1cc21abc7d1b26488c697bd6ab1381b9022'
+$Url32 = 'https://nodejs.org/dist/v14.17.4/node-v14.17.4-win-x86.7z'
+$Checksum32 = '67caaa209d2d938f763f1a9ab08b3e30c06b2f18bf5c5d90b1198d0ddbd35feb'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://nodejs.org/dist/v14.17.3/node-v14.17.3-win-x64.7z'
-$Checksum64 = '7d9b143760100ae12eb8e000f0bce7a23fe65b63e428802935fe5ddf7887141d'
+$Url64 = 'https://nodejs.org/dist/v14.17.4/node-v14.17.4-win-x64.7z'
+$Checksum64 = '0de71309336bc324bc5155867dc9d8d6337d83c1eed4777141ae83e967b3aca1'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
