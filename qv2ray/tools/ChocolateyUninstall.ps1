21b22f0d6d3f9013b70c4be76e5c9c9d5de50c16
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-08-21 15:40:13 +0800

    [qv2ray] remove

--- qv2ray/tools/ChocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$PackageName = 'qv2ray'
-$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-
-. $(Join-Path $ToolsPath "StopProcess.ps1")
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
