2dff7bb5265233f48411ded9d428875b56e23d6c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-09-09 23:59:32 +0800

    [macast] Add

---
+++ macast/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'macast'
+$Url = 'https://github.com/xfangfang/Macast/releases/download/v0.5/Macast-v0.5.exe'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+. $(Join-Path $ToolsPath "StopProcess.ps1")
+
+$BinFile = Join-Path $ToolsPath "${PackageName}.exe"
+
+$PackageArgs = @{
+    PackageName  = $PackageName
+    Url          = $Url
+    FileFullPath = $BinFile
+}
+Get-ChocolateyWebFile @PackageArgs
+
+Write-Output "$PackageName has been installed in $BinFile"
+
+Start-Process -FilePath $BinFile
---
+++ macast/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$PackageName = 'macast'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+
+. $(Join-Path $ToolsPath "StopProcess.ps1")
+
+Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
