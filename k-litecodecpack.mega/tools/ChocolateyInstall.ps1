7a4514c022827d456412cbb8208ae4e14177697e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-09-15 16:06:36 +0000

    AU: 4 updated - drawio.portable k-litecodecpack.mega macast openjdk [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/db3f307fe5f379e6671d63b9c1c23e0f231c195f

--- k-litecodecpack.mega/tools/ChocolateyInstall.ps1
+++ k-litecodecpack.mega/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'k-litecodecpack.mega'
-$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1640_Mega.exe'
-$Checksum32 = '60f8117fee204df800189bf1b945dbc3df313796cfe1e4e638cf25e948aa3495'
+$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1645_Mega.exe'
+$Checksum32 = 'a82261b7a50bb3caa9e2a642340d8d3be8d922bf8921abc53655b51135bcb7e4'
 $ChecksumType32 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
--- macast/tools/ChocolateyInstall.ps1
+++ macast/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'macast'
-$Url = 'https://github.com/xfangfang/Macast/releases/download/v0.61/Macast-Windows-v0.61.exe'
+$Url = 'https://github.com/xfangfang/Macast/releases/download/v0.64/Macast-Windows-v0.64.exe'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 . $(Join-Path $ToolsPath "StopProcess.ps1")
