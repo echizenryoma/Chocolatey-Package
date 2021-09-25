33800a0ce277e5e0515c851827534d4aea6b6f3a
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-09-25 16:06:21 +0000

    AU: 1 updated - freeplane.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/ff36e9fd4ebf9877f8ce24001157de7086da0ed3

--- freeplane.portable/tools/ChocolateyInstall.ps1
+++ freeplane.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'freeplane'
-$Url32 = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/freeplane_bin-1.9.8.zip'
+$Url32 = 'https://sourceforge.net/projects/freeplane/files/freeplane%20stable/freeplane_bin-1.9.9.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
