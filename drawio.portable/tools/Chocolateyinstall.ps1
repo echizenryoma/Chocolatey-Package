f4679c16422bde906b9ed274c299bce5bb68b093
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-09-29 10:31:49 +0000

    AU: 1 updated - drawio.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/fef435299eefbc46daea04b6db0c82ab29882d59

--- drawio.portable/tools/Chocolateyinstall.ps1
+++ drawio.portable/tools/Chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'drawio'
-$Url64 = 'https://github.com/jgraph/drawio-desktop/releases/download/v15.3.5/draw.io-15.3.5-windows-no-installer.exe'
+$Url64 = 'https://github.com/jgraph/drawio-desktop/releases/download/v15.3.7/draw.io-15.3.7-windows-no-installer.exe'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
