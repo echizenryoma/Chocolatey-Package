404c746b6a3e2094771a9bab27ef72a8b732ee5d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-10-01 11:31:23 +0800

    [notepad3.portable] fix update.ps1

--- notepad3.portable/update.ps1
+++ notepad3.portable/update.ps1
@@ -9,11 +9,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $base = "https://github.com"
-    $url = $base + "/rizonesoft/Notepad3/releases/latest"
+    $url = 'https://api.github.com/repos/rizonesoft/Notepad3/releases/latest'
     $page = Invoke-WebRequest -UseBasicParsing -Uri $url
-    $version = ($page.Links | Where-Object outerHTML -Match "Notepad3 Release")[0].href -split "_" -match "\d+(\.\d+)+"
-    $url32 = $base + ($page.Links.href -match "Notepad3_${version}.zip")[0]
+    $json = $page.Content | ConvertFrom-Json
+
+    $url32 = ($json.assets | Where-Object { $_.name -match "zip" -and $_.name -notmatch "Setup" })[0].browser_download_url
+    $version = [version](($json.name -split "_" -match "\d+(\.\d+)+")[0])
 	
     return @{
         Version = $version
