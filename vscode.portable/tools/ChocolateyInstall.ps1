0586424c14bed77de5d05c072a2cf1e69fa68e2d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-10-08 16:07:32 +0000

    AU: 3 updated - calibre.portable golang.portable vscode.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/9f90749dd997d2f01a4ae840387a976aa9e6e441

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'vscode'
-$Url32 = 'https://update.code.visualstudio.com/1.60.2/win32-archive/stable'
+$Url32 = 'https://update.code.visualstudio.com/1.61.0/win32-archive/stable'
 $ChecksumType32 = 'sha256'
-$Checksum32 = '48f0afe214e08c1a01b858ab1dc6391810d731389276e6caf04490b956f95510'
-$Url64 = 'https://update.code.visualstudio.com/1.60.2/win32-x64-archive/stable'
+$Checksum32 = 'aa7f9dff54df5cad6447f2e4acfe9d692a1d7eabf865b753711c0de29441b7bc'
+$Url64 = 'https://update.code.visualstudio.com/1.61.0/win32-x64-archive/stable'
 $ChecksumType64 = 'sha256'
-$Checksum64 = '64087d63fe17bd649995acdb6d2755a04dbe1943243fc71b499610ccdf10d5c1'
+$Checksum64 = 'cbfaf43c3fc87ae743363feaf595fbb8818517eedc0552d14dea473d00a33ca1'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $DataPath = Join-Path $InstallationPath 'data'
 
