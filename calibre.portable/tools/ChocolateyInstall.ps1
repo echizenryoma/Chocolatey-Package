82e55314d9033c7d23fe80374226931054fd656c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-10-22 16:07:09 +0000

    AU: 2 updated - calibre.portable qv2ray-plugin-vcore [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/bc9c9c84c311798dc34d9d3907669dfae87efe92

--- calibre.portable/tools/ChocolateyInstall.ps1
+++ calibre.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'Calibre'
-$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v5.29.0/calibre-portable-installer-5.29.0.exe'
+$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v5.30.0/calibre-portable-installer-5.30.0.exe'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 $SilentArgs = "${UnzipLocation}"
