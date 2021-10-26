02520717c8580fae42ebda0a206dacb0d31412d5
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-10-26 16:05:56 +0000

    AU: 1 updated - postman.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/4f0fe8f20f04ec8c4571ea7a16de22f4d69eaead

--- postman.portable/tools/ChocolateyInstall.ps1
+++ postman.portable/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'postman'
-$Url32 = 'https://dl.pstmn.io/download/9.1.1/Postman-win32-9.1.1-full.nupkg'
-$Checksum32 = '73505FE0FCD44C8F3C9A77BF36920EE07EF86914'
+$Url32 = 'https://dl.pstmn.io/download/9.1.2/Postman-win32-9.1.2-full.nupkg'
+$Checksum32 = '0A4E446FBA941566E652A164BB483CDAD77F8301'
 $ChecksumType32 = 'sha1'
-$Url64 = 'https://dl.pstmn.io/download/9.1.1/Postman-win64-9.1.1-full.nupkg'
-$Checksum64 = '4D28D50ADBED9E33CA86F953F3B542C5F1466942'
+$Url64 = 'https://dl.pstmn.io/download/9.1.2/Postman-win64-9.1.2-full.nupkg'
+$Checksum64 = '696F8E67ED4CB49C7CBC1B0BA405945A3404D48C'
 $ChecksumType64 = 'sha1'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
