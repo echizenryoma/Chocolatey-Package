a5bcbe93d43b82d1a99fac856f3c2c0be3aa9a8b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-11-01 16:05:15 +0000

    AU: 3 updated - citra qbittorrent.portable x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/ec8eb820818e9095cc2c86def0dc5accc32e4111

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2001/citra-windows-mingw-20211023-5a0c4bf.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2003/citra-windows-mingw-20211101-c2ae292.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
