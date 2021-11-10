e1745b4868b3020a47ff9477b3789f4240be0352
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2021-11-10 21:33:03 +0800

    [ffmpeg] Add

---
+++ ffmpeg/tools/ChocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'ffmpeg'
+$Url = 'https://github.com/GyanD/codexffmpeg/releases/download/4.4.1/ffmpeg-4.4.1-full_build-shared.7z'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Get-ChildItem -Directory $ToolsPath | Remove-Item -Recurse -Force -ErrorAction Ignore
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$UnzipLocation = (Get-ChildItem -Directory -Path $ToolsPath | Select-Object -Last 1).FullName
+Rename-Item -Path $UnzipLocation -NewName $PackageName -Force
