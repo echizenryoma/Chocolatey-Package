d83bf162cd5843cedb4fbb7af6efd0c96f6ab432
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-01-11 14:58:43 +0800

    [sslscan] Add

---
+++ sslscan/tools/ChocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'dog'
+$Url32 = 'https://github.com/rbsec/sslscan/releases/download/2.0.11/sslscan-win-2.0.11.zip'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url32
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
