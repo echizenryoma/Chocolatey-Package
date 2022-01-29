c76cb49e2b8138add09b18fc1d40261b23097955
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-01-29 16:07:04 +0000

    AU: 2 updated - citra gmkvextractgui [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/480d0dc98ea7d8959c23e8102e652ffdab3f6aac

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2035/citra-windows-mingw-20220128-5fb77b2.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2036/citra-windows-mingw-20220129-c522009.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
