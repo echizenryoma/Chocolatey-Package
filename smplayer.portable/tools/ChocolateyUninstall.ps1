8676e4206484487a6e6631403e2552b3ba3b38f0
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-02-01 19:41:44 +0800

    [smplayer.portable] Remove

--- smplayer.portable/tools/ChocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$PackageName = 'smplayer'
-
-$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
-Remove-Item $LinkPath -Force -ErrorAction Ignore
-Uninstall-BinFile -Name ${PackageName}
