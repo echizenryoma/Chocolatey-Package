97e3ef0917362652e3d7e82785a453e5df3d6a1b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-02-20 13:05:18 +0800

    [nirlauncher.zh_CN] Add

---
+++ nirlauncher.zh_CN/tools/ChocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'NirSoft'
+$Url = 'https://download.nirsoft.net/nirsoft_package_schinese.zip'
+$Checksum = '071d89c3937afa62fbf907668e932a6dfa94d3df019098fa9ac727da45d318bd'
+$ChecksumType = 'sha256'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$TmpLocation = Join-Path $InstallationPath 'tmp'
+$UnzipLocation = Join-Path $TmpLocation 'language'
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    Checksum      = $Checksum
+    ChecksumType  = $ChecksumType
+    UnzipLocation = $UnzipLocation
+    Options       = @{
+        Headers = @{
+            Referer = 'https://launcher.nirsoft.net/'
+        }
+    }
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$NirSoftPath = Join-Path $InstallationPath 'NirSoft'
+Copy-Item -Path $(Join-Path $UnzipLocation "*") -Destination $NirSoftPath -Force -Recurse
+$NirSoft64Path = Join-Path $NirSoftPath 'x64'
+Copy-Item -Path $(Join-Path $UnzipLocation "*") -Destination $NirSoft64Path -Force -Recurse
+
+Remove-Item $TmpLocation -Recurse -Force -ErrorAction Ignore
