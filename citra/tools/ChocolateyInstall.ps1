36b4ec4e3dceda9353dde561fba81f810b7c2359
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-02-28 16:06:46 +0000

    AU: 1 updated - citra [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/b66a8aa52747e0682e1eefcbe31847c59f906b32

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2065/citra-windows-mingw-20220227-10fd1c5.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2066/citra-windows-mingw-20220228-a618748.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
