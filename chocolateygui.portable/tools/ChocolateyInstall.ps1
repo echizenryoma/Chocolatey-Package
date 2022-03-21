390a155846d74d9b149ed78bccdc968e5661af80
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-03-21 16:04:44 +0000

    AU: 4 updated - chocolateygui.portable citra fastcopy.portable x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/23e0a64f2f4413f87f2bf1cfe7d006817c922af7

--- chocolateygui.portable/tools/ChocolateyInstall.ps1
+++ chocolateygui.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'ChocolateyGUI'
-$Url = 'https://github.com/chocolatey/ChocolateyGUI/releases/download/0.20.0/ChocolateyGUI.msi'
+$Url = 'https://github.com/chocolatey/ChocolateyGUI/releases/download/1.0.0/ChocolateyGUI.msi'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $TmpLocation = Join-Path $InstallationPath 'tmp'
--- x64dbg.portable/tools/ChocolateyInstall.ps1
+++ x64dbg.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'x64dbg'
-$Url32 = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2022-03-02_15-55.zip'
+$Url32 = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2022-03-21_14-08.zip'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 . $(Join-Path $ToolsPath "StopProcess.ps1")
