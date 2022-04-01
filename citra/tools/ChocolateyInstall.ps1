1d01d0b0de9d73fffc9b7a9880bfa5b1da644b50
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-04-01 16:05:05 +0000

    AU: 3 updated - calibre.portable citra fsviewer.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/8fbfd41d4b1d456a4a2d8c45acf65a2f788a512e

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2097/citra-windows-mingw-20220331-17a444c.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2098/citra-windows-mingw-20220401-d4b55cb.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- fsviewer.portable/tools/ChocolateyInstall.ps1
+++ fsviewer.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'fsview'
-$Url32 = 'https://www.faststonesoft.net/DN/FSViewer75.zip'
+$Url32 = 'https://www.faststonesoft.net/DN/FSViewer76.zip'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 Get-ChildItem -Directory $ToolsPath | Remove-Item -Recurse -Force -ErrorAction Ignore
