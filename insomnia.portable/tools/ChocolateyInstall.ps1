be671f4369f45745e48a5d04a24c423a958c91fb
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-04-12 07:35:29 +0800

    [insomnia.portable] Add

---
+++ insomnia.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'Insomnia'
+$Url64 = 'https://github.com/Kong/insomnia/releases/download/core%402022.2.1/insomnia-2022.2.1-full.nupkg'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$UnzipLocation = Join-Path $InstallationPath 'tmp'
+
+$PackageArgs = @{
+    PackageName    = $PackageName
+    Url64          = $Url64
+    UnzipLocation  = $UnzipLocation
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$UnzipPath = (Get-ChildItem $UnzipLocation -Directory -Recurse | Where-Object Name -Match "net" | Select-Object -First 1).FullName
+Copy-Item -Path $(Join-Path $UnzipPath "*") -Destination $InstallationPath -Recurse -Force
+Remove-Item -Path $UnzipLocation -Recurse -Force -ErrorAction Ignore
+
+$BinFileName = (Get-ChildItem $InstallationPath -File | Where-Object Name -Match $PackageName | Select-Object -First 1).FullName
+Install-BinFile -Name "${PackageName}" -Path $BinFileName
+$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
+Install-ChocolateyShortcut -ShortcutFilePath $LinkPath -TargetPath $BinFileName -WorkingDirectory $InstallationPath
