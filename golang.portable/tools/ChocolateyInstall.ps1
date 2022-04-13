1df31e2acc808e33837597d4c30168b6dbd94510
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-04-13 16:04:39 +0000

    AU: 1 updated - golang.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/83572c2768d13026e37af111bd2a7062c638fd08

--- golang.portable/tools/ChocolateyInstall.ps1
+++ golang.portable/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'go'
-$Url32 = 'https://storage.googleapis.com/golang/go1.18.windows-386.zip'
-$Url64 = 'https://storage.googleapis.com/golang/go1.18.windows-amd64.zip'
+$Url32 = 'https://storage.googleapis.com/golang/go1.18.1.windows-386.zip'
+$Url64 = 'https://storage.googleapis.com/golang/go1.18.1.windows-amd64.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
