ce001e38b4e2e1dc254b0432346a1f115b520346
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-04-16 16:02:30 +0000

    AU: 1 updated - k-litecodecpack.mega [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/0bfccb7344443079c4079bd621f755fbd7c4fd3c

--- k-litecodecpack.mega/tools/ChocolateyInstall.ps1
+++ k-litecodecpack.mega/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'k-litecodecpack.mega'
-$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1695_Mega.exe'
-$Checksum32 = '979d88059a3c16fb73b0d76f1231bc34d293fc11e00fdeab5701bcf2f1852579'
+$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1698_Mega.exe'
+$Checksum32 = '2dbfae82e8465c92e6eb8c7114da568a952165f758d8a33883883d8d21bb07a3'
 $ChecksumType32 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
