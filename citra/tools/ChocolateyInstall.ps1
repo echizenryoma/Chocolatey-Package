7bc263744ec9678d8aceefd247b97a7263ff2e27
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-04-22 16:04:26 +0000

    AU: 2 updated - calibre.portable citra [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/afcc07f3f14d1a2b2cb8848a429947803d3b547b

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2118/citra-windows-mingw-20220420-17630ed.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2119/citra-windows-mingw-20220422-b3f246c.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
