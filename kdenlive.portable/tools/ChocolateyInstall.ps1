86a881277e6cf4b84ae5cb04584f280bd737b8fd
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-04-23 19:05:03 +0800

    [kdenlive.portable] Add

---
+++ kdenlive.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'kdenlive'
+$Url64 = 'https://download.kde.org/stable/kdenlive/21.12/windows/kdenlive-21.12.3_standalone.exe'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$TmpPath = Join-Path $InstallationPath "tmp"
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url64         = $Url64
+    UnzipLocation = $TmpPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$AppPath = (Get-ChildItem -Path $TmpPath -Directory | Where-Object Name -Match "${PackageName}")[0].FullName
+Write-Output "Installing $AppPath"
+Get-ChildItem -Path $AppPath -Recurse | Move-Item -Destination $InstallationPath
+
+Remove-Item -Path $TmpPath -Force -Recurse
+
+$BinFile = Join-Path $InstallationPath "bin/${PackageName}.exe"
+Install-BinFile -Path $BinFile -Name "${PackageName}"
