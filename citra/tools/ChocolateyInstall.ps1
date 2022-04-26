54c8bb81ad6ee809b910eeae29b7e42316766a35
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-04-26 16:03:07 +0000

    AU: 2 updated - citra fastcopy.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/ca57cce69441c6a3a828dbdd58e390255419f01b

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2122/citra-windows-mingw-20220425-3d62fef.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2123/citra-windows-mingw-20220426-12b1a2d.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
