0dbb5a2cd4675598c1187d008f8618c36e798d53
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-05-05 08:42:59 +0800

    [openjdk.portable] Add

--- openjdk.portable/update.ps1
+++ openjdk.portable/update.ps1
@@ -12,8 +12,11 @@ function global:au_GetLatest {
 
     $page = Invoke-WebRequest -UseBasicParsing -Uri $url
     $url64 = ($page.Links.href -match "windows.*zip$")[0]
-    $version = ($url64 -split '/|_' -match '^openjdk-\d+(\.\d+)*$')[0] -replace "openjdk-",""
-    $dot_count = 2 - ($version.ToCharArray() | Where-Object {$_ -eq '.'} | Measure-Object).Count
+    $version = ($url64 -split '/|_' -match '^openjdk-\d+(\.\d+)*$')[0] -replace "openjdk-", ""
+    $dot_count = 2 - ($version.ToCharArray() | Where-Object { $_ -eq '.' } | Measure-Object).Count
+    if ($dot_count -lt 0) {
+        $dot_count = 0
+    }
     $version = $version + (".0" * $dot_count)
 
     return @{
