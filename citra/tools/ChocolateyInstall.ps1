f1d93de8277d009e1e988b4fbfde3966ef43d407
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-05-14 16:04:34 +0000

    AU: 2 updated - citra x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/adadf149fd2e05ef3e02c8fa1e59ff20b6db75a2

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2140/citra-windows-mingw-20220513-c4069c7.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2141/citra-windows-mingw-20220514-55ed4c0.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- x64dbg.portable/tools/ChocolateyInstall.ps1
+++ x64dbg.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'x64dbg'
-$Url32 = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2022-04-11_01-01.zip'
+$Url32 = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2022-05-14_16-09.zip'
 $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 . $(Join-Path $ToolsPath "StopProcess.ps1")
