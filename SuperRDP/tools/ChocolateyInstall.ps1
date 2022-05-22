95745d3aabc95b128517af48940a5c45f2bb6e80
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-05-22 10:54:19 +0800

    [SuperRDP] Add

---
+++ SuperRDP/tools/ChocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'SuperRDP'
+$Url = 'https://github.com/anhkgg/SuperRDP/releases/download/2.0.0.38/SuperRDP2_2.0.0.38.zip'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
