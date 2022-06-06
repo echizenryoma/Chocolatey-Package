5726a4f506234fa04afe588a8b293e6f16350e07
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-06-06 21:42:39 +0800

    [bcompare.portable] Add

---
+++ bcompare.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'BeyondCompare'
+$Url64 = 'https://www.scootersoftware.com/BCompare-4.4.2.26348.exe'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$TmpLocation = Join-Path $InstallationPath 'tmp'
+$UnzipLocation = $TmpLocation
+
+$DownloadFile = Join-Path $TmpLocation "${PackageName}.exe"
+$PackageArgs = @{
+    PackageName  = $PackageName
+    Url64        = $Url64
+    FileFullPath = $DownloadFile
+}
+Get-ChocolateyWebFile @PackageArgs
+
+& innoextract.exe -e "$DownloadFile" -d "$UnzipLocation"
+
+$BinName = "BCompare"
+$AppPath = (Get-ChildItem -Path $UnzipLocation -Recurse -Include "${BinName}.exe").Directory.FullName
+Copy-Item -Path $(Join-Path $AppPath "*") -Destination $InstallationPath -Recurse -Force
+Remove-Item -Path $TmpLocation -Recurse -Force -ErrorAction Ignore
+
+Install-BinFile -Path $(Join-Path $InstallationPath "${BinName}.exe") -Name "${BinName}"
---
+++ bcompare.portable/update.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $base = "https://www.scootersoftware.com"
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "$base/download.php"
+    $url64 = $base + ($page.Links.href -match "BCompare-.*.exe")[0]
+    $version = ($url64 -split '-|.exe' -match '\d+(\.\d+)')[0]
+
+    return @{
+        URL64   = $url64
+        Version = $version
+    }
+}
+
+Update-Package -NoCheckChocoVersion -ChecksumFor none
