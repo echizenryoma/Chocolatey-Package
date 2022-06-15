288ae0406a40f377f47b72887c3e012057294265
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-06-15 16:03:35 +0000

    AU: 2 updated - citra k-litecodecpack.mega [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/1ffbe8ffbd134fbb635c9d09c397a4169381211c

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2172/citra-windows-mingw-20220614-1fb57ca.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2173/citra-windows-mingw-20220615-d0cf14f.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- k-litecodecpack.mega/tools/ChocolateyInstall.ps1
+++ k-litecodecpack.mega/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'k-litecodecpack.mega'
-$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1700_Mega.exe'
-$Checksum32 = 'e9b3583e3716272c0da91b233f3c4808f2a2e11914c359a41f0bb2ab7fd2104a'
+$Url32 = 'https://files2.codecguide.com/K-Lite_Codec_Pack_1705_Mega.exe'
+$Checksum32 = 'd12b3e1694e4fdf0cb1fa62475a2ffaaa5ad1d0c38022f977a646d5e0d5bd87d'
 $ChecksumType32 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
