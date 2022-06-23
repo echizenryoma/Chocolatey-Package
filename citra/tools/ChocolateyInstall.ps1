296084ed9b60e7238962862c689bcd7f96aa0511
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-06-23 16:03:22 +0000

    AU: 2 updated - citra insomnia.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/5a502b4ee56ecc510faa31d7fc0480b863a97e96

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2180/citra-windows-mingw-20220622-479beb2.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2181/citra-windows-mingw-20220623-4207e97.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
