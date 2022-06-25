229f47f90f032303ec63d7b795fb6a0c91cf2454
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-06-25 16:06:31 +0000

    AU: 2 updated - citra npcap [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/a9f414ebdf617253efdb59e33c3835baf925f8bb

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2182/citra-windows-mingw-20220624-ae14756.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2183/citra-windows-mingw-20220625-d930274.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- npcap/tools/ChocolateyInstall.ps1
+++ npcap/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'npcap'
-$Url32 = 'https://nmap.org/npcap/dist/npcap-1.60.exe'
+$Url32 = 'https://nmap.org/npcap/dist/npcap-1.70.exe'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $DownloadPath = Join-Path $ToolsPath "tmp"
 
