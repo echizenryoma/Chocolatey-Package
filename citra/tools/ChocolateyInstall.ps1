473d21086a6e3fa7344a5f0523da42fd1286cf38
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-06-28 16:05:21 +0000

    AU: 2 updated - citra mitmproxy.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/4ff71e4d59bb2cc37adbfdfd89f4466a63ebb927

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2185/citra-windows-mingw-20220627-27db4a5.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2186/citra-windows-mingw-20220628-740c6a8.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
