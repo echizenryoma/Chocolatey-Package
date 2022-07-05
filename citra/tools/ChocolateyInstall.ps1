447c963541332b822c00d6e970442b3df4424e70
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-07-05 16:06:07 +0000

    AU: 2 updated - citra newfiletime [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/461b114cdc94d42cf3e299e6fde96288d3248c9f

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2192/citra-windows-mingw-20220704-7574727.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2193/citra-windows-mingw-20220705-b3f5a84.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- newfiletime/tools/ChocolateyInstall.ps1
+++ newfiletime/tools/ChocolateyInstall.ps1
@@ -2,13 +2,13 @@
 
 $PackageName = 'newfiletime'
 $Url32 = 'https://www.softwareok.com/Download/NewFileTime.zip'
-$Checksum32 = 'bb5fa4cafbc14ed59e15524e051d19e39ee2450c552a2126f5dd5c6bc88f8acc'
+$Checksum32 = '5f2f9f5642a8266e88c00f0f950c31be12dbecd5a6883a3e57a5b528a5adb808'
 $ChecksumType32 = 'sha256'
 $Url64 = 'https://www.softwareok.com/Download/NewFileTime_x64.zip'
-$Checksum64 = 'cc0ddd09b5d8f54a96be2e62ebf1f51359c355eb33650f269250e90eab339d6e'
+$Checksum64 = 'a39648b34e5b485727b5ca33f2768160120ed27118a6eeefd0164bfe3ad76363'
 $ChecksumType64 = 'sha256'
 $UrlExtra = 'https://www.softwareok.com/Download/NewFileTime_Unicode.zip'
-$ChecksumExtra = '45278468aeae4515198ca831fcdc98d937e895de35e9574fb3f174fa0f32327f'
+$ChecksumExtra = '3a4f5a086539cd59bf001b1c063d114781f7cae6abd80d97360853177f45b54a'
 $ChecksumTypeExtra = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
