a703ff91698bb8c93a5a535221656342ed0d096d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-07-07 16:04:44 +0000

    AU: 2 updated - citra nodejs.lts [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/0698cbbe926830d405b9d8048265b47605322ea2

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2194/citra-windows-mingw-20220706-f1899d3.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2195/citra-windows-mingw-20220707-a77a99d.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
--- nodejs.lts/tools/ChocolateyInstall.ps1
+++ nodejs.lts/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'nodejs'
-$Url32 = 'https://nodejs.org/dist/v16.15.1/node-v16.15.1-win-x86.7z'
-$Checksum32 = 'd03c7d1dbe397a6095d0119c2a48105949c12e212fb7cda5a8df986a1af69a9b'
+$Url32 = 'https://nodejs.org/dist/v16.16.0/node-v16.16.0-win-x86.7z'
+$Checksum32 = 'a933c0f78bc653dfba11df49c476e90e7ed2492f34319d74bf7de2b21f8943d8'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://nodejs.org/dist/v16.15.1/node-v16.15.1-win-x64.7z'
-$Checksum64 = '378a9202374ac65377e13966dc80d435538db5a5cb128d629fa69f638c157a83'
+$Url64 = 'https://nodejs.org/dist/v16.16.0/node-v16.16.0-win-x64.7z'
+$Checksum64 = 'cfe9ba47f32dd2c667078ed175e76cad1d00bd4ea9f915219ead81d2cfca3c34'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
