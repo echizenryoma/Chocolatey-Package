39e295d948e53dc6ddb0cbc595007c5803371720
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-07-28 16:03:20 +0000

    AU: 2 updated - citra ventoy.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/f59721def21a4018a4c8800217bb153cfaf0570d

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2215/citra-windows-mingw-20220727-dffe8e2.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2217/citra-windows-mingw-20220728-5af8b58.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
