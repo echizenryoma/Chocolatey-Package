593351c6355ae87b5f94b9c86e21f5b2b12e5a57
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-08-06 08:05:00 +0800

    Revert "[smplayer.portable] Remove"
    This reverts commit 8676e4206484487a6e6631403e2552b3ba3b38f0.

---
+++ smplayer.portable/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$PackageName = 'smplayer'
+
+$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "${PackageName}.lnk"
+Remove-Item $LinkPath -Force -ErrorAction Ignore
+Uninstall-BinFile -Name ${PackageName}
