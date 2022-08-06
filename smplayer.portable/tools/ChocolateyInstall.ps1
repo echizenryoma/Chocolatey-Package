bbc03e348e1de94550743e28ca98ae7062543896
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-08-06 08:16:32 +0800

    [smplayer.portable] Add

--- smplayer.portable/tools/ChocolateyInstall.ps1
+++ smplayer.portable/tools/ChocolateyInstall.ps1
@@ -1,20 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'SMPlayer'
-$Url64 = 'https://sourceforge.net/projects/smplayer/files/SMPlayer/21.10.0/smplayer-portable-21.10.0.0-x64.7z/download'
+$Url64 = 'https://sourceforge.net/projects/smplayer/files/SMPlayer/22.7.0/smplayer-22.7.0-x64.exe/download'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-$UnzipLocation = Join-Path $InstallationPath 'tmp'
 
 $PackageArgs = @{
     PackageName   = $PackageName
     Url64         = $Url64
-    UnzipLocation = $UnzipLocation
+    UnzipLocation = $InstallationPath
 }
 Install-ChocolateyZipPackage @PackageArgs
-
-$UnzipPath = (Get-ChildItem $UnzipLocation -Directory | Where-Object Name -Match "${PackageName}" | Select-Object -First 1).FullName
-Copy-Item -Path $(Join-Path $UnzipPath "*") -Destination $InstallationPath -Recurse -Force
-Remove-Item -Path $UnzipLocation -Recurse -Force -ErrorAction Ignore
 
 $BinFile = Join-Path $InstallationPath "${PackageName}.exe"
 Install-BinFile -Name ${PackageName} -Path $BinFile
