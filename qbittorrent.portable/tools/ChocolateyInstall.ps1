700de62e9b8bc83d7c7f6b078c8f0c442633d68f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-08-31 16:06:41 +0000

    AU: 3 updated - citra pkhex qbittorrent.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/36593b3283fb79e6609a2e5203c779b7b651ec4b

--- qbittorrent.portable/tools/ChocolateyInstall.ps1
+++ qbittorrent.portable/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'qbittorrent'
-$Url32 = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.4.4/qbittorrent_4.4.4_setup.exe/download'
-$Url64 = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.4.4/qbittorrent_4.4.4_x64_setup.exe/download'
+$Url32 = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.4.5/qbittorrent_4.4.5_setup.exe/download'
+$Url64 = 'https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.4.5/qbittorrent_4.4.5_x64_setup.exe/download'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
