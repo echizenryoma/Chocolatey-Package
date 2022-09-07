1b35f08219882d4084ba1a4a0e3ac3a4e1ac90fa
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-09-07 16:03:49 +0000

    AU: 2 updated - golang.portable x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/2647f152603c32f1e5e2d064e9dd0b5e30a9076f

--- golang.portable/tools/ChocolateyInstall.ps1
+++ golang.portable/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'go'
-$Url32 = 'https://storage.googleapis.com/golang/go1.19.windows-386.zip'
-$Url64 = 'https://storage.googleapis.com/golang/go1.19.windows-amd64.zip'
+$Url32 = 'https://storage.googleapis.com/golang/go1.19.1.windows-386.zip'
+$Url64 = 'https://storage.googleapis.com/golang/go1.19.1.windows-amd64.zip'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
