f04bf2d43ac5653e87b25a6ad688b6d0b2b20dfa
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-09-16 16:09:02 +0000

    AU: 2 updated - calibre.portable citra [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/4d852185f64ecb79bdc8ab63933d2744ab49e2b7

--- calibre.portable/tools/ChocolateyInstall.ps1
+++ calibre.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'Calibre'
-$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v6.4.0/calibre-portable-installer-6.4.0.exe'
+$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v6.5.0/calibre-portable-installer-6.5.0.exe'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 $SilentArgs = "${UnzipLocation}"
--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2266/citra-windows-mingw-20220915-baeff6e.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2267/citra-windows-mingw-20220916-d947e71.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
