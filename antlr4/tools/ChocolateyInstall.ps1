b0297f9d83e175d1acda2fb8c6197b749c2f94f7
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-09-29 11:45:37 +0800

    [antrlr4] Add

---
+++ antlr4/tools/ChocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'antlr4'
+$Url = 'https://www.antlr.org/download/antlr-4.11.1-complete.jar'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$SavePath = Join-Path ${ToolsPath} "antlr4-complete.jar"
+
+$PackageArgs = @{
+    PackageName  = $PackageName
+    Url          = $Url
+    FileFullPath = $SavePath
+}
+Get-ChocolateyWebFile @PackageArgs
