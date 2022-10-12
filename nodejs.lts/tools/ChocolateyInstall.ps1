ea45d1644211264826b7899a8b4f0eff404ef227
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-10-12 16:06:01 +0000

    AU: 3 updated - nodejs.lts vscode.portable x64dbg.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/075e0c5fe1d5b510299ada9ae58950bff5e55404

--- nodejs.lts/tools/ChocolateyInstall.ps1
+++ nodejs.lts/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'nodejs'
-$Url32 = 'https://nodejs.org/dist/v16.17.1/node-v16.17.1-win-x86.7z'
-$Checksum32 = '0f8101648d5c9e49e89fee541da9e574f899716c32b7c51a732b1766b9fc4526'
+$Url32 = 'https://nodejs.org/dist/v16.18.0/node-v16.18.0-win-x86.7z'
+$Checksum32 = '25caf14762fc76251e826562e7111baf7adaef268a618c0c87eadb26fe93eeb9'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://nodejs.org/dist/v16.17.1/node-v16.17.1-win-x64.7z'
-$Checksum64 = '9777e8c4b2864c5b54a0e4e9400f14887db68560a09b94b4113b560a64d1e680'
+$Url64 = 'https://nodejs.org/dist/v16.18.0/node-v16.18.0-win-x64.7z'
+$Checksum64 = '81b972f6f773fcf5297e6eb604d279ff8b76b5d6ea4e2cfab101de39f8e968b6'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'vscode'
-$Url32 = 'https://update.code.visualstudio.com/1.72.0/win32-archive/stable'
+$Url32 = 'https://update.code.visualstudio.com/1.72.1/win32-archive/stable'
 $ChecksumType32 = 'sha256'
-$Checksum32 = '775bcfe5ac162b66a2be2aeeac1a16797b0001ee79f9eba25849743e8215a60c'
-$Url64 = 'https://update.code.visualstudio.com/1.72.0/win32-x64-archive/stable'
+$Checksum32 = 'a57f8a19ad77908a4508c8b361dacc46aa81cedfb351014d1677324a84bd869d'
+$Url64 = 'https://update.code.visualstudio.com/1.72.1/win32-x64-archive/stable'
 $ChecksumType64 = 'sha256'
-$Checksum64 = '4d740241a40ba247b3606db7e20f66a783ea15c622ad0d3f54f1fc6cfbb84271'
+$Checksum64 = '77e4d037ae592e7a807a49657ad539429a43f3bd6552bc83c518d03eb31d769d'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $DataPath = Join-Path $InstallationPath 'data'
 
