ac158d5a342efd9f52fc733a897227dcde59f0fe
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-10-15 08:31:24 +0800

    [oh-my-posh.portable] Add

---
+++ oh-my-posh.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'oh-my-posh'
+$Url64 = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v12.3.0/posh-windows-amd64.7z'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$InstallationPath = (Get-Item -Path $ToolsPath).FullName
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url64         = $Url64
+    UnzipLocation = $InstallationPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$ThemesPath = Join-Path $InstallationPath 'themes'
+Install-ChocolateyEnvironmentVariable -VariableName 'POSH_THEMES_PATH' -VariableValue $ThemesPath -VariableType 'Machine'
---
+++ oh-my-posh.portable/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$InstallationPath = (Get-Item -Path $ToolsPath).FullName
+Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
+
+Uninstall-ChocolateyEnvironmentVariable -VariableName 'POSH_THEMES_PATH' -VariableType 'Machine'
