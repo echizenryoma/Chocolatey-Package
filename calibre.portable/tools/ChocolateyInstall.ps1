2b5c11e66210b1cc5ab1eb8ffae4b5372e93d248
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-10-16 05:04:46 +0000

    AU: 2 updated - calibre.portable oh-my-posh.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/f001942eb367d94ddbd5278fcbf753c3512dbb90

--- calibre.portable/tools/ChocolateyInstall.ps1
+++ calibre.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'Calibre'
-$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v6.5.0/calibre-portable-installer-6.5.0.exe'
+$Url = 'https://github.com/kovidgoyal/calibre/releases/download/v6.7.1/calibre-portable-installer-6.7.1.exe'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 $SilentArgs = "${UnzipLocation}"
