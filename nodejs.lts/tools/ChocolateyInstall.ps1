b3f295ade741ff1cd402b69609dfddb503673ee0
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-11-05 16:02:42 +0000

    AU: 3 updated - bcompare.portable bcompare-zh.portable nodejs.lts [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/e875c4076bdf65b65cf8d6e94a3610f1b9479e91

--- nodejs.lts/tools/ChocolateyInstall.ps1
+++ nodejs.lts/tools/ChocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'nodejs'
-$Url32 = 'https://nodejs.org/dist/v18.12.0/node-v18.12.0-win-x86.7z'
-$Checksum32 = '3c822e7179c544f7cf1b12ddd0de48e5d65d4c54f0c41d27949cddde4a603469'
+$Url32 = 'https://nodejs.org/dist/v18.12.1/node-v18.12.1-win-x86.7z'
+$Checksum32 = 'a02b64d9a3f457e00a6138d6b51812b53dcc458632b228cba862081263d01d48'
 $ChecksumType32 = 'sha256'
-$Url64 = 'https://nodejs.org/dist/v18.12.0/node-v18.12.0-win-x64.7z'
-$Checksum64 = 'cfa003b9ed643d41167a82802fdc1335a280b4844330423bf15f07dc3148d2bb'
+$Url64 = 'https://nodejs.org/dist/v18.12.1/node-v18.12.1-win-x64.7z'
+$Checksum64 = 'b8f73f9b7fa78aebf172893713b14908aa46585dbb88e333bf9c26102533a7f6'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
