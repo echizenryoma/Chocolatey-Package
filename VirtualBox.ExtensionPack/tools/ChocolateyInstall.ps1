76e34fda850a70857eca7163c1c551982913d232
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-11-06 09:24:33 +0800

    Revert "[VirtualBox.ExtensionPack] Remove"
    This reverts commit 8466b8a86135c6ff88707cc1525b60f456f3e01e.

---
+++ VirtualBox.ExtensionPack/tools/ChocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Ignore'
+
+$PackageName = 'VirtualBox.ExtensionPack'
+$Url = 'https://download.virtualbox.org/virtualbox/6.1.30/Oracle_VM_VirtualBox_Extension_Pack-6.1.30.vbox-extpack'
+$ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$VBoxExtensionPack = Join-Path $ToolsPath $([IO.Path]::GetFileName($Url))
+$VBoxManage = Join-Path $Env:VBOX_MSI_INSTALL_PATH "VBoxManage.exe"
+
+# Refresh the PS session environment so that if VirtualBox was just installed in this session, it will be found in PATH
+Update-SessionEnvironment
+
+$PackageArgs = @{
+  PackageName  = $PackageName
+  Url          = $Url
+  FileFullPath = $VBoxExtensionPack
+}
+Get-ChocolateyWebFile @PackageArgs
+
+
+
+# Install the Extension Pack using VBoxManage
+Write-Output "y" | & "$VBoxManage" extpack install --replace "$VBoxExtensionPack" 2>1
