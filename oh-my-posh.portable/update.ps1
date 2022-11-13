0b96bf7ee78f01bb8d068ca0fd07d7dc5669543c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-11-13 16:13:28 +0800

    [oh-my-posh] Update

--- oh-my-posh.portable/update.ps1
+++ oh-my-posh.portable/update.ps1
@@ -3,7 +3,9 @@
 function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
-            "(^[$]Url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^[$]Url32\s*=\s*)('.*')"    = "`$1'$($Latest.URL32)'"
+            "(^[$]Url64\s*=\s*)('.*')"    = "`$1'$($Latest.URL64)'"
+            "(^[$]UrlExtra\s*=\s*)('.*')" = "`$1'$($Latest.URLExtra)'"
         }
     }
 }
@@ -13,11 +15,15 @@ function global:au_GetLatest {
     $json = $page.Content | ConvertFrom-Json
 
     $version = $json.tag_name -replace "v", ""
-    $url64 = ($json.assets | Where-Object name -Match "amd64*\.7z$")[0].browser_download_url
+    $url32 = ($json.assets | Where-Object name -Match "windows-386.exe$")[0].browser_download_url
+    $url64 = ($json.assets | Where-Object name -Match "windows-amd64.exe$")[0].browser_download_url
+    $url_extra = ($json.assets | Where-Object name -Match "themes.zip$")[0].browser_download_url
     
     return @{
-        Version = $version
-        URL64   = $url64
+        Version  = $version
+        URL32    = $url32
+        URL64    = $url64
+        URLExtra = $url_extra
     }
 }
 
