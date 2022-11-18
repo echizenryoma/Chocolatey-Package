f91f788de41c2a1b68c63d6c5d5d247cd33f68b5
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-11-18 16:03:00 +0000

    AU: 3 updated - citra oh-my-posh.portable VirtualBox.ExtensionPack [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/0f28e980aa13a35bf1e2176f9f24e439d028607f

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2316/citra-windows-mingw-20221117-7b3bd20.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2317/citra-windows-mingw-20221118-ba7cdc4.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
