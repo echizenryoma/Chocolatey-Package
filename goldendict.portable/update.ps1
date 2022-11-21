09ff121550275b943d04f5f63b98c17b0b1aadfa
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-11-22 07:33:58 +0800

    [goldendict.portable] Use qt5

--- goldendict.portable/update.ps1
+++ goldendict.portable/update.ps1
@@ -12,7 +12,7 @@ function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/repos/xiaoyifang/goldendict/releases/latest"
     $json = $page.Content | ConvertFrom-Json
 
-    $url64 = ($json.assets | Where-Object name -Match "GoldenDict.exe.*windows.*.zip$")[-1].browser_download_url
+    $url64 = ($json.assets | Where-Object name -Match "GoldenDict.exe.*windows.*.zip$")[0].browser_download_url
     $version = ($json.name -split "/|-|v" -match "^\d+(\.\d+)+$")[0] + "." + ($json.name -split "\." -match "^\d{6}$")
 
     return @{
