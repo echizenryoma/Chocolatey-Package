b1e98a6802ff641d201a748866f16d64f720f285
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-12-03 16:05:15 +0000

    AU: 2 updated - citra oh-my-posh.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/7e6df12d2c92eb91514340ffee1b6a45e59fd806

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2329/citra-windows-mingw-20221202-75cca07.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2330/citra-windows-mingw-20221203-bf469e3.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
