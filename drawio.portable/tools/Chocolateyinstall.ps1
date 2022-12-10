d95f4f1fd75b8e9deccdde0272353bf716c2cffc
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-12-10 16:05:05 +0000

    AU: 3 updated - citra drawio.portable oh-my-posh.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/8d41159764b16eb3134ab3a3905883cb6aeaeec8

--- drawio.portable/tools/Chocolateyinstall.ps1
+++ drawio.portable/tools/Chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'drawio'
-$Url64 = 'https://github.com/jgraph/drawio-desktop/releases/download/v20.6.1/draw.io-20.6.1-windows-no-installer.exe'
+$Url64 = 'https://github.com/jgraph/drawio-desktop/releases/download/v20.6.2/draw.io-20.6.2-windows-no-installer.exe'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 $UnzipLocation = Join-Path $InstallationPath 'tmp'
 
