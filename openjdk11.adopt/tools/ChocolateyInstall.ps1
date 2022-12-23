f9720b05afccbb2ef15c01808be894d24cb81c43
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2022-12-23 21:11:55 +0800

    [openjdk11.adopt] Add symbolic link

--- openjdk11.adopt/tools/ChocolateyInstall.ps1
+++ openjdk11.adopt/tools/ChocolateyInstall.ps1
@@ -5,12 +5,21 @@ $Url64 = 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/j
 $Checksum64 = 'fea633dc37f007cb6b1e1af1874da63ad3d5e31817e583048287c67010dce5c8'
 $ChecksumType64 = 'sha256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) 'java'
+$TmpLocation = Join-Path $InstallationPath 'tmp'
 
 $PackageArgs = @{
     PackageName    = $PackageName
     Url64          = $Url64
     Checksum64     = $Checksum64
     ChecksumType64 = $ChecksumType64
-    UnzipLocation  = $InstallationPath
+    UnzipLocation  = $TmpLocation
 }
 Install-ChocolateyZipPackage @PackageArgs
+
+$UnzipLocation = Get-ChildItem $TmpLocation -Directory | Select-Object -Last 1
+$JdkLocation = Join-Path $InstallationPath $UnzipLocation.BaseName
+Copy-Item -Path $UnzipLocation.FullName -Destination $JdkLocation -Recurse -Force
+Remove-Item -Path $TmpLocation -Recurse -Force
+
+$JdkMajorLinkPath = Join-Path $InstallationPath "jdk11"
+New-Item -ItemType SymbolicLink -Path $JdkMajorLinkPath -Target $JdkLocation -Force
