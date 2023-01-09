2cdbd66a3f4428e866ebfea0d41a0ffd3741b37d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2023-01-09 16:04:09 +0000

    AU: 4 updated - citra dbeaver.portable insomnia.portable oh-my-posh.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/99dc33f497dd78def7e6465c9cefe7d7d76b6724

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2360/citra-windows-mingw-20230108-95aa445.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2361/citra-windows-mingw-20230109-f119f19.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
