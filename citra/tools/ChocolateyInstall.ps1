28c960d46dc612e5b29a03df94563aa660e37b6c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2023-01-14 04:15:02 +0000

    AU: 2 updated - citra foobar2000.portable [skip ci] https://gist.github.com/06bd306e9a1c0a26e8d81fe185ef77d0/dfe5c925d0df638062957052aab788e20f28f189

--- citra/tools/ChocolateyInstall.ps1
+++ citra/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'citra'
-$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2364/citra-windows-mingw-20230112-1a7c6f9.7z'
+$Url = 'https://github.com/citra-emu/citra-canary/releases/download/canary-2366/citra-windows-mingw-20230114-0bbf367.7z'
 $ToolsPath = Join-Path $(Get-ToolsLocation) $PackageName
 
 $PackageArgs = @{
