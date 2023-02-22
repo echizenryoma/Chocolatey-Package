ce6651cffe047fbf2bdcf548f65e93cff495c696
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2023-02-22 21:12:27 +0800

    [k-litecodecpack.mega] change url

--- k-litecodecpack.mega/tools/ChocolateyInstall.ps1
+++ k-litecodecpack.mega/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'k-litecodecpack.mega'
-$Url32 = 'https://files2.codecguide.com/K-Lite_Codec_Pack_1745_Mega.exe'
+$Url32 = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1745_Mega.exe'
 $Checksum32 = 'd819de92a675ae6da99c60cd3b0ee2bac21eb0560c3cef6d566c8cad4dfb61e4'
 $ChecksumType32 = 'sha256'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
