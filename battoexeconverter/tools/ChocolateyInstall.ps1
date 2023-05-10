991688babda6cdc061cd7c3b1d5bdfe1a84ba52f
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2023-05-10 19:42:27 +0800

    [battoexeconverter] Add

---
+++ battoexeconverter/tools/ChocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'battoexeconverter'
+$Url = 'https://ipfs.io/ipfs/QmPBp7wBSC9GukPUcp7LXFCGXBvc2e45PUfWUbCJzuLG65?filename=Bat_To_Exe_Converter.zip'
+$ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    UnzipLocation = $ToolsPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+Get-ChildItem $ToolsPath -File -Filter "*.exe" -Include "*Setup*", "*Installer*" -Recurse | Remove-Item -Force -Recurse
---
+++ battoexeconverter/update.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+    }
+}
+
+function global:au_AfterUpdate ($Package) {
+    $global:Options.Push = $true
+}
+
+function global:au_GetLatest {
+    $url = 'https://ipfs.io/ipfs/QmPBp7wBSC9GukPUcp7LXFCGXBvc2e45PUfWUbCJzuLG65?filename=Bat_To_Exe_Converter.zip'
+    $dir = Join-Path "$Env:TMP" "choco\battoexeconverter"
+    $file = Join-Path "$dir" "Bat_To_Exe_Converter.zip"
+    $null = New-Item -Path $dir -ItemType Directory
+    Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $file
+    Expand-Archive $file -DestinationPath $dir
+    $version = (Get-ChildItem -Path $dir -File -Filter "*.exe" -Exclude "*Setup*", "*Installer*" -Recurse)[0].VersionInfo.FileVersionRaw
+
+    return @{
+        Version = $version
+    }
+}
+
+Update-Package -NoCheckChocoVersion -ChecksumFor none
