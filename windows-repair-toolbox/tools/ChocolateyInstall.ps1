d932cf04c1c2bcff1ba47d2c0bf37648c0d45b24
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2023-06-14 16:03:11 +0000

    AU: 1 updated - windows-repair-toolbox [skip ci] https://gist.github.com/echizenryoma/06bd306e9a1c0a26e8d81fe185ef77d0/526cdc2d14baba9263415809554f5562194cb54d

--- windows-repair-toolbox/tools/ChocolateyInstall.ps1
+++ windows-repair-toolbox/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $PackageName = 'windows-repair-toolbox'
 $Url = 'https://windows-repair-toolbox.com/files/Windows_Repair_Toolbox.zip'
-$Checksum = '258BA6ACFAD693D3B8B06F47B684957DDD3AE6FFB23376FC8375B37F8459DDFB'
+$Checksum = '43FDE8EEACB4B2C977025E1F55B1D2B753ABC4CC67C80471FDB19B6BB40F6945'
 $ChecksumType = 'SHA256'
 $InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
 
