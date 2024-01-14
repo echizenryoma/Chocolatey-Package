dc1ae581455e9bb96b2800564d9f8b3a94f27b53
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2024-01-14 12:41:27 +0800

    windows-repair-toolbox: Remove

--- windows-repair-toolbox/tools/ChocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$PackageName = 'windows-repair-toolbox'
-$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
-
-Remove-Item $InstallationPath -Recurse -Force -ErrorAction Ignore
-Uninstall-BinFile -Name 'Windows_Repair_Toolbox'
