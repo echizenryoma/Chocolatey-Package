42b72c2d58f68ecf7bf3679cad369b4e925f2b6d
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2024-01-19 16:02:29 +0000

    AU: 1 updated - npcap [skip ci]

--- npcap/tools/ChocolateyInstall.ps1
+++ npcap/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'npcap'
-$Url32 = 'https://nmap.org/npcap/dist/npcap-1.78.exe'
+$Url32 = 'https://nmap.org/npcap/dist/npcap-1.79.exe'
 $ToolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $DownloadPath = (Get-Item -Path $ToolsPath).FullName
 
